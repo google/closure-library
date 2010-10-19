514ec4ccc74f62c2adb269ef041f529be73c9f0a
eae@google.com <eae@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2010-10-19 22:28:46 +0000

    Automated g4 rollback.
    *** Reason for rollback ***
    
    Fixed externs.
    
    *** Original change description ***
    
    Automated g4 rollback.
    
    *** Reason for rollback ***
    
    Broke builds due to out of date externs file.
    
    *** Original change description ***
    
    added state field to BrowserEvent.
    
    R=arv
    DELTA=30  (30 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=358600
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@375 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/events/browserevent.js
+++ closure/goog/events/browserevent.js
@@ -34,6 +34,7 @@
  * - altKey         {boolean}   Was alt key depressed
  * - shiftKey       {boolean}   Was shift key depressed
  * - metaKey        {boolean}   Was meta key depressed
+ * - state          {Object}    History state object
  *
  * NOTE: The keyCode member contains the raw browser keyCode. For normalized
  * key and character code use {@link goog.events.KeyHandler}.
@@ -207,6 +208,14 @@ goog.events.BrowserEvent.prototype.metaKey = false;
 
 
 /**
+ * History state object, only set for PopState events where it's a copy of the
+ * state object provided to pushState or replaceState.
+ * @type {Object}
+ */
+goog.events.BrowserEvent.prototype.state;
+
+
+/**
  * Whether the default platform modifier key was pressed at time of event.
  * (This is control for all platforms except Mac, where it's Meta.
  * @type {boolean}
@@ -273,6 +282,7 @@ goog.events.BrowserEvent.prototype.init = function(e, opt_currentTarget) {
   this.shiftKey = e.shiftKey;
   this.metaKey = e.metaKey;
   this.platformModifierKey = goog.userAgent.MAC ? e.metaKey : e.ctrlKey;
+  this.state = e.state;
   this.event_ = e;
   delete this.returnValue_;
   delete this.propagationStopped_;
