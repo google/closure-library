2b11604db40336907cfb2baf85d8ef4275f25cdc
marcosalmeida@google.com <marcosalmeida@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-01-20 21:54:24 +0000

    Automated g4 rollback.
    *** Reason for rollback ***
    
    Resubmission, plus: prevents NPE if menu item has no parent menu; 
    
    *** Original change description ***
    
    Automated g4 rollback.
    
    *** Reason for rollback ***
    
    Broke everybody
    
    *** Original change description ***
    
    Prevent mouseup from activating a menu item if that mouseup is part the user clicking a menu button.
    
    R=ssaviano,digi,eae
    DELTA=189  (159 added, 0 deleted, 30 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=333
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@638 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/ui/menubutton.js
+++ closure/goog/ui/menubutton.js
@@ -218,7 +218,7 @@ goog.ui.MenuButton.prototype.handleMouseDown = function(e) {
   goog.ui.MenuButton.superClass_.handleMouseDown.call(this, e);
   if (this.isActive()) {
     // The component was allowed to activate; toggle menu visibility.
-    this.setOpen(!this.isOpen());
+    this.setOpen(!this.isOpen(), e);
     if (this.menu_) {
       this.menu_.setMouseButtonPressed(this.isOpen());
     }
@@ -628,9 +628,11 @@ goog.ui.MenuButton.prototype.hideMenu = function() {
 /**
  * Opens or closes the attached popup menu.
  * @param {boolean} open Whether to open or close the menu.
+ * @param {goog.events.Event=} opt_e Mousedown event that caused the menu to
+ *     be opened.
  * @override
  */
-goog.ui.MenuButton.prototype.setOpen = function(open) {
+goog.ui.MenuButton.prototype.setOpen = function(open, opt_e) {
   goog.ui.MenuButton.superClass_.setOpen.call(this, open);
   if (this.menu_ && this.hasState(goog.ui.Component.State.OPENED) == open) {
     if (open) {
@@ -660,7 +662,7 @@ goog.ui.MenuButton.prototype.setOpen = function(open) {
         }
       }
     }
-    this.menu_.setVisible(open);
+    this.menu_.setVisible(open, false, opt_e);
     this.attachPopupListeners_(open);
   }
 };
