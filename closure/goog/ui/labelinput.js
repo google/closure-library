8d9f248ac2ba800a4bec970665c55ba6266e6d33
pallosp@google.com <pallosp@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-02-24 12:24:55 +0000

    Implemented goog.ui.LabelInput#getLabel and #setLabel
    R=arv
    DELTA=86  (81 added, 1 deleted, 4 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=649
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@722 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/ui/labelinput.js
+++ closure/goog/ui/labelinput.js
@@ -31,7 +31,6 @@
 
 goog.provide('goog.ui.LabelInput');
 
-
 goog.require('goog.Timer');
 goog.require('goog.dom');
 goog.require('goog.dom.classes');
@@ -82,7 +81,7 @@ goog.ui.LabelInput.prototype.eventHandler_;
  * @type {boolean}
  * @private
  */
-goog.ui.LabelInput.prototype.hasFocus_;
+goog.ui.LabelInput.prototype.hasFocus_ = false;
 
 
 /**
@@ -144,9 +143,11 @@ goog.ui.LabelInput.prototype.attachEvents_ = function() {
   eh.listen(this.getElement(), goog.events.EventType.BLUR, this.handleBlur_);
 
   if (goog.userAgent.GECKO) {
-    eh.listen(this.getElement(), [goog.events.EventType.KEYPRESS,
-        goog.events.EventType.KEYDOWN, goog.events.EventType.KEYUP],
-        this.handleEscapeKeys_);
+    eh.listen(this.getElement(), [
+      goog.events.EventType.KEYPRESS,
+      goog.events.EventType.KEYDOWN,
+      goog.events.EventType.KEYUP
+    ], this.handleEscapeKeys_);
   }
 
   // IE sets defaultValue upon load so we need to test that as well.
@@ -362,6 +363,27 @@ goog.ui.LabelInput.prototype.getValue = function() {
   }
   return this.hasChanged() ? /** @type {string} */ (this.getElement().value) :
       '';
+};
+
+
+/**
+ * Sets the label text.
+ * @param {string} label The text to show as the label.
+ */
+goog.ui.LabelInput.prototype.setLabel = function(label) {
+  if (this.getElement() && !this.hasChanged()) {
+    this.getElement().value = '';
+  }
+  this.label_ = label;
+  this.restoreLabel_();
+};
+
+
+/**
+ * @return {string} The text to show as the label.
+ */
+goog.ui.LabelInput.prototype.getLabel = function() {
+  return this.label_;
 };
 
 
