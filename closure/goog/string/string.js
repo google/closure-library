5aa65a72e4c1d07c10f8d79daaa82eb6caf1a071
bogosian@google.com <bogosian@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-03-04 02:30:59 +0000

    Adds a function to goog.string to truncate in the middle, heavily favoring the start of the string.
    R=pupius
    DELTA=24  (21 added, 0 deleted, 3 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=757
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@740 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/string/string.js
+++ closure/goog/string/string.js
@@ -717,15 +717,25 @@ goog.string.truncate = function(str, chars, opt_protectEscapedCharacters) {
  * @param {number} chars Max number of characters.
  * @param {boolean=} opt_protectEscapedCharacters Whether to protect escaped
  *     characters from being cutoff in the middle.
+ * @param {number=} opt_trailingChars Optional number of trailing characters to
+ *     leave at the end of the string, instead of truncating as close to the
+ *     middle as possible.
  * @return {string} A truncated copy of {@code str}.
  */
 goog.string.truncateMiddle = function(str, chars,
-    opt_protectEscapedCharacters) {
+    opt_protectEscapedCharacters, opt_trailingChars) {
   if (opt_protectEscapedCharacters) {
     str = goog.string.unescapeEntities(str);
   }
 
-  if (str.length > chars) {
+  if (opt_trailingChars) {
+    if (opt_trailingChars > chars) {
+      opt_trailingChars = chars;
+    }
+    var endPoint = str.length - opt_trailingChars;
+    var startPoint = chars - opt_trailingChars;
+    str = str.substring(0, startPoint) + '...' + str.substring(endPoint);
+  } else if (str.length > chars) {
     // Favor the beginning of the string:
     var half = Math.floor(chars / 2);
     var endPos = str.length - half;
