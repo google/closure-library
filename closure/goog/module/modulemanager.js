16d8c49c569199a36b7f671cda95c81d7c630207
mknichel@google.com <mknichel@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-03-23 16:48:09 +0000

    Fix a case where a module can be loaded after the module manager has been disposed, throwing an error.
    R=pupius
    DELTA=6  (6 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1017
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@794 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/module/modulemanager.js
+++ closure/goog/module/modulemanager.js
@@ -591,6 +591,12 @@ goog.module.ModuleManager.prototype.maybeFinishBaseLoad_ = function() {
  * @param {string} id A module id.
  */
 goog.module.ModuleManager.prototype.setLoaded = function(id) {
+  if (this.isDisposed()) {
+    this.logger_.warning(
+        'Module loaded after module manager was disposed: ' + id);
+    return;
+  }
+
   this.logger_.info('Module loaded: ' + id);
 
   this.moduleInfoMap_[id].onLoad(goog.bind(this.getModuleContext, this));
