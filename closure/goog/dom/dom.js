a3ddef4fdf233b0a96033a7c3422ac0137709f48
deboer@google.com <deboer@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-04-18 19:20:26 +0000

    Add getAncestorByClass to goog.dom.DomHelper.
    getAncestorByClass was added to goog.dom but not
    goog.dom.DomHelper
    
    R=pupius,nicksay
    DELTA=14  (14 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1515
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@868 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/dom/dom.js
+++ closure/goog/dom/dom.js
@@ -2452,6 +2452,20 @@ goog.dom.DomHelper.prototype.getAncestorByTagNameAndClass =
 
 
 /**
+ * Walks up the DOM hierarchy returning the first ancestor that has the passed
+ * class name. If the passed element matches the specified criteria, the
+ * element itself is returned.
+ * @param {Node} element The DOM node to start with.
+ * @param {?string=} opt_class The class name to match (or null/undefined to
+ *     match any node regardless of class name).
+ * @return {Node} The first ancestor that matches the passed criteria, or
+ *     null if none match.
+ */
+goog.dom.DomHelper.prototype.getAncestorByClass =
+    goog.dom.getAncestorByClass;
+
+
+/**
  * Walks up the DOM hierarchy returning the first ancestor that passes the
  * matcher function.
  * @param {Node} element The DOM node to start with.
