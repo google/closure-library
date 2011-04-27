3cbb899399cc612c980f73dda874d9170fe8064d
nnaze@google.com <nnaze@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-04-27 21:45:37 +0000

    Fix a bug in AvlTree.  getCount() can give the wrong count when count goes from 1 to 0.  Put in test case to verify behavior.
    R=tgordon
    DELTA=37  (11 added, 0 deleted, 26 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1647
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@893 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/structs/avltree.js
+++ closure/goog/structs/avltree.js
@@ -218,9 +218,10 @@ goog.structs.AvlTree.prototype.remove = function(value) {
     return retNode; // If null, we'll stop traversing the tree
   });
 
-  // If a node was removed, decrement count
+  // If a node was removed, decrement count.
   if (retValue) {
-    this.count_ -= 1;
+    // Had traverse_() cleared the tree, set to 0.
+    this.count_ = this.root_ ? this.count_ - 1 : 0;
   }
 
   // Return the value that was removed, null if the value was not in the tree
@@ -346,18 +347,18 @@ goog.structs.AvlTree.prototype.inOrderTraverse =
   // Depth traverse the tree to find node to begin in-order traversal from
   var startNode;
   if (opt_startValue) {
-      this.traverse_(function(node) {
-        var retNode = null;
-        if (this.comparator_(node.value, opt_startValue) > 0) {
-          retNode = node.left;
-          startNode = node;
-        } else if (this.comparator_(node.value, opt_startValue) < 0) {
-          retNode = node.right;
-        } else {
-          startNode = node;
-        }
-        return retNode; // If null, we'll stop traversing the tree
-      });
+    this.traverse_(function(node) {
+      var retNode = null;
+      if (this.comparator_(node.value, opt_startValue) > 0) {
+        retNode = node.left;
+        startNode = node;
+      } else if (this.comparator_(node.value, opt_startValue) < 0) {
+        retNode = node.right;
+      } else {
+        startNode = node;
+      }
+      return retNode; // If null, we'll stop traversing the tree
+    });
   } else {
     startNode = this.getMinNode_();
   }
@@ -403,18 +404,18 @@ goog.structs.AvlTree.prototype.reverseOrderTraverse =
   // Depth traverse the tree to find node to begin reverse-order traversal from
   var startNode;
   if (opt_startValue) {
-      this.traverse_(goog.bind(function(node) {
-        var retNode = null;
-        if (this.comparator_(node.value, opt_startValue) > 0) {
-          retNode = node.left;
-        } else if (this.comparator_(node.value, opt_startValue) < 0) {
-          retNode = node.right;
-          startNode = node;
-        } else {
-          startNode = node;
-        }
-        return retNode; // If null, we'll stop traversing the tree
-      }, this));
+    this.traverse_(goog.bind(function(node) {
+      var retNode = null;
+      if (this.comparator_(node.value, opt_startValue) > 0) {
+        retNode = node.left;
+      } else if (this.comparator_(node.value, opt_startValue) < 0) {
+        retNode = node.right;
+        startNode = node;
+      } else {
+        startNode = node;
+      }
+      return retNode; // If null, we'll stop traversing the tree
+    }, this));
   } else {
     startNode = this.getMaxNode_();
   }
