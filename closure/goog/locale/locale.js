96ce60f40046972fc9e878767006aec8ab67a4ed
zhenya@google.com <zhenya@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-07-02 01:06:21 +0000

    Updated the comment on the goog.locale.getLocale's deprecation jsdoc.
    R=pupius
    DELTA=1  (0 added, 0 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2515
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1096 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/locale/locale.js
+++ closure/goog/locale/locale.js
@@ -41,7 +41,7 @@ goog.locale.setLocale = function(localeName) {
 /**
  * Retrieve the currnet locale
  * @return {string} Current locale name string.
- * @deprecated Use goog.LOCALE instead.
+ * @deprecated Use goog.LOCALE and goog.i18n instead.
  */
 goog.locale.getLocale = function() {
   if (!goog.locale.activeLocale_) {
