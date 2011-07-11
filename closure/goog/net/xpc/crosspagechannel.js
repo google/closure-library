0bc10b6a95ab14ead3ed5fa8d7e34d8730f5c364
moishel@google.com <moishel@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-07-11 16:06:38 +0000

    Fix intermittent problem with window.closed in IE9.
    R=mibuerge,arv
    DELTA=7  (6 added, 0 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2556
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1108 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/net/xpc/crosspagechannel.js
+++ closure/goog/net/xpc/crosspagechannel.js
@@ -478,7 +478,13 @@ goog.net.xpc.CrossPageChannel.prototype.send = function(serviceName, payload) {
   // NOTE(user): This check is not reliable in IE, where a document in an
   // iframe does not get unloaded when removing the iframe element from the DOM.
   // TODO(user): Find something that works in IE as well.
-  if (this.peerWindowObject_.closed) {
+  // NOTE(user): "!this.peerWindowObject_.closed" evaluates to 'false' in IE9
+  // sometimes even though typeof(this.peerWindowObject_.closed) is boolean and
+  // this.peerWindowObject_.closed evaluates to 'false'. Casting it to a Boolean
+  // results in sane evaluation. When this happens, it's in the inner iframe
+  // when querying its parent's 'closed' status. Note that this is a different
+  // case than mibuerge@'s note above.
+  if (Boolean(this.peerWindowObject_.closed)) {
     goog.net.xpc.logger.severe('Peer has disappeared.');
     this.close();
     return;
