58e685f85693030a2950d237e3280f17f2dba009
henrywong@google.com <henrywong@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-07-25 07:23:22 +0000

    Fix compilation issue by declaring some types in advance of change to JS Compiler. I thought @type {?string=} would work but the presubmit doesn't like it
    R=pupius
    DELTA=12  (12 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2720
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1149 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/debug/tracer.js
+++ closure/goog/debug/tracer.js
@@ -218,6 +218,12 @@ goog.debug.Trace_.Stat_ = function() {
 
 
 /**
+ * @type {string|null|undefined}
+ */
+goog.debug.Trace_.Stat_.prototype.type;
+
+
+/**
  * @return {string} A string describing the tracer stat.
  */
 goog.debug.Trace_.Stat_.prototype.toString = function() {
@@ -241,6 +247,12 @@ goog.debug.Trace_.Stat_.prototype.toString = function() {
 goog.debug.Trace_.Event_ = function() {
   // the fields are different for different events - see usage in code
 };
+
+
+/**
+ * @type {string|null|undefined}
+ */
+goog.debug.Trace_.Event_.prototype.type;
 
 
 /**
