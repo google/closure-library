8687d57bfd8356dfb4ab55dc15d631803296fe7a
weitzman@google.com <weitzman@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-08-11 18:34:27 +0000

    Three minor closure fixes:
    - Make LabelInput.reset() safe when called before createDom() or decorate()
    - Make LabelInput.decorateInternal() recognize when it's attaching to an element that already has focus.
    - Make InputHandler.update(true /* force */) safe when there's no active element.
    
    R=pupius
    DELTA=56  (48 added, 3 deleted, 5 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=3024
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1210 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/ui/autocomplete/inputhandler.js
+++ closure/goog/ui/autocomplete/inputhandler.js
@@ -93,12 +93,14 @@ goog.provide('goog.ui.AutoComplete.InputHandler');
 
 goog.require('goog.Disposable');
 goog.require('goog.Timer');
+goog.require('goog.dom');
 goog.require('goog.dom.a11y');
 goog.require('goog.dom.selection');
-goog.require('goog.events');
 goog.require('goog.events.EventHandler');
+goog.require('goog.events.EventType');
 goog.require('goog.events.KeyCodes');
 goog.require('goog.events.KeyHandler');
+goog.require('goog.events.KeyHandler.EventType');
 goog.require('goog.string');
 goog.require('goog.ui.AutoComplete');
 goog.require('goog.userAgent');
@@ -1142,7 +1144,8 @@ goog.ui.AutoComplete.InputHandler.prototype.onIeKeyPress_ = function(e) {
  * @param {boolean=} opt_force If true the menu will be forced to update.
  */
 goog.ui.AutoComplete.InputHandler.prototype.update = function(opt_force) {
-  if (opt_force || this.activeElement_ && this.getValue() != this.lastValue_) {
+  if (this.activeElement_ &&
+      (opt_force || this.getValue() != this.lastValue_)) {
     if (opt_force || !this.rowJustSelected_) {
       var token = this.parseToken();
 
--- closure/goog/ui/labelinput.js
+++ closure/goog/ui/labelinput.js
@@ -123,6 +123,13 @@ goog.ui.LabelInput.prototype.decorateInternal = function(element) {
     this.label_ = element.getAttribute('label') || '';
   }
 
+  // Check if we're attaching to an element that already has focus.
+  var d = goog.dom.getOwnerDocument(element);
+  if (d && d.activeElement == element) {
+    this.hasFocus_ = true;
+    goog.dom.classes.remove(this.getElement(), this.LABEL_CLASS_NAME);
+  }
+
   if (goog.ui.LabelInput.SUPPORTS_PLACEHOLDER_) {
     this.getElement().placeholder = this.label_;
     return;
@@ -384,8 +391,8 @@ goog.ui.LabelInput.prototype.clear = function() {
 goog.ui.LabelInput.prototype.reset = function() {
   if (this.hasChanged()) {
     this.clear();
+    this.check_();
   }
-  this.check_();
 };
 
 
