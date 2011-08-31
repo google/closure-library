3f5eebb07e2886024d2cdc3d3ac4292f93e8e3b3
mschidlowsky@google.com <mschidlowsky@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-08-31 18:39:54 +0000

    Made checkbox correctly set aria label.
    R=nicksantos
    DELTA=41  (38 added, 0 deleted, 3 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=3182
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1266 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/ui/checkbox.js
+++ closure/goog/ui/checkbox.js
@@ -21,6 +21,8 @@
 goog.provide('goog.ui.Checkbox');
 goog.provide('goog.ui.Checkbox.State');
 
+goog.require('goog.dom.a11y');
+goog.require('goog.dom.a11y.State');
 goog.require('goog.events.EventType');
 goog.require('goog.events.KeyCodes');
 goog.require('goog.ui.CheckboxRenderer');
@@ -195,6 +197,15 @@ goog.ui.Checkbox.prototype.enterDocument = function() {
     // Checkbox needs to explicitly listen for click event.
     handler.listen(this.getElement(),
         goog.events.EventType.CLICK, this.handleClickOrSpace_);
+  }
+
+  // Set aria label.
+  if (this.label_) {
+    if (!this.label_.id) {
+      this.label_.id = this.makeId('lbl');
+    }
+    goog.dom.a11y.setState(this.getElement(),
+        goog.dom.a11y.State.LABELLEDBY, this.label_.id);
   }
 };
 
