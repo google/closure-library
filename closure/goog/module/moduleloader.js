b65389c19a8a2a57a19996f8a9174890e59ab2cd
ebixon@google.com <ebixon@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2011-11-23 08:10:58 +0000

    Refactor jsonp and moduleloader, introducing a new utility to help loading any js file.
    R=nicksay,pupius,bolinfest
    DELTA=841  (669 added, 129 deleted, 43 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=3869
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1421 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/module/moduleloader.js
+++ closure/goog/module/moduleloader.js
@@ -37,7 +37,7 @@ goog.require('goog.events.EventHandler');
 goog.require('goog.module.BaseModuleLoader');
 goog.require('goog.net.BulkLoader');
 goog.require('goog.net.EventType');
-goog.require('goog.userAgent');
+goog.require('goog.net.jsloader');
 
 
 
@@ -55,13 +55,6 @@ goog.module.ModuleLoader = function() {
    * @private
    */
   this.eventHandler_ = new goog.events.EventHandler(this);
-
-  /**
-   * Scripts waiting to be loaded. Only used in debug mode.
-   * @type {Array.<string>}
-   * @private
-   */
-  this.scriptsToLoadDebugMode_ = [];
 };
 goog.inherits(goog.module.ModuleLoader, goog.module.BaseModuleLoader);
 
@@ -86,7 +79,11 @@ goog.module.ModuleLoader.prototype.loadModulesInternal = function(
   this.logger.info('loadModules ids:' + ids + ' uris:' + uris);
 
   if (this.getDebugMode()) {
-    this.loadModulesInDebugMode_(uris);
+    // In debug mode use <script> tags rather than XHRs to load the files.
+    // This makes it possible to debug and inspect stack traces more easily.
+    // It's also possible to use it to load JavaScript files that are hosted on
+    // another domain.
+    goog.net.jsloader.loadMany(uris);
   } else {
     var bulkLoader = new goog.net.BulkLoader(uris);
     var eventHandler = this.eventHandler_;
@@ -121,78 +118,6 @@ goog.module.ModuleLoader.prototype.createScriptElement_ = function(uri) {
   scriptEl.src = uri;
   scriptEl.type = 'text/javascript';
   return scriptEl;
-};
-
-
-/**
- * Loads and evaluates the JavaScript files at the specified URIs, in order.
- * This method uses &lt;script> tags rather than XHRs to load the files. This
- * makes it possible to debug and inspect stack traces more easily. It's also
- * possible to use it to load JavaScript files that are hosted on another
- * domain.
- * @param {Array.<string>} uris The URIs to load.
- * @private
- */
-goog.module.ModuleLoader.prototype.loadModulesInDebugMode_ = function(uris) {
-  // Loading the scripts in serial introduces asynchronosity into the module
-  // loading flow. In production mode, we can guarantee that scripts will
-  // be loaded synchronously one after another, but that doesn't hold true
-  // in debug mode. So in debug mode, there are race conditions where
-  // module A can kick off the load sequence for module B, even though
-  // module A's scripts haven't all been loaded yet.
-  //
-  // To work around this issue, all module loads share a queue.
-  if (!uris.length) {
-    return;
-  }
-
-  // If IE6 or 7 is still parsing the document, appending to the document
-  // element will lead to an operation aborted alert. If possible, try to find
-  // a head element to append on to make this a bit more idiot-proof.
-  var scriptParent = document.getElementsByTagName('head')[0] ||
-      document.documentElement;
-  if (goog.userAgent.GECKO && !goog.userAgent.isVersion(2)) {
-    // For <script> tags that are loaded in this manner, Gecko 1.9 and earlier
-    // ensures that tag order is consistent with evaluation order.
-    // Unfortunately, other browsers do not make that guarantee. So the other
-    // browsers need a slower and more complex implementation.
-    for (var i = 0; i < uris.length; i++) {
-      var scriptEl = this.createScriptElement_(uris[i]);
-      scriptParent.appendChild(scriptEl);
-    }
-  } else {
-    var isAnotherModuleLoading = this.scriptsToLoadDebugMode_.length;
-    goog.array.extend(this.scriptsToLoadDebugMode_, uris);
-    if (isAnotherModuleLoading) {
-      // The module loader is still loading some other module's code.
-      // In order to prevent the race condition noted above, we just add
-      // these URIs to the end of the other module's queue and return.
-      return;
-    }
-
-    var moduleLoader = this;
-    uris = this.scriptsToLoadDebugMode_;
-    var popAndLoadNextScript = function() {
-      var uri = uris.shift();
-      var scriptEl = moduleLoader.createScriptElement_(uri);
-      if (uris.length) {
-        if (goog.userAgent.IE) {
-          scriptEl.onreadystatechange = function() {
-            if (!this.readyState || this.readyState == 'loaded' ||
-                this.readyState == 'complete') {
-              // Guard against redundant state changes.
-              scriptEl.onreadystatechange = goog.nullFunction;
-              popAndLoadNextScript();
-            }
-          };
-        } else {
-          scriptEl.onload = popAndLoadNextScript;
-        }
-      }
-      scriptParent.appendChild(scriptEl);
-    };
-    popAndLoadNextScript();
-  }
 };
 
 
