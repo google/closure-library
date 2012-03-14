a0ea9f0cfc8f338d73cc40be78699c823fba6987
chrishenry@google.com <chrishenry@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-03-14 19:03:55 +0000

    Adds goog.labs.object.is that implements harmony:egal proposal.
    R=nnaze,nicksantos
    DELTA=254  (182 added, 17 deleted, 55 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=4371
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1654 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/deps.js
+++ closure/goog/deps.js
@@ -315,6 +315,7 @@ goog.addDependency('json/json.js', ['goog.json', 'goog.json.Serializer'], []);
 goog.addDependency('labs/async/result/result.js', ['goog.labs.async.Result'], []);
 goog.addDependency('labs/async/result/resultbase.js', ['goog.labs.async.ResultBase', 'goog.labs.async.ResultBase.StateError'], ['goog.debug.Error', 'goog.labs.async.Result']);
 goog.addDependency('labs/net/xhr.js', ['goog.labs.net.xhr', 'goog.labs.net.xhr.Error', 'goog.labs.net.xhr.HttpError', 'goog.labs.net.xhr.TimeoutError'], ['goog.async.Deferred', 'goog.debug.Error', 'goog.json', 'goog.net.HttpStatus', 'goog.net.XmlHttp', 'goog.string', 'goog.uri.utils']);
+goog.addDependency('labs/object/object.js', ['goog.labs.object'], []);
 goog.addDependency('labs/observe/notice.js', ['goog.labs.observe.Notice'], []);
 goog.addDependency('labs/observe/noticetype.js', ['goog.labs.observe.NoticeType'], []);
 goog.addDependency('labs/observe/observable.js', ['goog.labs.observe.Observable'], ['goog.disposable.IDisposable']);
@@ -548,7 +549,7 @@ goog.addDependency('testing/net/xhrio.js', ['goog.testing.net.XhrIo'], ['goog.ar
 goog.addDependency('testing/net/xhriopool.js', ['goog.testing.net.XhrIoPool'], ['goog.net.XhrIoPool', 'goog.testing.net.XhrIo']);
 goog.addDependency('testing/objectpropertystring.js', ['goog.testing.ObjectPropertyString'], []);
 goog.addDependency('testing/performancetable.js', ['goog.testing.PerformanceTable'], ['goog.dom', 'goog.testing.PerformanceTimer']);
-goog.addDependency('testing/performancetimer.js', ['goog.testing.PerformanceTimer'], ['goog.array', 'goog.math']);
+goog.addDependency('testing/performancetimer.js', ['goog.testing.PerformanceTimer', 'goog.testing.PerformanceTimer.Task'], ['goog.array', 'goog.math']);
 goog.addDependency('testing/propertyreplacer.js', ['goog.testing.PropertyReplacer'], ['goog.userAgent']);
 goog.addDependency('testing/pseudorandom.js', ['goog.testing.PseudoRandom'], ['goog.Disposable']);
 goog.addDependency('testing/recordfunction.js', ['goog.testing.FunctionCall', 'goog.testing.recordConstructor', 'goog.testing.recordFunction'], []);
