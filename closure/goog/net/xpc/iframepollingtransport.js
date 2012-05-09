71896e99d38fa09aeb58826a2f2dd66a7e655b0c
dbk@google.com <dbk@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-05-09 23:40:22 +0000

    Previously, in iframe polling transport, if the inner frame didn't find the outer frame's sender iframes immediately on startup, it would begin its reconnection sequence.  That reconnection sequence doesn't seem to work anymore (at least in IE7) possibly due to changes in some browser behavior it relied on.  It's perhaps assuming that setting .name on the iframe element will also control what you get if you do .frames['id'].name?  Not sure, but in any case it can't seem to find a reconnection frame when one exists.  A change in startup timing was causing reconnection to get triggered where it wasn't before.  This change therefore allows the inner frame to poll a limited number of times for the outer frame's sender frames before it starts the reconnection sequence.  If that sequence itself needs repair, that's for another day.
    R=nicksantos
    DELTA=39  (27 added, 0 deleted, 12 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=4729
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@1841 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/net/xpc/iframepollingtransport.js
+++ closure/goog/net/xpc/iframepollingtransport.js
@@ -80,12 +80,22 @@ goog.inherits(goog.net.xpc.IframePollingTransport, goog.net.xpc.Transport);
 
 
 /**
+ * The number of times the inner frame will check for evidence of the outer
+ * frame before it tries its reconnection sequence.  These occur at 100ms
+ * intervals, making this an effective max waiting period of 500ms.
+ * @type {number}
+ * @private
+ */
+goog.net.xpc.IframePollingTransport.prototype.pollsBeforeReconnect_ = 5;
+
+
+/**
  * The transport type.
  * @type {number}
  * @protected
  */
 goog.net.xpc.IframePollingTransport.prototype.transportType =
-  goog.net.xpc.TransportTypes.IFRAME_POLLING;
+    goog.net.xpc.TransportTypes.IFRAME_POLLING;
 
 
 /**
@@ -110,6 +120,14 @@ goog.net.xpc.IframePollingTransport.prototype.waitForAck_ = false;
  * @private
  */
 goog.net.xpc.IframePollingTransport.prototype.initialized_ = false;
+
+
+/**
+ * Reconnection iframe created by inner peer.
+ * @type {Element}
+ * @private
+ */
+goog.net.xpc.IframePollingTransport.prototype.reconnectFrame_ = null;
 
 
 /**
@@ -211,16 +229,25 @@ goog.net.xpc.IframePollingTransport.prototype.constructSenderFrame_ =
  * of bfcached iframes.
  * @private
  */
-goog.net.xpc.IframePollingTransport.prototype.innerPeerReconnect_ = function() {
-  goog.net.xpc.logger.finest('innerPeerReconnect called');
+goog.net.xpc.IframePollingTransport.prototype.maybeInnerPeerReconnect_ =
+    function() {
+  // Reconnection has been found to not function on some browsers (eg IE7), so
+  // it's important that the mechanism only be triggered as a last resort.  As
+  // such, we poll a number of times to find the outer iframe before triggering
+  // it.
+  if (this.reconnectFrame_ || this.pollsBeforeReconnect_-- > 0) {
+    return;
+  }
+
+  goog.net.xpc.logger.finest('Inner peer reconnect triggered.');
   this.channel_.name = goog.net.xpc.getRandomString(10);
   goog.net.xpc.logger.finest('switching channels: ' + this.channel_.name);
   this.deconstructSenderFrames_();
   this.initialized_ = false;
   // Communicate new channel name to outer peer.
   this.reconnectFrame_ = this.constructSenderFrame_(
-    goog.net.xpc.IframePollingTransport.IFRAME_PREFIX +
-      '_reconnect_' + this.channel_.name);
+      goog.net.xpc.IframePollingTransport.IFRAME_PREFIX +
+          '_reconnect_' + this.channel_.name);
 };
 
 
@@ -296,13 +323,13 @@ goog.net.xpc.IframePollingTransport.prototype.checkForeignFramesReady_ =
         this.isRcvFrameReady_(this.getAckFrameName_()))) {
     goog.net.xpc.logger.finest('foreign frames not (yet) present');
 
-    if (this.channel_.getRole() == goog.net.xpc.CrossPageChannelRole.INNER &&
-        !this.reconnectFrame_) {
-      // The inner peer should always have its receiving frames ready.
-      // It is safe to assume the channel name has fallen out of sync
-      // (which happens with bfcached frames). Create "reconnect" frames,
-      // which the outer peer will find, and use to resync the channel names.
-      this.innerPeerReconnect_();
+    if (this.channel_.getRole() == goog.net.xpc.CrossPageChannelRole.INNER) {
+      // The outer peer might need a short time to get its frames ready, as
+      // CrossPageChannel prevents them from getting created until the inner
+      // peer's frame has thrown its loaded event.  This method is a noop for
+      // the first few times it's called, and then allows the reconnection
+      // sequence to begin.
+      this.maybeInnerPeerReconnect_();
     } else if (this.channel_.getRole() ==
                goog.net.xpc.CrossPageChannelRole.OUTER) {
       // The inner peer is either not loaded yet, or the receiving
