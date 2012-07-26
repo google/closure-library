e1682f263dd2eda79cdb5fcfd03f27e038158db0
ruilopes@google.com <ruilopes@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-07-26 23:09:47 +0000

    Add a cross-browser function to get data-* as a string map.
    R=nnaze,chrishenry
    DELTA=55  (52 added, 0 deleted, 3 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5244
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2061 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/dom/dataset.js
+++ closure/goog/dom/dataset.js
@@ -25,6 +25,16 @@ goog.require('goog.string');
 
 
 /**
+ * The DOM attribute name prefix that must be present for it to be considered
+ * for a dataset.
+ * @type {string}
+ * @const
+ * @private
+ */
+goog.dom.dataset.PREFIX_ = 'data-';
+
+
+/**
  * Sets a custom data attribute on an element. The key should be
  * in camelCase format (e.g "keyName" for the "data-key-name" attribute).
  * @param {Element} element DOM node to set the custom data attribute on.
@@ -66,7 +76,8 @@ goog.dom.dataset.remove = function(element, key) {
   if (element.dataset) {
     delete element.dataset[key];
   } else {
-    element.removeAttribute('data-' + goog.string.toSelectorCase(key));
+    element.removeAttribute(goog.dom.dataset.PREFIX_ +
+                            goog.string.toSelectorCase(key));
   }
 };
 
@@ -83,8 +94,36 @@ goog.dom.dataset.has = function(element, key) {
   if (element.dataset) {
     return key in element.dataset;
   } else if (element.hasAttribute) {
-    return element.hasAttribute('data-' + goog.string.toSelectorCase(key));
+    return element.hasAttribute(goog.dom.dataset.PREFIX_ +
+                                goog.string.toSelectorCase(key));
   } else {
-    return !!(element.getAttribute('data-' + goog.string.toSelectorCase(key)));
+    return !!(element.getAttribute(goog.dom.dataset.PREFIX_ +
+                                   goog.string.toSelectorCase(key)));
+  }
+};
+
+
+/**
+ * Gets all custom data attributes as a string map.
+ * @param {!Element} element DOM node to get the data attributes from.
+ * @return {!Object} The string map containing data attributes and their
+ *     respective values.
+ */
+goog.dom.dataset.getAll = function(element) {
+  if (element.dataset) {
+    return element.dataset;
+  } else {
+    var dataset = {};
+    var attributes = element.attributes;
+    for (var i = 0; i < attributes.length; ++i) {
+      var attribute = attributes[i];
+      if (goog.string.startsWith(attribute.name,
+                                 goog.dom.dataset.PREFIX_)) {
+        // We use substr(5), since it's faster than replacing 'data-' with ''.
+        var key = goog.string.toCamelCase(attribute.name.substr(5));
+        dataset[key] = attribute.value;
+      }
+    }
+    return dataset;
   }
 };
