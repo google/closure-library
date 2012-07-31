1cea98d1589b82a4b60f1f04272624aee3c7ba78
asweintraub@google.com <asweintraub@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-07-31 21:29:59 +0000

    Adds a new method to closure to easily create a map/object from an array.
    R=wgrose,rnaratawadekar,nicksantos
    DELTA=46  (46 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5285
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2073 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/array/array.js
+++ closure/goog/array/array.js
@@ -1266,6 +1266,31 @@ goog.array.bucket = function(array, sorter) {
 
 
 /**
+ * Creates a new object built from the provided array and the key-generation
+ * function.
+ * @param {Array.<T>|goog.array.ArrayLike} arr Array or array like object over
+ *     which to iterate whose elements will be the values in the new object.
+ * @param {?function(this:S, T, number, ?) : string} keyFunc The function to
+ *     call for every element. This function takes 3 arguments (the element, the
+ *     index and the array) and should return a string that will be used as the
+ *     key for the element in the new object. If the function returns the same
+ *     key for more than one element, the value for that key is
+ *     implementation-defined.
+ * @param {S=} opt_obj  The object to be used as the value of 'this'
+ *     within keyFunc.
+ * @return {!Object.<T>} The new object.
+ * @template T,S
+ */
+goog.array.toObject = function(arr, keyFunc, opt_obj) {
+  var ret = {};
+  goog.array.forEach(arr, function(element, index) {
+    ret[keyFunc.call(opt_obj, element, index, arr)] = element;
+  });
+  return ret;
+};
+
+
+/**
  * Returns an array consisting of the given value repeated N times.
  *
  * @param {*} value The value to repeat.
