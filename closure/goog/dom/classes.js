00425e1689a3a07b608598c8481d17e7fa38bfb6
nnaze@google.com <nnaze@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-08-02 23:04:50 +0000

    Note that goog.dom.classes are not meant for use with non-string className properties as found in SVG elements.
    Fixes issue 471.
    
    R=chrishenry
    DELTA=3  (3 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5311
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2081 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/dom/classes.js
+++ closure/goog/dom/classes.js
@@ -15,6 +15,9 @@
 /**
  * @fileoverview Utilities for adding, removing and setting classes.
  *
+ * Note: these utilities are meant to operate on HTMLElements and
+ * will not work on elements with differing interfaces (such as SVGElements).
+ *
  */
 
 
