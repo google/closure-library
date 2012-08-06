f1641a46113c0ee82ef0e1674d3ed0fe98b88055
ace@google.com <ace@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-08-06 21:11:47 +0000

    Removing error re-throw from pump_
    I can't find any cases where returning from pump_ after an error causes
    problems.
    
    R=yongrim,agrieve
    DELTA=17  (0 added, 16 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5323
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2088 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/testing/asynctestcase.js
+++ closure/goog/testing/asynctestcase.js
@@ -709,14 +709,6 @@ goog.testing.AsyncTestCase.prototype.pump_ = function(opt_doFirst) {
   if (!this.returnWillPump_) {
     this.setBatchTime(this.now());
     this.returnWillPump_ = true;
-    // If we catch an exception in the step, we don't want to return control
-    // to our caller since there may be non-testcase code in our call stack.
-    // Eg)
-    //   asyncCallback() { fail(1); fail(2); }
-    //                       V
-    //   - ...
-    //   - pump_();
-    // We don't want fail(2) to ever be called.
     var topFuncResult = {};
 
     if (opt_doFirst) {
@@ -750,14 +742,6 @@ goog.testing.AsyncTestCase.prototype.pump_ = function(opt_doFirst) {
       }
     }
     this.returnWillPump_ = false;
-    // See note at top of this function.
-    if (topFuncResult.controlBreakingExceptionThrown) {
-      this.numControlExceptionsExpected_ += 1;
-      this.dbgLog_('pump: numControlExceptionsExpected_ = ' +
-          this.numControlExceptionsExpected_ + ' and throwing exception.');
-      throw new goog.testing.AsyncTestCase.
-          ControlBreakingException(topFuncResult.message);
-    }
   } else if (opt_doFirst) {
     opt_doFirst.call(this);
   }
