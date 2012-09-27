873cb8ae86f5bb8864d03f832dc6fe534f11e656
tomzli@google.com <tomzli@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-09-27 18:39:06 +0000

    Automated g4 rollback
    *** Reason for rollback ***
    
    It broke a test
    
    *** Original change description ***
    
    Let the caller decide on the final state when animation is in progress
    and setExpanded is called
    
    R=eae
    DELTA=38  (1 added, 37 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5566
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2190 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/ui/animatedzippy.js
+++ closure/goog/ui/animatedzippy.js
@@ -125,6 +125,7 @@ goog.ui.AnimatedZippy.prototype.setExpanded = function(expanded) {
   // Stop active animation (if any) and determine starting height.
   var startH = 0;
   if (this.anim_) {
+    expanded = this.isExpanded();
     goog.events.removeAll(this.anim_);
     this.anim_.stop(false);
 
