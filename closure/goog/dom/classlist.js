c14e9775c90c37ff814ce2ee4063c810e6e03263
jeffschiller@google.com <jeffschiller@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-10-26 17:38:39 +0000

    Introduce alternative to goog.dom.classes:  goog.dom.classlist that is ideally a thin wrapper around Element.classList (DOMTokenList)
    R=gboyer,arv
    DELTA=401  (401 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5723
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2253 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

---
+++ closure/goog/dom/classlist.js
@@ -0,0 +1,189 @@
+// Copyright 2012 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Utilities for detecting, adding and removing classes.  Prefer
+ * this over goog.dom.classes for new code since it attempts to use classList
+ * (DOMTokenList: http://dom.spec.whatwg.org/#domtokenlist) which is faster
+ * and requires less code.
+ *
+ * Note: these utilities are meant to operate on HTMLElements and
+ * will not work on elements with differing interfaces (such as SVGElements).
+ */
+
+
+goog.provide('goog.dom.classlist');
+
+goog.require('goog.array');
+goog.require('goog.asserts');
+
+
+
+/**
+ * Override this define at build-time if you know your target supports it.
+ * @define {boolean} Whether to use the classList property (DOMTokenList).
+ */
+goog.dom.classlist.ALWAYS_USE_DOM_TOKEN_LIST = false;
+
+
+/**
+ * Enables use of the native DOMTokenList methods.  See the spec at
+ * {@link http://dom.spec.whatwg.org/#domtokenlist}.
+ * @type {boolean}
+ * @private
+ */
+goog.dom.classlist.NATIVE_DOM_TOKEN_LIST_ =
+    goog.dom.classlist.ALWAYS_USE_DOM_TOKEN_LIST ||
+    typeof goog.global.DOMTokenList != 'undefined';
+
+
+/**
+ * Gets an array-like object of class names on an element.
+ * @param {Element} element DOM node to get the classes of.
+ * @return {!goog.array.ArrayLike} Class names on {@code element}.
+ */
+goog.dom.classlist.get = goog.dom.classlist.NATIVE_DOM_TOKEN_LIST_ ?
+    function(element) {
+      return element.classList;
+    } :
+    function(element) {
+      var className = element.className;
+      // Some types of elements don't have a className in IE (e.g. iframes).
+      // Furthermore, in Firefox, className is not a string when the element is
+      // an SVG element.
+      return goog.isString(className) && className.match(/\S+/g) || [];
+    };
+
+
+/**
+ * Sets the entire class name of an element.
+ * @param {Element} element DOM node to set class of.
+ * @param {string} className Class name(s) to apply to element.
+ */
+goog.dom.classlist.set = function(element, className) {
+  element.className = className;
+};
+
+
+/**
+ * Returns true if an element has a class.  This method may throw a DOM
+ * exception for an invalid or empty class name if DOMTokenList is used.
+ * @param {Element} element DOM node to test.
+ * @param {string} className Class name to test for.
+ * @return {boolean} Whether element has the class.
+ */
+goog.dom.classlist.contains = goog.dom.classlist.NATIVE_DOM_TOKEN_LIST_ ?
+    function(element, className) {
+      goog.asserts.assert(!!element.classList);
+      return element.classList.contains(className);
+    } :
+    function(element, className) {
+      return goog.array.contains(goog.dom.classlist.get(element), className);
+    };
+
+
+/**
+ * Adds a class to an element.  Does not add multiples of class names.  This
+ * method may throw a DOM exception for an invalid or empty class name if
+ * DOMTokenList is used.
+ * @param {Element} element DOM node to add class to.
+ * @param {string} className Class name to add.
+ */
+goog.dom.classlist.add = goog.dom.classlist.NATIVE_DOM_TOKEN_LIST_ ?
+    function(element, className) {
+      element.classList.add(className);
+    } :
+    function(element, className) {
+      if (!goog.dom.classlist.contains(element, className)) {
+        // Ensure we add a space if this is not the first class name added.
+        element.className += element.className.length > 0 ?
+            (' ' + className) : className;
+      }
+    };
+
+
+/**
+ * Removes a class from an element.  This method may throw a DOM exception
+ * for an invalid or empty class name if DOMTokenList is used.
+ * @param {Element} element DOM node to remove class from.
+ * @param {string} className Class name to remove.
+ */
+goog.dom.classlist.remove = goog.dom.classlist.NATIVE_DOM_TOKEN_LIST_ ?
+    function(element, className) {
+      element.classList.remove(className);
+    } :
+    function(element, className) {
+      if (goog.dom.classlist.contains(element, className)) {
+        // Filter out the class name.
+        element.className = goog.array.filter(
+            goog.dom.classlist.get(element),
+            function(c) {
+              return c != className;
+            }).join(' ');
+      }
+    };
+
+
+/**
+ * Adds or removes a class depending on the enabled argument.  This method
+ * may throw a DOM exception for an invalid or empty class name if DOMTokenList
+ * is used.
+ * @param {Element} element DOM node to add or remove the class on.
+ * @param {string} className Class name to add or remove.
+ * @param {boolean} enabled Whether to add or remove the class (true adds,
+ *     false removes).
+ */
+goog.dom.classlist.enable = function(element, className, enabled) {
+  if (enabled) {
+    goog.dom.classlist.add(element, className);
+  } else {
+    goog.dom.classlist.remove(element, className);
+  }
+};
+
+
+/**
+ * Switches a class on an element from one to another without disturbing other
+ * classes. If the fromClass isn't removed, the toClass won't be added.  This
+ * method may throw a DOM exception if the class names are empty or invalid.
+ * @param {Element} element DOM node to swap classes on.
+ * @param {string} fromClass Class to remove.
+ * @param {string} toClass Class to add.
+ * @return {boolean} Whether classes were switched.
+ */
+goog.dom.classlist.swap = function(element, fromClass, toClass) {
+  if (goog.dom.classlist.contains(element, fromClass)) {
+    goog.dom.classlist.enable(element, fromClass, false);
+    goog.dom.classlist.enable(element, toClass, true);
+    return true;
+  }
+  return false;
+};
+
+
+/**
+ * Removes a class if an element has it, and adds it the element doesn't have
+ * it.  Won't affect other classes on the node.  This method may throw a DOM
+ * exception if the class name is empty or invalid.
+ * @param {Element} element DOM node to toggle class on.
+ * @param {string} className Class to toggle.
+ * @return {boolean} True if class was added, false if it was removed
+ *     (in other words, whether element has the class after this function has
+ *     been called).
+ */
+goog.dom.classlist.toggle = function(element, className) {
+  var add = !goog.dom.classlist.contains(element, className);
+  goog.dom.classlist.enable(element, className, add);
+  return add;
+};
