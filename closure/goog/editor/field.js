08be2008691ea4173d3a9a9641519bf0524d4bfc
nkerem@google.com <nkerem@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-11-08 15:30:41 +0000

    Implement a Google-specific variant of the First Strong algorithm in the closure editor. The Frist Strong algorithm is a heuristic used to automatically set paragraph direction depending on the paragraph's content.
    R=marcosalmeida
    DELTA=1067  (1066 added, 0 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5787
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2284 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/editor/field.js
+++ closure/goog/editor/field.js
@@ -1013,10 +1013,7 @@ goog.editor.Field.MUTATION_EVENTS_GECKO = [
  * @protected
  */
 goog.editor.Field.prototype.setupMutationEventHandlersGecko = function() {
-  // Always use DOMSubtreeModified on Gecko when not using an iframe so that
-  // DOM mutations outside the Field do not trigger handleMutationEventGecko_.
-  if (goog.editor.BrowserFeature.HAS_DOM_SUBTREE_MODIFIED_EVENT ||
-      !this.usesIframe()) {
+  if (goog.editor.BrowserFeature.HAS_DOM_SUBTREE_MODIFIED_EVENT) {
     this.eventRegister.listen(this.getElement(), 'DOMSubtreeModified',
         this.handleMutationEventGecko_);
   } else {
--- closure/goog/editor/plugins/removeformatting.js
+++ closure/goog/editor/plugins/removeformatting.js
@@ -185,16 +185,9 @@ goog.editor.plugins.RemoveFormatting.prototype.removeFormatting_ = function() {
  * @return {Node} The table, or null if one was not found.
  * @private
  */
-goog.editor.plugins.RemoveFormatting.prototype.getTableAncestor_ = function(
-    nodeToCheck) {
-  var fieldElement = this.getFieldObject().getElement();
-  while (nodeToCheck && nodeToCheck != fieldElement) {
-    if (nodeToCheck.tagName == goog.dom.TagName.TABLE) {
-      return nodeToCheck;
-    }
-    nodeToCheck = nodeToCheck.parentNode;
-  }
-  return null;
+goog.editor.plugins.RemoveFormatting.getTableAncestor_ = function(nodeToCheck) {
+  return goog.dom.getAncestor(nodeToCheck,
+      function(node) { return node.tagName == goog.dom.TagName.TABLE; }, true);
 };
 
 
@@ -502,8 +495,10 @@ goog.editor.plugins.RemoveFormatting.prototype.convertSelectedHtmlText_ =
     var expandedRange = goog.editor.range.expand(range,
         this.getFieldObject().getElement());
 
-    var startInTable = this.getTableAncestor_(expandedRange.getStartNode());
-    var endInTable = this.getTableAncestor_(expandedRange.getEndNode());
+    var startInTable = goog.editor.plugins.RemoveFormatting.getTableAncestor_(
+        expandedRange.getStartNode());
+    var endInTable = goog.editor.plugins.RemoveFormatting.getTableAncestor_(
+        expandedRange.getEndNode());
 
     if (startInTable || endInTable) {
       if (startInTable == endInTable) {
