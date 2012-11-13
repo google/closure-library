17cc90ac9ac2f6e820fe5a1c0843984ae5cf3f30
jeffschiller@google.com <jeffschiller@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-11-13 06:31:47 +0000

    Change LabelInput to use goog.dom.classlist again
    R=arv
    DELTA=16  (1 added, 0 deleted, 15 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5819
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2297 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/ui/labelinput.js
+++ closure/goog/ui/labelinput.js
@@ -42,7 +42,7 @@ goog.require('goog.Timer');
 goog.require('goog.dom');
 goog.require('goog.dom.a11y');
 goog.require('goog.dom.a11y.State');
-goog.require('goog.dom.classes');
+goog.require('goog.dom.classlist');
 goog.require('goog.events.EventHandler');
 goog.require('goog.events.EventType');
 goog.require('goog.ui.Component');
@@ -130,7 +130,7 @@ goog.ui.LabelInput.prototype.decorateInternal = function(element) {
   if (goog.dom.getActiveElement(goog.dom.getOwnerDocument(element)) ==
       element) {
     this.hasFocus_ = true;
-    goog.dom.classes.remove(this.getElement(), this.LABEL_CLASS_NAME);
+    goog.dom.classlist.remove(this.getElement(), this.LABEL_CLASS_NAME);
   }
 
   if (goog.ui.LabelInput.SUPPORTS_PLACEHOLDER_) {
@@ -247,7 +247,7 @@ goog.ui.LabelInput.prototype.LABEL_CLASS_NAME =
  */
 goog.ui.LabelInput.prototype.handleFocus_ = function(e) {
   this.hasFocus_ = true;
-  goog.dom.classes.remove(this.getElement(), this.LABEL_CLASS_NAME);
+  goog.dom.classlist.remove(this.getElement(), this.LABEL_CLASS_NAME);
   if (goog.ui.LabelInput.SUPPORTS_PLACEHOLDER_) {
     return;
   }
@@ -475,7 +475,7 @@ goog.ui.LabelInput.prototype.check_ = function() {
 
   if (!this.hasChanged()) {
     if (!this.inFocusAndSelect_ && !this.hasFocus_) {
-      goog.dom.classes.add(this.getElement(), this.LABEL_CLASS_NAME);
+      goog.dom.classlist.add(this.getElement(), this.LABEL_CLASS_NAME);
     }
 
     // Allow browser to catchup with CSS changes before restoring the label.
@@ -483,7 +483,7 @@ goog.ui.LabelInput.prototype.check_ = function() {
       goog.Timer.callOnce(this.restoreLabel_, 10, this);
     }
   } else {
-    goog.dom.classes.remove(this.getElement(), this.LABEL_CLASS_NAME);
+    goog.dom.classlist.remove(this.getElement(), this.LABEL_CLASS_NAME);
   }
 };
 
@@ -527,7 +527,7 @@ goog.ui.LabelInput.prototype.focusAndSelect = function() {
  */
 goog.ui.LabelInput.prototype.setEnabled = function(enabled) {
   this.getElement().disabled = !enabled;
-  goog.dom.classes.enable(this.getElement(),
+  goog.dom.classlist.enable(this.getElement(),
       goog.getCssName(this.LABEL_CLASS_NAME, 'disabled'), !enabled);
 };
 
