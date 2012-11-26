da6ba635cb942ea818e57bad830950942c40a761
kpy@google.com <kpy@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-11-26 19:53:42 +0000

    Fix typo / clarify documentation for goog.Uri.prototype.resolve.
    R=chrishenry
    DELTA=6  (1 added, 0 deleted, 5 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5886
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2327 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/uri/uri.js
+++ closure/goog/uri/uri.js
@@ -249,19 +249,20 @@ goog.Uri.prototype.toString = function() {
 
 
 /**
- * Resolves a relative url string to a this base uri.
+ * Resolves the given relative URI (a goog.Uri object), using the URI
+ * represented by this instance as the base URI.
  *
- * There are several kinds of relative urls:<br>
+ * There are several kinds of relative URIs:<br>
  * 1. foo - replaces the last part of the path, the whole query and fragment<br>
  * 2. /foo - replaces the the path, the query and fragment<br>
  * 3. //foo - replaces everything from the domain on.  foo is a domain name<br>
  * 4. ?foo - replace the query and fragment<br>
  * 5. #foo - replace the fragment only
  *
- * Additionally, if relative url has a non-empty path, all ".." and "."
+ * Additionally, if relative URI has a non-empty path, all ".." and "."
  * segments will be resolved, as described in RFC 3986.
  *
- * @param {goog.Uri} relativeUri The relative url to resolve.
+ * @param {goog.Uri} relativeUri The relative URI to resolve.
  * @return {!goog.Uri} The resolved URI.
  */
 goog.Uri.prototype.resolve = function(relativeUri) {
