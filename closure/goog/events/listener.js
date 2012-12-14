ae2e29ed683f906082e058bbfbcbc55e49c70584
pstemari@google.com <pstemari@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2012-12-14 00:27:00 +0000

    Automated g4 rollback
    *** Reason for rollback ***
    
    
    *** Original change description ***
    
    Add goog.events.Listenable interface.
    
    R=chrishenry,nicksantos,nnaze
    DELTA=207 (4 added, 201 deleted, 2 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5978
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2378 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/events/listener.js
+++ closure/goog/events/listener.js
@@ -17,15 +17,16 @@
  * @see ../demos/events.html
  */
 
-goog.provide('goog.events.Listener');
 
-goog.require('goog.events.Listenable');
+/**
+ * Namespace for events
+ */
+goog.provide('goog.events.Listener');
 
 
 
 /**
  * Simple class that stores information about a listener
- * @implements {goog.events.Listenable.Key}
  * @constructor
  */
 goog.events.Listener = function() {
