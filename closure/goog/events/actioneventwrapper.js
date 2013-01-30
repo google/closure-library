87987ad8f5ce0fd1b429571cf4cc49fd5812bee3
jcaballero@google.com <jcaballero@google.com@0b95b8e8-c90f-11de-9d4f-f947ee5921c8>
2013-01-30 17:48:13 +0000

    Add capture support for ActionEventWrapper.listen and .unlisten.
    R=eae
    DELTA=54 (49 added, 0 deleted, 5 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6223
    
    
    git-svn-id: https://closure-library.googlecode.com/svn/trunk@2486 0b95b8e8-c90f-11de-9d4f-f947ee5921c8

--- closure/goog/events/actioneventwrapper.js
+++ closure/goog/events/actioneventwrapper.js
@@ -94,11 +94,11 @@ goog.events.ActionEventWrapper_.prototype.listen = function(target, listener,
   if (opt_eventHandler) {
     opt_eventHandler.listen(target,
         goog.events.ActionEventWrapper_.EVENT_TYPES_,
-        callback);
+        callback, opt_capt);
   } else {
     goog.events.listen(target,
         goog.events.ActionEventWrapper_.EVENT_TYPES_,
-        callback);
+        callback, opt_capt);
   }
 };
 
@@ -121,14 +121,15 @@ goog.events.ActionEventWrapper_.prototype.unlisten = function(target, listener,
     opt_capt, opt_scope, opt_eventHandler) {
   for (var type, j = 0; type = goog.events.ActionEventWrapper_.EVENT_TYPES_[j];
       j++) {
-    var listeners = goog.events.getListeners(target, type, false);
+    var listeners = goog.events.getListeners(target, type, !!opt_capt);
     for (var obj, i = 0; obj = listeners[i]; i++) {
       if (obj.listener.listener_ == listener &&
           obj.listener.scope_ == opt_scope) {
         if (opt_eventHandler) {
-          opt_eventHandler.unlisten(target, type, obj.listener);
+          opt_eventHandler.unlisten(target, type, obj.listener, opt_capt,
+              opt_scope);
         } else {
-          goog.events.unlisten(target, type, obj.listener);
+          goog.events.unlisten(target, type, obj.listener, opt_capt, opt_scope);
         }
         break;
       }
