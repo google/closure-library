9e017bd95f03a3a95fe8581de339982945235d9c
Nathan Naze <nnaze@google.com>
2013-02-22 12:57:33 -0500

    Add a backward compatible option to goog.debug.ErrorHandler to not wrap exceptions before re-throwing. This makes errors in the console of Chrome Dev Tools and other browsers show up like expected with the correct message, filename, line number and stack trace (welcome to 2013 :).
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=42929501

--- closure/goog/debug/errorhandler.js
+++ closure/goog/debug/errorhandler.js
@@ -53,6 +53,23 @@ goog.debug.ErrorHandler = function(handler) {
    * @private
    */
   this.errorHandlerFn_ = handler;
+
+  /**
+   * Whether errors should be wrapped in
+   * goog.debug.ErrorHandler.ProtectedFunctionError before rethrowing.
+   * @type {boolean}
+   * @private
+   */
+  this.wrapErrors_ = true;  // TODO(user) Change default.
+
+  /**
+   * Whether to add a prefix to all error messages. The prefix is
+   * goog.debug.ErrorHandler.ProtectedFunctionError.MESSAGE_PREFIX. This option
+   * only has an effect if this.wrapErrors_  is set to false.
+   * @type {boolean}
+   * @private
+   */
+  this.prefixErrorMessages_ = false;
 };
 goog.inherits(goog.debug.ErrorHandler, goog.Disposable);
 
@@ -164,6 +181,22 @@ goog.debug.ErrorHandler.prototype.getProtectedFunction = function(fn) {
       return fn.apply(this, arguments);
     } catch (e) {
       that.errorHandlerFn_(e);
+      if (!that.wrapErrors_) {
+        // Add the prefix to the existing message.
+        if (that.prefixErrorMessages_) {
+          if (typeof e === 'object') {
+            e.message =
+                goog.debug.ErrorHandler.ProtectedFunctionError.MESSAGE_PREFIX +
+                e.message;
+          } else {
+            e = goog.debug.ErrorHandler.ProtectedFunctionError.MESSAGE_PREFIX +
+                e;
+          }
+        }
+        // Re-throw original error. This is great for debugging as it makes
+        // browser JS dev consoles show the correct error and stack trace.
+        throw e;
+      }
       // Re-throw it since this may be expected by the caller.
       throw new goog.debug.ErrorHandler.ProtectedFunctionError(e);
     } finally {
@@ -224,6 +257,28 @@ goog.debug.ErrorHandler.prototype.protectWindowFunctionsHelper_ =
     }
   };
   win[fnName][this.getFunctionIndex_(false)] = originalFn;
+};
+
+
+/**
+ * Set whether to wrap errors that occur in protected functions in a
+ * goog.debug.ErrorHandler.ProtectedFunctionError.
+ * @param {boolean} wrapErrors Whether to wrap errors.
+ */
+goog.debug.ErrorHandler.prototype.setWrapErrors = function(wrapErrors) {
+  this.wrapErrors_ = wrapErrors;
+};
+
+
+/**
+ * Set whether to add a prefix to all error messages that occur in protected
+ * functions.
+ * @param {boolean} prefixErrorMessages Whether to add a prefix to error
+ *     messages.
+ */
+goog.debug.ErrorHandler.prototype.setPrefixErrorMessages =
+    function(prefixErrorMessages) {
+  this.prefixErrorMessages_ = prefixErrorMessages;
 };
 
 
