c7da2aca845ce8137c05d89d989939859972b9bb
Nathan Naze <nnaze@google.com>
2013-03-08 16:05:11 -0500

    Assert with an informative error message that every module that is requested for loading exists. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=42981630

--- closure/goog/module/modulemanager.js
+++ closure/goog/module/modulemanager.js
@@ -505,6 +505,7 @@ goog.module.ModuleManager.prototype.loadModulesOrEnqueueIfNotLoadedOrLoading_ =
   for (var i = 0; i < uniqueIds.length; i++) {
     var id = uniqueIds[i];
     var moduleInfo = this.getModuleInfo(id);
+    goog.asserts.assertObject(moduleInfo, 'Unknown module: ' + id);
     var d = new goog.async.Deferred();
     deferredMap[id] = d;
     if (moduleInfo.isLoaded()) {
