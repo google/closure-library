d25ca6307529288fecd119d5e75777992a14d247
Nathan Naze <nnaze@google.com>
2013-03-08 17:09:38 -0500

    Add coordinate helper functions to goog.math.Rect: getTopLeft, getCenter, and getBottomRight.
    Minor cleanups in the tests and comments.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=43472018

--- closure/goog/math/rect.js
+++ closure/goog/math/rect.js
@@ -16,7 +16,6 @@
  * @fileoverview A utility class for representing rectangles.
  */
 
-
 goog.provide('goog.math.Rect');
 
 goog.require('goog.math.Box');
@@ -34,35 +33,22 @@ goog.require('goog.math.Size');
  * @constructor
  */
 goog.math.Rect = function(x, y, w, h) {
-  /**
-   * Left
-   * @type {number}
-   */
+  /** @type {number} */
   this.left = x;
 
-  /**
-   * Top
-   * @type {number}
-   */
+  /** @type {number} */
   this.top = y;
 
-  /**
-   * Width
-   * @type {number}
-   */
+  /** @type {number} */
   this.width = w;
 
-  /**
-   * Height
-   * @type {number}
-   */
+  /** @type {number} */
   this.height = h;
 };
 
 
 /**
- * Returns a new copy of the rectangle.
- * @return {!goog.math.Rect} A clone of this Rectangle.
+ * @return {!goog.math.Rect} A new copy of this Rectangle.
  */
 goog.math.Rect.prototype.clone = function() {
   return new goog.math.Rect(this.left, this.top, this.width, this.height);
@@ -359,11 +345,39 @@ goog.math.Rect.prototype.distance = function(point) {
 
 
 /**
- * Returns the size of this rectangle.
  * @return {!goog.math.Size} The size of this rectangle.
  */
 goog.math.Rect.prototype.getSize = function() {
   return new goog.math.Size(this.width, this.height);
+};
+
+
+/**
+ * @return {!goog.math.Coordinate} A new coordinate for the top-left corner of
+ *     the rectangle.
+ */
+goog.math.Rect.prototype.getTopLeft = function() {
+  return new goog.math.Coordinate(this.left, this.top);
+};
+
+
+/**
+ * @return {!goog.math.Coordinate} A new coordinate for the center of the
+ *     rectangle.
+ */
+goog.math.Rect.prototype.getCenter = function() {
+  return new goog.math.Coordinate(
+      this.left + this.width / 2, this.top + this.height / 2);
+};
+
+
+/**
+ * @return {!goog.math.Coordinate} A new coordinate for the bottom-right corner
+ *     of the rectangle.
+ */
+goog.math.Rect.prototype.getBottomRight = function() {
+  return new goog.math.Coordinate(
+      this.left + this.width, this.top + this.height);
 };
 
 
