361c423b5e64831dfa2867da85d65c9be57cd564
Nathan Naze <nanaze@gmail.com>
2013-04-30 17:55:08 -0400

    Use DomHelper in goog.ui.CharPicker.
    Organize goog.ui.CharPicker's and its test's goog.requires.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=45456524

--- closure/goog/ui/charpicker.js
+++ closure/goog/ui/charpicker.js
@@ -24,7 +24,6 @@ goog.require('goog.a11y.aria');
 goog.require('goog.a11y.aria.State');
 goog.require('goog.array');
 goog.require('goog.asserts');
-goog.require('goog.dom');
 goog.require('goog.dom.classes');
 goog.require('goog.events');
 goog.require('goog.events.Event');
@@ -327,39 +326,40 @@ goog.ui.CharPicker.prototype.decorateInternal = function(element) {
   var chrs = this.decompressor_.toCharList(':2%C^O80V1H2s2G40Q%s0');
   this.layoutAlteringChars_ = new goog.structs.Set(chrs);
 
-  this.menu_ = new goog.ui.Menu();
+  this.menu_ = new goog.ui.Menu(this.getDomHelper());
 
   var categories = this.data_.categories;
   for (var i = 0; i < this.data_.categories.length; i++) {
     this.menu_.addChild(this.createMenuItem_(i, categories[i]), true);
   }
 
-  this.menubutton_ = new goog.ui.MenuButton('Category Menu', this.menu_);
+  this.menubutton_ = new goog.ui.MenuButton('Category Menu', this.menu_,
+      /* opt_renderer */ undefined, this.getDomHelper());
   this.addChild(this.menubutton_, true);
 
-  this.submenu_ = new goog.ui.Menu();
+  this.submenu_ = new goog.ui.Menu(this.getDomHelper());
 
   this.submenubutton_ = new goog.ui.MenuButton('Subcategory Menu',
-      this.submenu_);
+      this.submenu_, /* opt_renderer */ undefined, this.getDomHelper());
   this.addChild(this.submenubutton_, true);
 
   // The containing component for grid component and the scroller.
-  var gridcontainer = new goog.ui.Component();
+  var gridcontainer = new goog.ui.Component(this.getDomHelper());
   this.addChild(gridcontainer, true);
 
-  var stickwrap = new goog.ui.Component();
+  var stickwrap = new goog.ui.Component(this.getDomHelper());
   gridcontainer.addChild(stickwrap, true);
   this.stickwrap_ = stickwrap.getElement();
 
-  var stick = new goog.ui.Component();
+  var stick = new goog.ui.Component(this.getDomHelper());
   stickwrap.addChild(stick, true);
   this.stick_ = stick.getElement();
 
-  this.grid_ = new goog.ui.Component();
+  this.grid_ = new goog.ui.Component(this.getDomHelper());
   gridcontainer.addChild(this.grid_, true);
 
-  this.notice_ = new goog.ui.Component();
-  this.notice_.setElementInternal(goog.dom.createDom('div'));
+  this.notice_ = new goog.ui.Component(this.getDomHelper());
+  this.notice_.setElementInternal(this.getDomHelper().createDom('div'));
   this.addChild(this.notice_, true);
 
   // The component used for displaying 'Recent Selections' label.
@@ -367,43 +367,47 @@ goog.ui.CharPicker.prototype.decorateInternal = function(element) {
    * @desc The text label above the list of recently selected characters.
    */
   var MSG_CHAR_PICKER_RECENT_SELECTIONS = goog.getMsg('Recent Selections:');
-  var recenttext = new goog.ui.Component();
-  recenttext.setElementInternal(goog.dom.createDom('span', null,
+  var recenttext = new goog.ui.Component(this.getDomHelper());
+  recenttext.setElementInternal(this.getDomHelper().createDom('span', null,
       MSG_CHAR_PICKER_RECENT_SELECTIONS));
   this.addChild(recenttext, true);
 
-  this.recentgrid_ = new goog.ui.Component();
+  this.recentgrid_ = new goog.ui.Component(this.getDomHelper());
   this.addChild(this.recentgrid_, true);
 
   // The component used for displaying 'U+'.
-  var uplus = new goog.ui.Component();
-  uplus.setElementInternal(goog.dom.createDom('span', null, 'U+'));
+  var uplus = new goog.ui.Component(this.getDomHelper());
+  uplus.setElementInternal(this.getDomHelper().createDom('span', null, 'U+'));
   this.addChild(uplus, true);
 
   /**
    * @desc The text inside the input box to specify the hex code of a character.
    */
   var MSG_CHAR_PICKER_HEX_INPUT = goog.getMsg('Hex Input');
-  this.input_ = new goog.ui.LabelInput(MSG_CHAR_PICKER_HEX_INPUT);
+  this.input_ = new goog.ui.LabelInput(
+      MSG_CHAR_PICKER_HEX_INPUT, this.getDomHelper());
   this.addChild(this.input_, true);
 
-  this.okbutton_ = new goog.ui.Button('OK');
+  this.okbutton_ = new goog.ui.Button(
+      'OK', /* opt_renderer */ undefined, this.getDomHelper());
   this.addChild(this.okbutton_, true);
   this.okbutton_.setEnabled(false);
 
-  this.zoomEl_ = goog.dom.createDom('div',
+  this.zoomEl_ = this.getDomHelper().createDom('div',
       {id: 'zoom', className: goog.getCssName('goog-char-picker-char-zoom')});
 
-  this.charNameEl_ = goog.dom.createDom('div',
+  this.charNameEl_ = this.getDomHelper().createDom('div',
       {id: 'charName', className: goog.getCssName('goog-char-picker-name')});
 
-  this.unicodeEl_ = goog.dom.createDom('div',
+  this.unicodeEl_ = this.getDomHelper().createDom('div',
       {id: 'unicode', className: goog.getCssName('goog-char-picker-unicode')});
 
-  var card = goog.dom.createDom('div', {'id': 'preview'}, this.zoomEl_,
-      this.charNameEl_, this.unicodeEl_);
+  var card = this.getDomHelper().createDom('div',
+      {'id': 'preview'},
+      this.zoomEl_, this.charNameEl_, this.unicodeEl_);
   goog.style.setElementShown(card, false);
-  this.hc_ = new goog.ui.HoverCard({'DIV': 'char'});
+  this.hc_ = new goog.ui.HoverCard({'DIV': 'char'},
+      /* opt_checkDescendants */ undefined, this.getDomHelper());
   this.hc_.setElement(card);
   var self = this;
 
@@ -674,8 +678,7 @@ goog.ui.CharPicker.prototype.getChar_ = function(e) {
  * @private
  */
 goog.ui.CharPicker.prototype.createMenuItem_ = function(id, caption) {
-  var item = new goog.ui.MenuItem(caption);
-  item.setValue(id);
+  var item = new goog.ui.MenuItem(caption, /* model */ id, this.getDomHelper());
   item.setVisible(true);
   return item;
 };
@@ -803,7 +806,8 @@ goog.ui.CharPicker.prototype.modifyGridWithItems_ = function(grid, items,
 goog.ui.CharPicker.prototype.populateGridWithButtons_ = function(grid) {
   for (var i = 0; i < grid.buttoncount; i++) {
     var button = new goog.ui.Button(' ',
-                                    goog.ui.FlatButtonRenderer.getInstance());
+                                    goog.ui.FlatButtonRenderer.getInstance(),
+                                    this.getDomHelper());
 
     // Dispatch the focus event so we can update the aria description while
     // the user tabs through the cells.
