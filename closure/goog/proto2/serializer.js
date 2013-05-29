d06b6b06a0260e84dc3c69f453f67526436e7d76
Andy Martone <martone@google.com>
2013-05-29 11:55:22 -0700

    Style tweaks (symbolic enum names deserialization). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=47102042

--- closure/goog/proto2/serializer.js
+++ closure/goog/proto2/serializer.js
@@ -18,7 +18,6 @@
 
 goog.provide('goog.proto2.Serializer');
 
-goog.require('goog.object');
 goog.require('goog.proto2.Descriptor');
 goog.require('goog.proto2.FieldDescriptor');
 goog.require('goog.proto2.Message');
@@ -129,16 +128,16 @@ goog.proto2.Serializer.prototype.getDeserializedValue = function(field, value) {
   // Decode enum values.
   if (field.getFieldType() == goog.proto2.FieldDescriptor.FieldType.ENUM) {
     // If it's a string, get enum value by name.
+    // NB: In order this feature to work, property renaming should be turned off
+    // for the respective enums.
     if (goog.proto2.Serializer.DECODE_SYMBOLIC_ENUMS && goog.isString(value)) {
       // enumType is a regular Javascript enum as defined in field's metadata.
       var enumType = field.getNativeType();
-      if (goog.object.containsKey(enumType, value)) {
+      if (enumType.hasOwnProperty(value)) {
         return enumType[value];
       }
-      // Return unknown values as is for backward compatibility.
-      return value;
     }
-    // Return value as is for backward compatibility.
+    // Return unknown values as is for backward compatibility.
     return value;
   }
 
