1f29532d97068a4df225c8fbe316d1c2c3a8f7e8
Nathan Naze <nnaze@google.com>
2013-07-09 10:40:30 -0400

    Allow stack-free disposable monitoring (but not used by default). The intent is to use this for tests which assert that there are no undiposed objects, without the overhead of tracking where each are created.
    Just the enum change, I'll switch to this in timely afterwards, and contact a
    few other teams using the DisposableMonitoring pattern in their own tests.
    
    Testing:
    Unit test added
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=48886987

--- closure/goog/disposable/disposable.js
+++ closure/goog/disposable/disposable.js
@@ -36,7 +36,9 @@ goog.require('goog.disposable.IDisposable');
  */
 goog.Disposable = function() {
   if (goog.Disposable.MONITORING_MODE != goog.Disposable.MonitoringMode.OFF) {
-    this.creationStack = new Error().stack;
+    if (goog.Disposable.INCLUDE_STACK_ON_CREATION) {
+      this.creationStack = new Error().stack;
+    }
     goog.Disposable.instances_[goog.getUid(this)] = this;
   }
 };
@@ -74,6 +76,13 @@ goog.Disposable.MonitoringMode = {
  *     compiles down to 0 bytes.
  */
 goog.define('goog.Disposable.MONITORING_MODE', 0);
+
+
+/**
+ * @define {boolean} Whether to attach creation stack to each created disposable
+ *     instance; This is only relevant for when MonitoringMode != OFF.
+ */
+goog.define('goog.Disposable.INCLUDE_STACK_ON_CREATION', true);
 
 
 /**
