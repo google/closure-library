2642e3ca116c6c6444aef58ea80649ae0f1d3254
Vipul Bhasin <vbhasin@google.com>
2013-07-31 16:02:01 -0700

    goog.i18n.DateTimeParse does not handle 'L' pattern symbol (Standalone Month) ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=49577506

--- closure/goog/i18n/datetimeparse.js
+++ closure/goog/i18n/datetimeparse.js
@@ -404,7 +404,7 @@ goog.i18n.DateTimeParse.prototype.getNextCharCount_ =
  * All acceptable pattern characters.
  * @private
  */
-goog.i18n.DateTimeParse.PATTERN_CHARS_ = 'GyMdkHmsSEDahKzZvQ';
+goog.i18n.DateTimeParse.PATTERN_CHARS_ = 'GyMdkHmsSEDahKzZvQL';
 
 
 /**
@@ -519,6 +519,7 @@ goog.i18n.DateTimeParse.prototype.subParse_ =
       cal.era = this.matchString_(text, pos, goog.i18n.DateTimeSymbols.ERAS);
       return true;
     case 'M': // MONTH
+    case 'L': // STANDALONEMONTH
       return this.subParseMonth_(text, pos, cal, value);
     case 'E':
       return this.subParseDayOfWeek_(text, pos, cal);
@@ -619,19 +620,21 @@ goog.i18n.DateTimeParse.prototype.subParseYear_ =
  */
 goog.i18n.DateTimeParse.prototype.subParseMonth_ =
     function(text, pos, cal, value) {
-  // when month is symbols, i.e., MMM or MMMM, value will be -1
+  // when month is symbols, i.e., MMM, MMMM, LLL or LLLL, value will be -1
   if (value < 0) {
     // Want to be able to parse both short and long forms.
-    // Try count == 4 first:
-    value = this.matchString_(text, pos, goog.i18n.DateTimeSymbols.MONTHS);
-    if (value < 0) { // count == 4 failed, now try count == 3
-      value = this.matchString_(text, pos,
-                                goog.i18n.DateTimeSymbols.SHORTMONTHS);
-    }
+    // Try count == 4 first
+    var months = goog.i18n.DateTimeSymbols.MONTHS.concat(
+        goog.i18n.DateTimeSymbols.STANDALONEMONTHS).concat(
+        goog.i18n.DateTimeSymbols.SHORTMONTHS).concat(
+        goog.i18n.DateTimeSymbols.STANDALONESHORTMONTHS);
+    value = this.matchString_(text, pos, months);
     if (value < 0) {
       return false;
     }
-    cal.month = value;
+    // The months variable is multiple of 12, so we have to get the actual
+    // month index by modulo 12.
+    cal.month = (value % 12);
     return true;
   } else {
     cal.month = value - 1;
