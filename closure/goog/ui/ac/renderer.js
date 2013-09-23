ee9cbe52dfafd5243653268b5e538922e26e119e
Vipul Bhasin <vbhasin@google.com>
2013-09-23 13:04:22 -0700

    Automated rollback
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=52317490

--- closure/goog/ui/ac/renderer.js
+++ closure/goog/ui/ac/renderer.js
@@ -28,7 +28,7 @@ goog.require('goog.array');
 goog.require('goog.dispose');
 goog.require('goog.dom');
 goog.require('goog.dom.NodeType');
-goog.require('goog.dom.classes');
+goog.require('goog.dom.classlist');
 goog.require('goog.events');
 goog.require('goog.events.EventTarget');
 goog.require('goog.events.EventType');
@@ -139,13 +139,13 @@ goog.ui.ac.Renderer = function(opt_parentNode, opt_customRenderer,
   this.visible_ = false;
 
   /**
-   * Classname for the main element
+   * Classname for the main element.  This must be a single valid class name.
    * @type {string}
    */
   this.className = goog.getCssName('ac-renderer');
 
   /**
-   * Classname for row divs
+   * Classname for row divs.  This must be a single valid class name.
    * @type {string}
    */
   this.rowClassName = goog.getCssName('ac-row');
@@ -164,7 +164,7 @@ goog.ui.ac.Renderer = function(opt_parentNode, opt_customRenderer,
   this.legacyActiveClassName_ = goog.getCssName('active');
 
   /**
-   * Class name for active row div.
+   * Class name for active row div.  This must be a single valid class name.
    * Active row will have rowClassName & activeClassName &
    * legacyActiveClassName.
    * @type {string}
@@ -486,8 +486,8 @@ goog.ui.ac.Renderer.prototype.hiliteRow = function(index) {
     this.hiliteNone();
     this.hilitedRow_ = index;
     if (rowDiv) {
-      goog.dom.classes.add(rowDiv, this.activeClassName,
-          this.legacyActiveClassName_);
+      goog.dom.classlist.addAll(rowDiv, [this.activeClassName,
+          this.legacyActiveClassName_]);
       if (this.target_) {
         goog.a11y.aria.setActiveDescendant(this.target_, rowDiv);
       }
@@ -502,8 +502,8 @@ goog.ui.ac.Renderer.prototype.hiliteRow = function(index) {
  */
 goog.ui.ac.Renderer.prototype.hiliteNone = function() {
   if (this.hilitedRow_ >= 0) {
-    goog.dom.classes.remove(this.rowDivs_[this.hilitedRow_],
-                            this.activeClassName, this.legacyActiveClassName_);
+    goog.dom.classlist.removeAll(this.rowDivs_[this.hilitedRow_],
+        [this.activeClassName, this.legacyActiveClassName_]);
   }
 };
 
@@ -534,7 +534,8 @@ goog.ui.ac.Renderer.prototype.hiliteId = function(id) {
  * @private
  */
 goog.ui.ac.Renderer.prototype.setMenuClasses_ = function(elt) {
-  goog.dom.classes.add(elt, this.className);
+  // Legacy clients may set the renderer's className to a space-separated list.
+  goog.dom.classlist.addAll(elt, this.className.split(' '));
 };
 
 
@@ -899,25 +900,25 @@ goog.ui.ac.Renderer.prototype.getTokenRegExp_ = function(tokenOrArray) {
  * @return {Element} An element with the rendered HTML.
  */
 goog.ui.ac.Renderer.prototype.renderRowHtml = function(row, token) {
-  // Create and return the node
-  var node = this.dom_.createDom('div', {
+  // Create and return the element.
+  var elem = this.dom_.createDom('div', {
     className: this.rowClassName,
     id: goog.ui.IdGenerator.getInstance().getNextUniqueId()
   });
-  goog.a11y.aria.setRole(node, goog.a11y.aria.Role.OPTION);
+  goog.a11y.aria.setRole(elem, goog.a11y.aria.Role.OPTION);
   if (this.customRenderer_ && this.customRenderer_.renderRow) {
-    this.customRenderer_.renderRow(row, token, node);
+    this.customRenderer_.renderRow(row, token, elem);
   } else {
-    this.renderRowContents_(row, token, node);
+    this.renderRowContents_(row, token, elem);
   }
 
   if (token && this.useStandardHighlighting_) {
-    this.hiliteMatchingText_(node, token);
+    this.hiliteMatchingText_(elem, token);
   }
 
-  goog.dom.classes.add(node, this.rowClassName);
-  this.rowDivs_.push(node);
-  return node;
+  goog.dom.classlist.add(elem, this.rowClassName);
+  this.rowDivs_.push(elem);
+  return elem;
 };
 
 
@@ -931,7 +932,7 @@ goog.ui.ac.Renderer.prototype.renderRowHtml = function(row, token) {
  */
 goog.ui.ac.Renderer.prototype.getRowFromEventTarget_ = function(et) {
   while (et && et != this.element_ &&
-      !goog.dom.classes.has(et, this.rowClassName)) {
+      !goog.dom.classlist.contains(et, this.rowClassName)) {
     et = /** @type {Element} */ (et.parentNode);
   }
   return et ? goog.array.indexOf(this.rowDivs_, et) : -1;
