1f44210f161e302dfed9dea9896e9cfe133b6bc9
Nathan Naze <nnaze@google.com>
2013-10-09 17:17:44 -0400

    Make get/set equation public, not protected.
    Presently, they're accesssed in TexEditor and are breaking the build with the new js compiler release.
    
    javascript/closure/ui/equation/texeditor.js:92: ERROR - Access to protected property getEquation of goog.ui.equation.TexPane not allowed here.
    javascript/closure/ui/equation/texeditor.js:102: ERROR - Access to protected property setEquation of goog.ui.equation.TexPane not allowed here.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=53932594

--- closure/goog/ui/equation/editorpane.js
+++ closure/goog/ui/equation/editorpane.js
@@ -54,14 +54,12 @@ goog.ui.equation.EditorPane.prototype.setVisible =
 /**
  * Sets the equation to show in this tab pane.
  * @param {string} equation The equation.
- * @protected
  */
 goog.ui.equation.EditorPane.prototype.setEquation = goog.abstractMethod;
 
 
 /**
  * @return {string} The equation shown in this tab pane.
- * @protected
  */
 goog.ui.equation.EditorPane.prototype.getEquation = goog.abstractMethod;
 
