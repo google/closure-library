3242bd25bc671dc8171b4de915524b97f9dd5ab0
Nathan Naze <nnaze@google.com>
2013-10-09 17:05:23 -0400

    Automated g4 rollback of changelist 53574027.
    *** Reason for rollback ***
    
    Maybe causing timeout in  https://test.corp.google.com/ui#t=//javatests/com/google/cultural/frontend/admin/javascript:rollup_test_chrome-linux&cl=53585117&p=cultural_center&flags=CAIQAQ==
    
    *** Original change description ***
    
    Replace XhrIo.js w/ goog.labs.net.xhr.  Saves ~ 1kb non-gzipped.
    
    Removed goog.structs.map as acceptable argument type for extra logging leaders.  I couldn't find any instances of someone passing goog.structs.map so this should be innocuous.
    
    Fixed tests in ads/circulars/javascript/... that were relying on calls to xhrio.
    
    ***
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=53837773

--- closure/goog/debug/errorreporter.js
+++ closure/goog/debug/errorreporter.js
@@ -28,8 +28,8 @@ goog.require('goog.debug.entryPointRegistry');
 goog.require('goog.events');
 goog.require('goog.events.Event');
 goog.require('goog.events.EventTarget');
-goog.require('goog.labs.net.xhr');
 goog.require('goog.log');
+goog.require('goog.net.XhrIo');
 goog.require('goog.object');
 goog.require('goog.string');
 goog.require('goog.uri.utils');
@@ -91,7 +91,7 @@ goog.debug.ErrorReporter = function(
 
   /**
    * XHR sender.
-   * @type {function(string, string, string, Object=)}
+   * @type {function(string, string, string, (Object|goog.structs.Map)=)}
    * @private
    */
   this.xhrSender_ = goog.debug.ErrorReporter.defaultXhrSender;
@@ -172,7 +172,7 @@ goog.debug.ErrorReporter.ExceptionEvent.TYPE =
 
 /**
  * Extra headers for the error-reporting XHR.
- * @type {Object|undefined}
+ * @type {Object|goog.structs.Map|undefined}
  * @private
  */
 goog.debug.ErrorReporter.prototype.extraHeaders_;
@@ -215,14 +215,14 @@ goog.debug.ErrorReporter.install = function(
  * Default implementation of XHR sender interface.
  *
  * @param {string} uri URI to make request to.
- * @param {string} method HTTP request method.
+ * @param {string} method Send method.
  * @param {string} content Post data.
- * @param {Object=} opt_headers Map of headers to add to the
+ * @param {Object|goog.structs.Map=} opt_headers Map of headers to add to the
  *     request.
  */
 goog.debug.ErrorReporter.defaultXhrSender = function(uri, method, content,
     opt_headers) {
-  goog.labs.net.xhr.send(method, uri, content, {headers: opt_headers});
+  goog.net.XhrIo.send(uri, null, method, content, opt_headers);
 };
 
 
@@ -281,7 +281,8 @@ if (goog.debug.ErrorReporter.ALLOW_AUTO_PROTECT) {
 
 /**
  * Add headers to the logging url.
- * @param {Object} loggingHeaders Extra headers to send to the logging URL.
+ * @param {Object|goog.structs.Map} loggingHeaders Extra headers to send
+ *     to the logging URL.
  */
 goog.debug.ErrorReporter.prototype.setLoggingHeaders =
     function(loggingHeaders) {
@@ -291,10 +292,11 @@ goog.debug.ErrorReporter.prototype.setLoggingHeaders =
 
 /**
  * Set the function used to send error reports to the server.
- * @param {function(string, string, string, Object=)} xhrSender If provided,
- *     this will be used to send a report to the server instead of the default
- *     method. The function will be given the URI, HTTP method request content,
- *     and (optionally) request headers to be added.
+ * @param {function(string, string, string, (Object|goog.structs.Map)=)}
+ *     xhrSender If provided, this will be used to send a report to the
+ *     server instead of the default method. The function will be given the URI,
+ *     HTTP method request content, and (optionally) request headers to be
+ *     added.
  */
 goog.debug.ErrorReporter.prototype.setXhrSender = function(xhrSender) {
   this.xhrSender_ = xhrSender;
