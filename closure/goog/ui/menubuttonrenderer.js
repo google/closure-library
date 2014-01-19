e7ad9235206a94e39c6ead0ec18c0cd2fab197d4
Nathan Naze <nnaze@google.com>
2014-01-18 23:23:09 -0500

    Removing a legacy fix for Firefox, which is causing differences in rendering between FF and Chrome. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=58279238

--- closure/goog/ui/menubuttonrenderer.js
+++ closure/goog/ui/menubuttonrenderer.js
@@ -31,7 +31,6 @@ goog.require('goog.ui.CustomButtonRenderer');
 goog.require('goog.ui.INLINE_BLOCK_CLASSNAME');
 goog.require('goog.ui.Menu');
 goog.require('goog.ui.MenuRenderer');
-goog.require('goog.userAgent');
 
 
 
@@ -58,40 +57,6 @@ goog.ui.MenuButtonRenderer.CSS_CLASS = goog.getCssName('goog-menu-button');
 
 
 /**
- * A property to denote content elements that have been wrapped in an extra
- * div to work around FF2/RTL bugs.
- * @type {string}
- * @private
- */
-goog.ui.MenuButtonRenderer.WRAPPER_PROP_ = '__goog_wrapper_div';
-
-
-if (goog.userAgent.GECKO) {
-  /**
-   * Takes the menubutton's root element, and sets its content to the given
-   * text caption or DOM structure. Because the DOM structure of this button is
-   * conditional based on whether we need to work around FF2/RTL bugs, we
-   * override the default implementation to take this into account.
-   * @param {Element} element The control's root element.
-   * @param {goog.ui.ControlContent} content Text caption or DOM
-   *     structure to be set as the control's content.
-   * @override
-   */
-  goog.ui.MenuButtonRenderer.prototype.setContent = function(element,
-      content) {
-    var caption =
-        goog.ui.MenuButtonRenderer.superClass_.getContentElement.call(this,
-            /** @type {Element} */ (element && element.firstChild));
-    if (caption) {
-      goog.dom.replaceNode(
-          this.createCaption(content, goog.dom.getDomHelper(element)),
-          caption);
-    }
-  };
-} // end goog.userAgent.GECKO
-
-
-/**
  * Takes the button's root element and returns the parent element of the
  * button's contents.  Overrides the superclass implementation by taking
  * the nested DIV structure of menu buttons into account.
@@ -101,14 +66,8 @@ if (goog.userAgent.GECKO) {
  * @override
  */
 goog.ui.MenuButtonRenderer.prototype.getContentElement = function(element) {
-  var content =
-      goog.ui.MenuButtonRenderer.superClass_.getContentElement.call(this,
-          /** @type {Element} */ (element && element.firstChild));
-  if (goog.userAgent.GECKO && content &&
-      content[goog.ui.MenuButtonRenderer.WRAPPER_PROP_]) {
-    content = /** @type {Element} */ (content.firstChild);
-  }
-  return content;
+  return goog.ui.MenuButtonRenderer.superClass_.getContentElement.call(this,
+      /** @type {Element} */ (element && element.firstChild));
 };
 
 
