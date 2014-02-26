ffc3d72ae3ec6d1bee41a2b2fd724888a4585420
Andy Martone <martone@google.com>
2014-02-26 15:09:04 -0800

    test.html -> test.js (partial) conversion for goog.testing. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=62150557

--- closure/goog/net/xhrio_test.js
+++ closure/goog/net/xhrio_test.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-goog.provide('goog.testing.net.XhrIoTest');
-goog.setTestOnly('goog.testing.net.XhrIoTest');
+goog.provide('goog.net.XhrIoTest');
+goog.setTestOnly('goog.net.XhrIoTest');
 
 goog.require('goog.Uri');
 goog.require('goog.debug.EntryPointMonitor');
