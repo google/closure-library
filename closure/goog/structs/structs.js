621ffef6d6bdf3b9abda5f0809142183376a0768
Andy Martone <martone@google.com>
2014-03-10 11:11:22 -0700

    Undeprecate goog.structs.forEach until uses in Closure are eliminated. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=62353296

--- closure/goog/structs/structs.js
+++ closure/goog/structs/structs.js
@@ -161,6 +161,9 @@ goog.structs.clear = function(col) {
  * Calls a function for each value in a collection. The function takes
  * three arguments; the value, the key and the collection.
  *
+ * NOTE: This will be deprecated soon! Please use a more specific method if
+ * possible, e.g. goog.array.forEach, goog.object.forEach, etc.
+ *
  * @param {S} col The collection-like object.
  * @param {function(this:T,?,?,S):?} f The function to call for every value.
  *     This function takes
@@ -169,8 +172,6 @@ goog.structs.clear = function(col) {
  * @param {T=} opt_obj The object to be used as the value of 'this'
  *     within {@code f}.
  * @template T,S
- * @deprecated Use a more specific method (e.g. goog.array.forEach,
- *     goog.object.forEach, etc.)
  */
 goog.structs.forEach = function(col, f, opt_obj) {
   if (typeof col.forEach == 'function') {
