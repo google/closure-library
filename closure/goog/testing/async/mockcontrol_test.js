739713e30dec3d5ef27eefd0773378836c3a5b9f
Andy Martone <martone@google.com>
2014-03-10 10:41:01 -0700

    test.html -> test.js conversion for goog.testing. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=62235339

--- closure/goog/testing/async/mockcontrol_test.js
+++ closure/goog/testing/async/mockcontrol_test.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-goog.provide('goog.testing.MockControlTest');
-goog.setTestOnly('goog.testing.MockControlTest');
+goog.provide('goog.testing.async.MockControlTest');
+goog.setTestOnly('goog.testing.async.MockControlTest');
 
 goog.require('goog.async.Deferred');
 goog.require('goog.testing.MockControl');
