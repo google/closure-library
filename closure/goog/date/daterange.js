2e1d66ccbc534391fcb07c86e2568f52288c9bbc
teppeis <teppeis@gmail.com>
2014-04-27 16:35:04 +0900

    Fix JSDoc of goog/date/daterange.js

--- closure/goog/date/daterange.js
+++ closure/goog/date/daterange.js
@@ -96,7 +96,7 @@ goog.date.DateRange.prototype.contains = function(date) {
 
 
 /**
- * @return {!goog.iter.Iterator} An iterator over the date range.
+ * @return {!goog.date.DateRange.Iterator} An iterator over the date range.
  */
 goog.date.DateRange.prototype.iterator = function() {
   return new goog.date.DateRange.Iterator(this);
@@ -393,7 +393,7 @@ goog.date.DateRange.cloneOrCreate_ = function(opt_today) {
 /**
  * Creates an iterator over the dates in a {@link goog.date.DateRange}.
  * @constructor
- * @extends {goog.iter.Iterator}
+ * @extends {goog.iter.Iterator<goog.date.Date>}
  * @param {goog.date.DateRange} dateRange The date range to iterate.
  * @final
  */
