fe293532fcf92361212bba2a9d80e00ba4c1a15a
Nathan Naze <nanaze@gmail.com>
2014-05-10 15:08:28 -0400

    Reapply nicks change
    https://github.com/google/closure-library/commit/6c9950247f4c2e6bc38a39ec07e827e37c59aeae
    
    This was incorrectly overwritten by MOE.

--- closure/goog/dom/textrange.js
+++ closure/goog/dom/textrange.js
@@ -304,9 +304,7 @@ goog.dom.TextRange.prototype.getStartOffset = function() {
 
 /** @override */
 goog.dom.TextRange.prototype.getStartPosition = function() {
-  return this.isReversed() ?
-      this.getBrowserRangeWrapper_().getEndPosition() :
-      this.getBrowserRangeWrapper_().getStartPosition();
+  return this.getBrowserRangeWrapper_().getStartPosition();
 };
 
 
@@ -326,9 +324,7 @@ goog.dom.TextRange.prototype.getEndOffset = function() {
 
 /** @override */
 goog.dom.TextRange.prototype.getEndPosition = function() {
-  return this.isReversed() ?
-      this.getBrowserRangeWrapper_().getStartPosition() :
-      this.getBrowserRangeWrapper_().getEndPosition();
+  return this.getBrowserRangeWrapper_().getEndPosition();
 };
 
 
