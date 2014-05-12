27f25782e6c399abac041561af844fd4fe53cb78
Nick Santos <nicholas.j.santos@gmail.com>
2014-05-11 23:29:20 -0400

    fixes range's getStartPosition/getEndPosition, try #2

--- closure/goog/dom/textrange.js
+++ closure/goog/dom/textrange.js
@@ -285,9 +285,7 @@ goog.dom.TextRange.prototype.getStartOffset = function() {
 
 /** @override */
 goog.dom.TextRange.prototype.getStartPosition = function() {
-  return this.isReversed() ?
-      this.getBrowserRangeWrapper_().getEndPosition() :
-      this.getBrowserRangeWrapper_().getStartPosition();
+  return this.getBrowserRangeWrapper_().getStartPosition();
 };
 
 
@@ -307,9 +305,7 @@ goog.dom.TextRange.prototype.getEndOffset = function() {
 
 /** @override */
 goog.dom.TextRange.prototype.getEndPosition = function() {
-  return this.isReversed() ?
-      this.getBrowserRangeWrapper_().getStartPosition() :
-      this.getBrowserRangeWrapper_().getEndPosition();
+  return this.getBrowserRangeWrapper_().getEndPosition();
 };
 
 
