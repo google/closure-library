4c50f43e9582359ecb286cb72ccf60c48aab8e37
Nathan Naze <nnaze@google.com>
2014-05-24 09:36:17 -0400

    Add initial docs.

---
+++ api/static/js/externs.js
@@ -0,0 +1,16 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+
+/**
+ * @fileoverview Externs for grokdoc JS.
+ *
+ * @author nicksantos@google.com (Nick Santos)
+ */
+
+/** @type {string} */
+var _staticFilePath;
+
+// for prettify.js
+function prettyPrint(opt_whenDone) {}
+
+var console;
+var PR;
