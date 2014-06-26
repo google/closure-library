7e590b3cd4f3db26d48f6081aae62ad5061129f2
Nathan Naze <nnaze@google.com>
2014-06-26 13:18:16 -0400

    Fix CSSMatrix constructor so it works in IE. No-op for IE9 translation test as it does not support CSSMatrix like Gecko.
    Part of effort to fix Closure tests on IE. See https://docs.google.com/a/google.com/spreadsheets/d/1koLr1rFjV1kKcsiSui7pqTexanpAR9q-P3-QwhFYnvw/edit#gid=0
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=69297622

--- closure/goog/style/transform.js
+++ closure/goog/style/transform.js
@@ -84,7 +84,9 @@ goog.style.transform.setTranslation = function(element, x, y) {
   var translation = goog.style.transform.is3dSupported() ?
       'translate3d(' + x + 'px,' + y + 'px,' + '0px)' :
       'translate(' + x + 'px,' + y + 'px)';
-  goog.style.setStyle(element, 'transform', translation);
+  var property = goog.userAgent.IE && goog.userAgent.DOCUMENT_MODE == 9 ?
+      '-ms-transform' : 'transform';
+  goog.style.setStyle(element, property, translation);
   return true;
 };
 
@@ -97,17 +99,14 @@ goog.style.transform.setTranslation = function(element, x, y) {
  */
 goog.style.transform.matrixConstructor_ =
     goog.functions.cacheReturnValue(function() {
-  // TODO(user): Unless these are accessed from the default namespace the
-  // compiler will rename them. A better way is to use goog.global and prevent
-  // renaming.
-  if (typeof WebKitCSSMatrix !== undefined) {
-    return WebKitCSSMatrix;
+  if ('WebKitCSSMatrix' in goog.global) {
+    return goog.global['WebKitCSSMatrix'];
   }
-  if (typeof MSCSSMatrix !== undefined) {
-    return MSCSSMatrix;
+  if ('MSCSSMatrix' in goog.global) {
+    return goog.global['MSCSSMatrix'];
   }
-  if (typeof CSSMatrix !== undefined) {
-    return CSSMatrix;
+  if ('CSSMatrix' in goog.global) {
+    return goog.global['CSSMatrix'];
   }
   return null;
 });
