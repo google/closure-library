fadd3dde4deb16fbb806b9e1a3cff17d7e603ad2
Nathan Naze <nnaze@google.com>
2014-06-26 12:51:20 -0400

    Fix bug where result is truncated when rounding by significant digits would produce a more precise result than by the maximum fraction digits.
    Before:
    0.1284  // Number
    0.128   // rounded to three significant digits
    0.12    // truncated to two fractional digits
    
    After:
    0.1284  // Number
    0.128   // rounded to three significant digits
    0.13    // _rounded_ to two fractional digits
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=69172977

--- closure/goog/i18n/numberformat_test.js
+++ closure/goog/i18n/numberformat_test.js
@@ -761,6 +761,7 @@ function testSignificantDigitsEqualToMax() {
   assertEquals('12', fmt.format(12.34));
   assertEquals('1.2', fmt.format(1.234));
   assertEquals('0.12', fmt.format(0.1234));
+  assertEquals('0.13', fmt.format(0.1284));
 }
 
 function testSignificantDigitsLessThanMax() {
@@ -773,6 +774,7 @@ function testSignificantDigitsLessThanMax() {
   assertEquals('12', fmt.format(12.34));
   assertEquals('1', fmt.format(1.234));
   assertEquals('0.1', fmt.format(0.1234));
+  assertEquals('0.2', fmt.format(0.1834));
 }
 
 function testSignificantDigitsMoreThanMax() {
@@ -786,6 +788,7 @@ function testSignificantDigitsMoreThanMax() {
   assertEquals('12.3', fmt.format(12.34));
   assertEquals('1.23', fmt.format(1.234));
   assertEquals('0.12', fmt.format(0.1234));
+  assertEquals('0.13', fmt.format(0.1284));
 }
 
 function testSimpleCompactFrench() {
