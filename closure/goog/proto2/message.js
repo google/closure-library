02938b075ba4d9779a1d71f8dc332f1f6de58ac1
Nathan Naze <nnaze@google.com>
2014-08-26 21:51:46 -0400

    Add more formatting to 2 JS proto2 error strings. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=74060024

--- closure/goog/proto2/message.js
+++ closure/goog/proto2/message.js
@@ -568,8 +568,10 @@ goog.proto2.Message.prototype.get$Value = function(tag, opt_index) {
 
   if (this.fields_[tag].isRepeated()) {
     var index = opt_index || 0;
-    goog.asserts.assert(index >= 0 && index < value.length,
-        'Given index is out of bounds');
+    goog.asserts.assert(
+        index >= 0 && index < value.length,
+        'Given index %s is out of bounds.  Repeated field length: %s',
+        index, value.length);
     return value[index];
   }
 
