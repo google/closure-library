8d587d4e55784b56d7b730a210278f934ea2f6e7
nnaze <nnaze@google.com>
2014-12-05 19:00:42 -0800

    Introduce a shared definition for the varaibles used across files in the mechanism unit tests.
    When the test files are shifted around (when new test files
    are introduced), this is causing compilation errors.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=81471360

--- closure/goog/storage/mechanism/html5sessionstorage_test.js
+++ closure/goog/storage/mechanism/html5sessionstorage_test.js
@@ -20,6 +20,8 @@ goog.require('goog.storage.mechanism.HTML5SessionStorage');
 goog.require('goog.storage.mechanism.mechanismSeparationTester');
 /** @suppress {extraRequire} */
 goog.require('goog.storage.mechanism.mechanismSharingTester');
+/** @suppress {extraRequire} */
+goog.require('goog.storage.mechanism.mechanismTestDefinition');
 goog.require('goog.testing.jsunit');
 goog.require('goog.userAgent');
 
