2a79f2bc6b43115631fc6a1000e027c9d863a24d
Brendan Linn <brndn@google.com>
2015-01-20 10:58:53 -0800

    Revert e9dcd0ba641a8a3817e49b295a347954e47b3379.

--- closure/goog/crypt/sha1.js
+++ closure/goog/crypt/sha1.js
@@ -26,8 +26,6 @@
  *   Chrome 23:   ~400 Mbit/s
  *   Firefox 16:  ~250 Mbit/s
  *
- * Note: The idiom expr|0 is used to provide a type-hint to the VM, in order
- * to avoid unnecessary uint32-double-uint32 roundtripping.
  */
 
 goog.provide('goog.crypt.Sha1');
@@ -125,11 +123,10 @@ goog.crypt.Sha1.prototype.compress_ = function(buf, opt_offset) {
   }
 
   var W = this.W_;
-  var i;
 
   // get 16 big endian words
   if (goog.isString(buf)) {
-    for (i = 0; i < 16; i++) {
+    for (var i = 0; i < 16; i++) {
       // TODO(user): [bug 8140122] Recent versions of Safari for Mac OS and iOS
       // have a bug that turns the post-increment ++ operator into pre-increment
       // during JIT compilation.  We have code that depends heavily on SHA-1 for
@@ -138,20 +135,26 @@ goog.crypt.Sha1.prototype.compress_ = function(buf, opt_offset) {
       // this change once the Safari bug
       // (https://bugs.webkit.org/show_bug.cgi?id=109036) has been fixed and
       // most clients have been updated.
-      W[i] = (((buf.charCodeAt(opt_offset) << 24) |
-               (buf.charCodeAt(opt_offset + 1) << 16) |
-               (buf.charCodeAt(opt_offset + 2) << 8) |
-               (buf.charCodeAt(opt_offset + 3))) & 0xffffffff) | 0;
+      W[i] = (buf.charCodeAt(opt_offset) << 24) |
+             (buf.charCodeAt(opt_offset + 1) << 16) |
+             (buf.charCodeAt(opt_offset + 2) << 8) |
+             (buf.charCodeAt(opt_offset + 3));
       opt_offset += 4;
     }
   } else {
-    for (i = 0; i < 16; i++) {
-      W[i] = (((buf[opt_offset] << 24) |
-               (buf[opt_offset + 1] << 16) |
-               (buf[opt_offset + 2] << 8) |
-               (buf[opt_offset + 3])) & 0xffffffff) | 0;
+    for (var i = 0; i < 16; i++) {
+      W[i] = (buf[opt_offset] << 24) |
+             (buf[opt_offset + 1] << 16) |
+             (buf[opt_offset + 2] << 8) |
+             (buf[opt_offset + 3]);
       opt_offset += 4;
     }
+  }
+
+  // expand to 80 words
+  for (var i = 16; i < 80; i++) {
+    var t = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16];
+    W[i] = ((t << 1) | (t >>> 31)) & 0xffffffff;
   }
 
   var a = this.chain_[0];
@@ -159,27 +162,10 @@ goog.crypt.Sha1.prototype.compress_ = function(buf, opt_offset) {
   var c = this.chain_[2];
   var d = this.chain_[3];
   var e = this.chain_[4];
-  var f, k, t;
+  var f, k;
 
-  // Steps 0-16.
-  for (i = 0; i < 16; i++) {
-    f = d ^ (b & (c ^ d));
-    k = 0x5a827999;
-
-    t = ((((a << 5) | (a >>> 27)) + f + e + k + W[i]) & 0xffffffff) | 0;
-    e = d;
-    d = c;
-    c = (((b << 30) | (b >>> 2)) & 0xffffffff) | 0;
-    b = a;
-    a = t;
-  }
-  // Steps 16-80. W is formally described as an 80-word array, and usually
-  // computed that way. However, only 16 elements are needed for any iteration
-  // so we compute W on the fly and keep only the last 16 values. This improves
-  // performance by about 10% on Chrome 35.
-  for (i = 16; i < 80; i++) {
-    t = W[(i - 3) & 15] ^ W[(i - 8) & 15] ^ W[(i - 14) & 15] ^ W[i & 15];
-    W[i & 15] = (((t << 1) | (t >>> 31)) & 0xfffffff) | 0;
+  // TODO(user): Try to unroll this loop to speed up the computation.
+  for (var i = 0; i < 80; i++) {
     if (i < 40) {
       if (i < 20) {
         f = d ^ (b & (c ^ d));
@@ -198,33 +184,32 @@ goog.crypt.Sha1.prototype.compress_ = function(buf, opt_offset) {
       }
     }
 
-    t = ((((a << 5) | (a >>> 27)) + f + e + k + W[i & 15]) & 0xffffffff) | 0;
+    var t = (((a << 5) | (a >>> 27)) + f + e + k + W[i]) & 0xffffffff;
     e = d;
     d = c;
-    c = (((b << 30) | (b >>> 2)) & 0xffffffff) | 0;
+    c = ((b << 30) | (b >>> 2)) & 0xffffffff;
     b = a;
     a = t;
   }
 
-  this.chain_[0] = ((this.chain_[0] + a) & 0xfffffff) | 0;
-  this.chain_[1] = ((this.chain_[1] + b) & 0xfffffff) | 0;
-  this.chain_[2] = ((this.chain_[2] + c) & 0xfffffff) | 0;
-  this.chain_[3] = ((this.chain_[3] + d) & 0xfffffff) | 0;
-  this.chain_[4] = ((this.chain_[4] + e) & 0xfffffff) | 0;
+  this.chain_[0] = (this.chain_[0] + a) & 0xffffffff;
+  this.chain_[1] = (this.chain_[1] + b) & 0xffffffff;
+  this.chain_[2] = (this.chain_[2] + c) & 0xffffffff;
+  this.chain_[3] = (this.chain_[3] + d) & 0xffffffff;
+  this.chain_[4] = (this.chain_[4] + e) & 0xffffffff;
 };
 
 
 /** @override */
 goog.crypt.Sha1.prototype.update = function(bytes, opt_length) {
   // TODO(johnlenz): tighten the function signature and remove this check
-  if (bytes === null) {
+  if (bytes == null) {
     return;
   }
 
   if (!goog.isDef(opt_length)) {
     opt_length = bytes.length;
   }
-  opt_length = (bytes.length < opt_length) ? bytes.length : opt_length;
 
   var lengthMinusBlock = opt_length - this.blockSize;
   var n = 0;
@@ -238,7 +223,7 @@ goog.crypt.Sha1.prototype.update = function(bytes, opt_length) {
     // input buffer (assuming it contains sufficient data). This gives ~25%
     // speedup on Chrome 23 and ~15% speedup on Firefox 16, but requires that
     // the data is provided in large chunks (or in multiples of 64 bytes).
-    if (inbuf === 0) {
+    if (inbuf == 0) {
       while (n <= lengthMinusBlock) {
         this.compress_(bytes, n);
         n += this.blockSize;
@@ -282,8 +267,6 @@ goog.crypt.Sha1.prototype.digest = function() {
   var digest = [];
   var totalBits = this.total_ * 8;
 
-  var i;
-
   // Add pad 0x80 0x00*.
   if (this.inbuf_ < 56) {
     this.update(this.pad_, 56 - this.inbuf_);
@@ -292,7 +275,7 @@ goog.crypt.Sha1.prototype.digest = function() {
   }
 
   // Add # bits.
-  for (i = this.blockSize - 1; i >= 56; i--) {
+  for (var i = this.blockSize - 1; i >= 56; i--) {
     this.buf_[i] = totalBits & 255;
     totalBits /= 256; // Don't use bit-shifting here!
   }
@@ -300,7 +283,7 @@ goog.crypt.Sha1.prototype.digest = function() {
   this.compress_(this.buf_);
 
   var n = 0;
-  for (i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {
     for (var j = 24; j >= 0; j -= 8) {
       digest[n] = (this.chain_[i] >> j) & 255;
       ++n;
--- closure/goog/crypt/sha1_test.js
+++ closure/goog/crypt/sha1_test.js
@@ -72,47 +72,4 @@ function testHashing() {
   sha1.update('The quick brown fox jumps over the lazy dog');
   assertEquals('2fd4e1c67a2d28fced849ee1bb76e7391b93eb12',
                goog.crypt.byteArrayToHex(sha1.digest()));
-
-  sha1.reset();
-  sha1.update(goog.string.repeat('a', 1024));
-  assertEquals('8eca554631df9ead14510e1a70ae48c70f9b9384',
-               goog.crypt.byteArrayToHex(sha1.digest()));
-
-}
-
-function testLength() {
-  var sha = new goog.crypt.Sha1();
-
-  // Test that truncating a message works.
-  sha.reset();
-  sha.update('abc');
-  assertEquals('a9993e364706816aba3e25717850c26c9cd0d89d',
-               goog.crypt.byteArrayToHex(sha.digest()));
-  sha.reset();
-  sha.update('abcde', 3);
-  assertEquals('a9993e364706816aba3e25717850c26c9cd0d89d',
-               goog.crypt.byteArrayToHex(sha.digest()));
-
-  // Test that lengths work correctly.
-  var message = goog.crypt.hexToByteArray(
-      'd9b28b643d16efc8a17a532c05deb79069421bf4cda67f58310ae3bc956e4720' +
-      'f9d2ab845d360fe8c19a734c25fed7b089623b14edc69f78512a03dcb58e6740');
-
-  // Lengths from 0 to 64.
-  sha.reset();
-  for (var i = 0; i < 64; i++) {
-    sha.update(message, i);
-  }
-  assertElementsEquals(goog.crypt.hexToByteArray(
-      '04df2fcf7b12b7735e0a3d05d9723702aa70de30'),
-      sha.digest());
-
-  // Lengths from 0 to 71 to include message overrun cases.
-  sha.reset();
-  for (var i = 0; i < 71; i++) {
-    sha.update(message, i);
-  }
-  assertElementsEquals(goog.crypt.hexToByteArray(
-      'f62df7546b7f70351a5c25bfd9e77ba90ca697f4'),
-      sha.digest());
 }
