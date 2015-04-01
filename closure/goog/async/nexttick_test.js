9579dd36a433a4b416d2c74109209d812d45d6ce
brenneman <brenneman@google.com>
2015-04-01 11:32:52 -0700

    Convert closure/async/nexttick_test from AsyncTestCase to Promises. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=90083358

--- closure/goog/async/nexttick_test.js
+++ closure/goog/async/nexttick_test.js
@@ -14,19 +14,17 @@
 goog.provide('goog.async.nextTickTest');
 goog.setTestOnly('goog.async.nextTickTest');
 
+goog.require('goog.Promise');
+goog.require('goog.Timer');
 goog.require('goog.async.nextTick');
 goog.require('goog.debug.ErrorHandler');
 goog.require('goog.debug.entryPointRegistry');
 goog.require('goog.dom');
 goog.require('goog.dom.TagName');
 goog.require('goog.labs.userAgent.browser');
-goog.require('goog.testing.AsyncTestCase');
 goog.require('goog.testing.MockClock');
 goog.require('goog.testing.PropertyReplacer');
 goog.require('goog.testing.jsunit');
-
-var asyncTestCase = goog.testing.AsyncTestCase.createAndInstall(
-    'asyncNextTickTest');
 
 var clock;
 var propertyReplacer = new goog.testing.PropertyReplacer();
@@ -39,70 +37,74 @@ function tearDown() {
   if (clock) {
     clock.uninstall();
   }
+  // Unset the cached setImmediate_ behavior so it's re-evaluated for each test.
   goog.async.nextTick.setImmediate_ = undefined;
   propertyReplacer.reset();
 }
 
 
 function testNextTick() {
-  var c = 0;
-  var max = 100;
-  var async = true;
-  var counterStep = function(i) {
-    async = false;
-    assertEquals('Order correct', i, c);
-    c++;
-    if (c === max) {
-      asyncTestCase.continueTesting();
+  return new goog.Promise(function(resolve, reject) {
+    var c = 0;
+    var max = 100;
+    var async = true;
+    var counterStep = function(i) {
+      async = false;
+      assertEquals('Order correct', i, c);
+      c++;
+      if (c === max) {
+        resolve();
+      }
+    };
+    for (var i = 0; i < max; i++) {
+      goog.async.nextTick(goog.partial(counterStep, i));
     }
-  };
-  for (var i = 0; i < max; i++) {
-    goog.async.nextTick(goog.partial(counterStep, i));
-  }
-  assertTrue(async);
-  asyncTestCase.waitForAsync('Wait for callback');
+    assertTrue(async);
+  });
 }
 
 
 function testNextTickSetImmediate() {
-  var c = 0;
-  var max = 100;
-  var async = true;
-  var counterStep = function(i) {
-    async = false;
-    assertEquals('Order correct', i, c);
-    c++;
-    if (c === max) {
-      asyncTestCase.continueTesting();
+  return new goog.Promise(function(resolve, reject) {
+    var c = 0;
+    var max = 100;
+    var async = true;
+    var counterStep = function(i) {
+      async = false;
+      assertEquals('Order correct', i, c);
+      c++;
+      if (c === max) {
+        resolve();
+      }
+    };
+    for (var i = 0; i < max; i++) {
+      goog.async.nextTick(goog.partial(counterStep, i), undefined,
+          /* opt_useSetImmediate */ true);
     }
-  };
-  for (var i = 0; i < max; i++) {
-    goog.async.nextTick(goog.partial(counterStep, i), undefined,
-        /* opt_useSetImmediate */ true);
-  }
-  assertTrue(async);
-  asyncTestCase.waitForAsync('Wait for callback');
+    assertTrue(async);
+  });
 }
 
 function testNextTickContext() {
-  var context = {};
-  var c = 0;
-  var max = 10;
-  var async = true;
-  var counterStep = function(i) {
-    async = false;
-    assertEquals('Order correct', i, c);
-    assertEquals(context, this);
-    c++;
-    if (c === max) {
-      asyncTestCase.continueTesting();
+  return new goog.Promise(function(resolve, reject) {
+    var context = {};
+    var c = 0;
+    var max = 10;
+    var async = true;
+    var counterStep = function(i) {
+      async = false;
+      assertEquals('Order correct', i, c);
+      assertEquals(context, this);
+      c++;
+      if (c === max) {
+        resolve();
+      }
+    };
+    for (var i = 0; i < max; i++) {
+      goog.async.nextTick(goog.partial(counterStep, i), context);
     }
-  };
-  for (var i = 0; i < max; i++) {
-    goog.async.nextTick(goog.partial(counterStep, i), context);
-  }
-  assertTrue(async);
-  asyncTestCase.waitForAsync('Wait for callback');
+    assertTrue(async);
+  });
 }
 
 
@@ -125,57 +127,56 @@ function testNextTickMockClock() {
 
 
 function testNextTickDoesntSwallowError() {
-  var before = window.onerror;
-  var sentinel = 'sentinel';
-  window.onerror = function(e) {
-    e = '' + e;
-    // Don't test for contents in IE7, which does not preserve the exception
-    // message.
-    if (e.indexOf('Exception thrown and not caught') == -1) {
-      assertContains(sentinel, e);
-    }
-    window.onerror = before;
-    asyncTestCase.continueTesting();
-    return false;
-  };
-  goog.async.nextTick(function() {
-    throw sentinel;
+  return new goog.Promise(function(resolve, reject) {
+    var sentinel = 'sentinel';
+
+    propertyReplacer.replace(window, 'onerror', function(e) {
+      e = '' + e;
+      // Don't test for contents in IE7, which does not preserve the exception
+      // message.
+      if (e.indexOf('Exception thrown and not caught') == -1) {
+        assertContains(sentinel, e);
+      }
+      resolve();
+      return false;
+    });
+
+    goog.async.nextTick(function() {
+      throw sentinel;
+    });
   });
-  asyncTestCase.waitForAsync('Wait for error');
 }
 
 
 function testNextTickProtectEntryPoint() {
-  var errorHandlerCallbackCalled = false;
-  var errorHandler = new goog.debug.ErrorHandler(function() {
-    errorHandlerCallbackCalled = true;
+  return new goog.Promise(function(resolve, reject) {
+    var errorHandlerCallbackCalled = false;
+    var errorHandler = new goog.debug.ErrorHandler(function() {
+      errorHandlerCallbackCalled = true;
+    });
+
+    // This is only testing wrapping the callback with the protected entry
+    // point, so it's okay to replace this function with a fake.
+    propertyReplacer.set(goog.async.nextTick, 'setImmediate_', function(cb) {
+      try {
+        cb();
+        fail('The callback should have thrown an error.');
+      } catch (e) {
+        assertTrue(errorHandlerCallbackCalled);
+        assertTrue(
+            e instanceof goog.debug.ErrorHandler.ProtectedFunctionError);
+      } finally {
+        // Restore setImmediate so it doesn't interfere with Promise behavior.
+        propertyReplacer.reset();
+      }
+      resolve();
+    });
+
+    goog.debug.entryPointRegistry.monitorAll(errorHandler);
+    goog.async.nextTick(function() {
+      throw Error('This should be caught by the protected function.');
+    });
   });
-
-  // This is only testing wrapping the callback with the protected entry point,
-  // so it's okay to replace this function with a fake.
-  goog.async.nextTick.setImmediate_ = function(cb) {
-    try {
-      cb();
-      fail('The callback should have thrown an error.');
-    } catch (e) {
-      assertTrue(
-          e instanceof goog.debug.ErrorHandler.ProtectedFunctionError);
-    }
-    asyncTestCase.continueTesting();
-  };
-  var origSetImmediate;
-  if (window.setImmediate) {
-    origSetImmediate = window.setImmediate;
-    window.setImmediate = goog.async.nextTick.setImmediate_;
-  }
-  goog.debug.entryPointRegistry.monitorAll(errorHandler);
-
-  function thrower() {
-    throw Error('This should be caught by the protected function.');
-  }
-  asyncTestCase.waitForAsync('Wait for callback');
-  goog.async.nextTick(thrower);
-  window.setImmediate = origSetImmediate;
 }
 
 
@@ -192,13 +193,15 @@ function testNextTick_notStarvedBySetTimeout() {
     }, 0);
   }
   busy();
-  goog.async.nextTick(function() {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-    asyncTestCase.continueTesting();
+
+  return new goog.Promise(function(resolve, reject) {
+    goog.async.nextTick(function() {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      resolve();
+    });
   });
-  asyncTestCase.waitForAsync('Waiting not to starve');
 }
 
 
@@ -214,7 +217,7 @@ function testPostMessagePolyfillDoesNotPumpCallbackQueueIfMessageIsIncorrect() {
     return;
   }
 
-  // Force postMessage pollyfill for setImmediate.
+  // Force postMessage polyfill for setImmediate.
   propertyReplacer.set(window, 'setImmediate', undefined);
   propertyReplacer.set(window, 'MessageChannel', undefined);
 
@@ -232,14 +235,12 @@ function testPostMessagePolyfillDoesNotPumpCallbackQueueIfMessageIsIncorrect() {
     error = e;
   };
 
-  setTimeout(function() {
+  return goog.Timer.promise(0).then(function() {
     assert('Callback should have been called.', callbackCalled);
     assertNull('An unexpected error was thrown.', error);
+  }).thenAlways(function() {
     goog.dom.removeNode(frame);
-    asyncTestCase.continueTesting();
-  }, 0);
-
-  asyncTestCase.waitForAsync('Waiting for callbacks to be invoked.');
+  });
 }
 
 
