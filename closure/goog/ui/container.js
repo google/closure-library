c48f75e73aa6ed740b2ca30ad0bf0002def806c0
Joel Martinez <joeltine@google.com>
2015-06-15 13:35:25 -0700

    Add missing function this type annotation.
    Fixes conformance issues for unknown this in callback.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=96038239

--- closure/goog/ui/container.js
+++ closure/goog/ui/container.js
@@ -1273,8 +1273,9 @@ goog.ui.Container.prototype.highlightPrevious = function() {
 /**
  * Helper function that manages the details of moving the highlight among
  * child controls in response to keyboard events.
- * @param {function(number, number) : number} fn Function that accepts the
- *     current and maximum indices, and returns the next index to check.
+ * @param {function(this: goog.ui.Container, number, number) : number} fn
+ *     Function that accepts the current and maximum indices, and returns the
+ *     next index to check.
  * @param {number} startIndex Start index.
  * @return {boolean} Whether the highlight has changed.
  * @protected
