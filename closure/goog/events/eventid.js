f45dd6cfe35a87e1429333f6ec98635e26714e32
Erik R. Neumann <erikn@myPhysicsLab.com>
2015-11-12 10:14:19 -0800

    use &lt; instead of < in comment
    In goog.events.eventid the comment has sample code which includes text
    that looks like an HTML tag.  The text is:
        /** @type {goog.events.EventId<MyEventObj>}
    When building documentation with js-dossier, this comment generates
    warnings:
        [WARNING][com.github.jsdossier.soy.HtmlSanitizer]
        Discarded tag "myeventobj" in text:
    
    Change to use &lt; instead of < in that comment to prevent these
    warnings.  Additionally, use <pre> tags so that the comment appears
    as a comment block with the original line breaks (instead of being
    run together into a long line of text).  Further, add a zero-width
    space character so that the closing comment symbol */ can be added
    to the sample code.

--- closure/goog/events/eventid.js
+++ closure/goog/events/eventid.js
@@ -14,19 +14,26 @@
 
 goog.provide('goog.events.EventId');
 
+// Note: in the comment below there is a zero-width space \u008b between
+// the * and / in the sample code, so that the */ doesn't end the comment
+// block.
+// To recreate the zero-width space execute in a browser console:
+//      console.log('*\u008b/');
+// and copy/paste the result where needed.
 
 
 /**
  * A templated class that is used when registering for events. Typical usage:
- * <code>
- *   /** @type {goog.events.EventId<MyEventObj>}
- *   var myEventId = new goog.events.EventId(
- *       goog.events.getUniqueId(('someEvent'));
  *
- *   // No need to cast or declare here since the compiler knows the correct
- *   // type of 'evt' (MyEventObj).
- *   something.listen(myEventId, function(evt) {});
- * </code>
+ * <pre>
+ * /** @type {goog.events.EventId&lt;MyEventObj&gt;}*/
+ * var myEventId = new goog.events.EventId(
+ *     goog.events.getUniqueId(('someEvent'));
+ *
+ * // No need to cast or declare here since the compiler knows the
+ * // correct type of 'evt' (MyEventObj).
+ * something.listen(myEventId, function(evt) {});
+ * </pre>
  *
  * @param {string} eventId
  * @template T
