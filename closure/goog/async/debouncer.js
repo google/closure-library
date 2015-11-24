d9ebdf19a5365b59c96c8ae090fc1c5f4438fc82
tbreisacher <tbreisacher@google.com>
2015-11-24 11:20:14 -0800

    Fix the type of Debouncer.args_ to include Arguments. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=108630890

--- closure/goog/async/debouncer.js
+++ closure/goog/async/debouncer.js
@@ -61,7 +61,7 @@ goog.async.Debouncer = function(listener, interval, opt_handler) {
 
   /**
    * Cached callback function invoked after the debounce timeout completes
-   * @type {Function}
+   * @type {!Function}
    * @private
    */
   this.callback_ = goog.bind(this.onTimer_, this);
@@ -91,7 +91,7 @@ goog.async.Debouncer = function(listener, interval, opt_handler) {
 
   /**
    * The last arguments passed into {@code fire}.
-   * @private {!Array}
+   * @private {!Array|!IArrayLike}
    */
   this.args_ = [];
 };
