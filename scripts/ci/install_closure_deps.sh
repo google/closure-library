002f5864ddb91aab5b1d82f65f12377bb8fa86d4
Joel Martinez <joeltine@google.com>
2015-12-18 11:52:13 -0800

    Update project to use new Compiler Linter instead of gjslint

--- scripts/ci/install_closure_deps.sh
+++ scripts/ci/install_closure_deps.sh
@@ -8,11 +8,9 @@ set -ex
 # Install closure compiler and linter.
 cd ..
 git clone --depth 1 https://github.com/google/closure-compiler.git
-git clone --depth 1 https://github.com/google/closure-linter.git
 cd closure-compiler
 ant jar
-cd ../closure-linter
-python ./setup.py install --user
+ant linter
 cd ../closure-library
 
 # Installs node "devDependencies" found in package.json.
