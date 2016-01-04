9a13abe2aefb7c3de3cd16a3aad6cd6eef122789
Joel Martinez <joeltine@google.com>
2016-01-04 15:21:17 -0800

    Clang format unformatted files.

--- browser_capabilities.js
+++ browser_capabilities.js
@@ -2,8 +2,8 @@ var sauceBrowsers = require('./sauce_browsers.json');
 
 function getBrowserName(browserCap) {
   var name = browserCap.browserName == 'internet explorer' ?
-                 'ie' :
-                 browserCap.browserName;
+      'ie' :
+      browserCap.browserName;
   var version = browserCap.version || '-latest';
   return name + version;
 }
@@ -12,9 +12,9 @@ function getJobName(browserCap) {
   var browserName = getBrowserName(browserCap);
 
   return process.env.TRAVIS_PULL_REQUEST == 'false' ?
-             'CO-' + process.env.TRAVIS_BRANCH + '-' + browserName :
-             'PR-' + process.env.TRAVIS_PULL_REQUEST + '-' + browserName + '-' +
-                 process.env.TRAVIS_BRANCH;
+      'CO-' + process.env.TRAVIS_BRANCH + '-' + browserName :
+      'PR-' + process.env.TRAVIS_PULL_REQUEST + '-' + browserName + '-' +
+          process.env.TRAVIS_BRANCH;
 }
 
 function getBrowserCapabilities(browsers) {
