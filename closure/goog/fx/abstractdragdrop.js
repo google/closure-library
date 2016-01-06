c09844090498fffd33de879eab00d75a832fb50d
John Lenz <concavelenz@gmail.com>
2016-01-06 10:58:11 -0800

    Automated g4 rollback of changelist 111522388.
    *** Reason for rollback ***
    
    Bringing original changes back
    
    *** Original change description ***
    
    Automated g4 rollback of changelist 111352260.
    
    *** Reason for rollback ***
    
    Breaks Gmail
    
    *** Original change description ***
    
    Allow further propagation of `mousedown` event for draggable elements.
    
    There is currently an issue when default mousedown event gets cancelled for
    draggable elements.
    Consider an example when you have a draggable div with some
    input inside. If you click on input, it won't get focus and you won't be able
    to type anything, because focus is a default ev...
    
    ***
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=111525196

--- closure/goog/fx/abstractdragdrop.js
+++ closure/goog/fx/abstractdragdrop.js
@@ -1404,8 +1404,6 @@ goog.fx.DragDropItem.prototype.maybeStartDrag_ = function(event, element) {
 
   this.startPosition_ = new goog.math.Coordinate(
       event.clientX, event.clientY);
-
-  event.preventDefault();
 };
 
 
@@ -1432,6 +1430,9 @@ goog.fx.DragDropItem.prototype.mouseMove_ = function(event) {
     this.eventHandler_.removeAll();
     this.parent_.startDrag(event, this);
   }
+
+  // Prevent text selection while dragging an element.
+  event.preventDefault();
 };
 
 
