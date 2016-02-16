768dd56a0b70408c55775feeb08abfd2eef5f33c
John Lenz <concavelenz@gmail.com>
2016-02-16 00:16:03 -0800

    Roll forward now that I am convinced that it is valid.
    Change the implementation of testing.blob.fs to use bytes instead of string.
    This fixes the unicode string length computation.
    
    Automated rollback
    
    *** Reason for rollback ***
    
    Roll forward now that I am convinced that it is valid
    
    *** Original change description ***
    
    Change the implementation of testing.blob.fs to use bytes instead of string.
    
    This fixes the unicode string length computation.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=114727799

--- closure/goog/testing/fs/blob_test.js
+++ closure/goog/testing/fs/blob_test.js
@@ -15,26 +15,59 @@
 goog.provide('goog.testing.fs.BlobTest');
 goog.setTestOnly('goog.testing.fs.BlobTest');
 
+goog.require('goog.dom');
 goog.require('goog.testing.fs.Blob');
 goog.require('goog.testing.jsunit');
 
-function testAttributes() {
-  var blob = new goog.testing.fs.Blob();
-  assertEquals(0, blob.size);
-  assertEquals('', blob.type);
+var hasArrayBuffer = goog.isDef(goog.global.ArrayBuffer);
 
-  blob = new goog.testing.fs.Blob('foo bar baz');
-  assertEquals(11, blob.size);
+function testInput() {
+  var blob = new goog.testing.fs.Blob();
+  assertEquals('', blob.toString());
+  assertEquals(0, blob.size);
+
+  // input is a string
+  blob = new goog.testing.fs.Blob('資');
+  assertEquals('資', blob.toString());
+  assertEquals(3, blob.size);
+
+  if (!hasArrayBuffer) {
+    return;
+  }
+
+  // input is an Array of Arraybuffer.
+  // decimal code for e8 b3 87, that's 資 in UTF-8
+  blob = new goog.testing.fs.Blob([new Uint8Array([232, 179, 135])]);
+  assertEquals('資', blob.toString());
+  assertEquals(3, blob.size);
+
+  // input is an Array of Arraybuffer with control characters.
+  var data = goog.dom.getWindow().atob('iVBORw0KGgo=');
+  var arr = [];
+  for (var i = 0; i < data.length; i++) {
+    arr.push(data.charCodeAt(i));
+  }
+  blob = new goog.testing.fs.Blob([new Uint8Array(arr)]);
+  assertArrayEquals([137, 80, 78, 71, 13, 10, 26, 10], blob.data_);
+  assertEquals(8, blob.size);
+
+  // input is an Array of strings
+  blob = new goog.testing.fs.Blob(['資', 'é']);
+  assertEquals('資é', blob.toString());
+  assertEquals(5, blob.size);
+
+  // input is an Array of Arraybuffer + string
+  blob = new goog.testing.fs.Blob([new Uint8Array([232, 179, 135]), 'é']);
+  assertEquals('資é', blob.toString());
+  assertEquals(5, blob.size);
+}
+
+function testType() {
+  var blob = new goog.testing.fs.Blob();
   assertEquals('', blob.type);
 
   blob = new goog.testing.fs.Blob('foo bar baz', 'text/plain');
-  assertEquals(11, blob.size);
   assertEquals('text/plain', blob.type);
-}
-
-function testToString() {
-  assertEquals('', new goog.testing.fs.Blob().toString());
-  assertEquals('foo bar', new goog.testing.fs.Blob('foo bar').toString());
 }
 
 function testSlice() {
@@ -43,6 +76,7 @@ function testSlice() {
   assertEquals('def', blob.slice(3, 10).toString());
   assertEquals('abcd', blob.slice(0, -2).toString());
   assertEquals('', blob.slice(10, 1).toString());
+  assertEquals('', blob.slice(10, 30).toString());
   assertEquals('b', blob.slice(-5, 2).toString());
 
   assertEquals('abcdef', blob.slice().toString());
@@ -50,16 +84,33 @@ function testSlice() {
   assertEquals('def', blob.slice(3).toString());
 
   assertEquals('text/plain', blob.slice(1, 2, 'text/plain').type);
+
+  blob = new goog.testing.fs.Blob('ab資cd');
+  assertEquals('ab資', blob.slice(0, 5).toString());  // 資 is 3-bytes long.
+  assertEquals('資', blob.slice(2, 5).toString());
+  assertEquals('資cd', blob.slice(2, 10).toString());
+  assertEquals('ab', blob.slice(0, -5).toString());
+  assertEquals('c', blob.slice(-2, -1).toString());
+  assertEquals('資c', blob.slice(-5, -1).toString());
+
+  assertEquals('ab資cd', blob.slice().toString());
+  assertEquals('ab資', blob.slice(/* opt_start */ undefined, 5).toString());
+  assertEquals('cd', blob.slice(5).toString());
+  assertArrayEquals([232], blob.slice(2, 3).data_);  // first byte of 資.
 }
 
-function testSetDataInternal() {
-  var blob = new goog.testing.fs.Blob();
+function testToArrayBuffer() {
+  if (!hasArrayBuffer) {
+    return;
+  }
+  var blob = new goog.testing.fs.Blob('資');
+  var buf = new ArrayBuffer(this.size);
+  var arr = new Uint8Array(buf);
+  arr = [232, 179, 135];
+  assertElementsEquals(buf, blob.toArrayBuffer());
+}
 
-  blob.setDataInternal('asdf');
-  assertEquals('asdf', blob.toString());
-  assertEquals(4, blob.size);
-
-  blob.setDataInternal('');
-  assertEquals('', blob.toString());
-  assertEquals(0, blob.size);
+function testToDataUrl() {
+  var blob = new goog.testing.fs.Blob('資', 'text');
+  assertEquals('data:text;base64,6LOH', blob.toDataUrl());
 }
