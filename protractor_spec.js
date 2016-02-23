ef457ca69b9fcb89b2760e10c5ba3817185d3f89
Joel Martinez <joeltine@users.noreply.github.com>
2016-02-23 10:36:21 -0800

    Disable crossdomainrpc_test
    Flaky on IE

--- protractor_spec.js
+++ protractor_spec.js
@@ -18,7 +18,8 @@ var IGNORED_TESTS = [
   'closure/goog/net/jsloader_test.html',
   'closure/goog/net/filedownloader_test.html',
   'closure/goog/promise/promise_test.html',
-  'closure/goog/editor/plugins/abstractdialogplugin_test.html'
+  'closure/goog/editor/plugins/abstractdialogplugin_test.html',
+  'closure/goog/net/crossdomainrpc_test.html'
 ];
 
 describe('Run all Closure unit tests', function() {
