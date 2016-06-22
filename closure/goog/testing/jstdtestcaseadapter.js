f251c4358e5874cfdc76c08b212a8433f8f2c1d7
Joel Martinez <joeltine@google.com>
2016-06-21 17:10:47 -0700

    Update JsTdTestCaseAdapter such that @suppress {duplicate} annotations are actually considered by the compiler.
    The existing code structure defines a function and assigns it at the same time, making the @suppress {duplicate} present in the JsDocs of the function a no-op for the compiler. This creates a problem for compiled tests that are coming from jstestdriver since javascript/jstestdriver/externs.js is being pulled in.
    
    RELNOTES: Update JsTdTestCaseAdapter such that @suppress {duplicate} annotations are actually considered by the compiler.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=125513510

--- closure/goog/testing/jstdtestcaseadapter.js
+++ closure/goog/testing/jstdtestcaseadapter.js
@@ -61,16 +61,13 @@ goog.testing.JsTdTestCaseAdapter.TestCaseFactory_ = function(
 };
 
 
-// --- conditionally add polyfills for the basic JSTD API ---
-
-
 /**
  * @param {string} testCaseName The name of the test case.
  * @param {?=} opt_proto An optional prototype object for the test case.
  * @return {!Function}
- * @suppress {duplicate}
+ * @private
  */
-var TestCase = TestCase || function(testCaseName, opt_proto) {
+goog.testing.JsTdTestCaseAdapter.TestCase_ = function(testCaseName, opt_proto) {
   return goog.testing.JsTdTestCaseAdapter.TestCaseFactory_(
       testCaseName, goog.functions.TRUE, opt_proto);
 };
@@ -82,22 +79,23 @@ var TestCase = TestCase || function(testCaseName, opt_proto) {
  *     run the tests.
  * @param {?=} opt_proto An optional prototype object for the test case.
  * @return {!Function}
- * @suppress {duplicate}
+ * @private
  */
-var ConditionalTestCase =
-    ConditionalTestCase || function(testCaseName, condition, opt_proto) {
-      return goog.testing.JsTdTestCaseAdapter.TestCaseFactory_(
-          testCaseName, condition, opt_proto);
-    };
+goog.testing.JsTdTestCaseAdapter.ConditionalTestCase_ = function(
+    testCaseName, condition, opt_proto) {
+  return goog.testing.JsTdTestCaseAdapter.TestCaseFactory_(
+      testCaseName, condition, opt_proto);
+};
 
 
 /**
  * @param {string} testCaseName The name of the test case.
  * @param {?=} opt_proto An optional prototype object for the test case.
  * @return {!Function}
- * @suppress {duplicate}
+ * @private
  */
-var AsyncTestCase = AsyncTestCase || function(testCaseName, opt_proto) {
+goog.testing.JsTdTestCaseAdapter.AsyncTestCase_ = function(
+    testCaseName, opt_proto) {
   return goog.testing.JsTdTestCaseAdapter.TestCaseFactory_(
       testCaseName, goog.functions.TRUE, opt_proto, true);
 };
@@ -109,25 +107,40 @@ var AsyncTestCase = AsyncTestCase || function(testCaseName, opt_proto) {
  *     run the tests.
  * @param {?=} opt_proto An optional prototype object for the test case.
  * @return {!Function}
- * @suppress {duplicate}
+ * @private
  */
-var AsyncConditionalTestCase =
-    AsyncConditionalTestCase || function(testCaseName, condition, opt_proto) {
-      return goog.testing.JsTdTestCaseAdapter.TestCaseFactory_(
-          testCaseName, condition, opt_proto, true);
-    };
+goog.testing.JsTdTestCaseAdapter.AsyncConditionalTestCase_ = function(
+    testCaseName, condition, opt_proto) {
+  return goog.testing.JsTdTestCaseAdapter.TestCaseFactory_(
+      testCaseName, condition, opt_proto, true);
+};
 
 
-/**
- * @param {string} testCaseName The name of the test case.
- * @param {function(): boolean} condition A condition to determine whether to
- *     run the tests.
- * @param {?=} opt_proto An optional prototype object for the test case.
- * @return {!Function}
- * @suppress {duplicate}
- */
-var ConditionalAsyncTestCase =
-    ConditionalAsyncTestCase || AsyncConditionalTestCase;
+// --- conditionally add polyfills for the basic JSTD API ---
+
+
+/** @suppress {duplicate} */
+var TestCase = TestCase || goog.testing.JsTdTestCaseAdapter.TestCase_;
+
+
+/** @suppress {duplicate} */
+var ConditionalTestCase = ConditionalTestCase ||
+    goog.testing.JsTdTestCaseAdapter.ConditionalTestCase_;
+
+
+/** @suppress {duplicate} */
+var AsyncTestCase =
+    AsyncTestCase || goog.testing.JsTdTestCaseAdapter.AsyncTestCase_;
+
+
+/** @suppress {duplicate} */
+var AsyncConditionalTestCase = AsyncConditionalTestCase ||
+    goog.testing.JsTdTestCaseAdapter.AsyncConditionalTestCase_;
+
+
+/** @suppress {duplicate} */
+var ConditionalAsyncTestCase = ConditionalAsyncTestCase ||
+    goog.testing.JsTdTestCaseAdapter.AsyncConditionalTestCase_;
 
 
 // The API is also available under the jstestdriver namespace.
