c6c579a445d2a6415120753314f701eb0ae989bc
Joel Martinez <joeltine@google.com>
2016-06-23 19:30:14 -0700

    Accessibility improvements for thumb slider follows instructions given: https://www.w3.org/TR/wai-aria-practices/#slider https://www.w3.org/TR/wai-aria-practices/#slidertwothumb
    Sets the ARIA roles, states, properties on the thumbs.
    
    RELNOTES: For slider and two thumbs slider: TAB focus is set on the
    thumbs instead of the highlighted slider handle. For two thumbs slider: Key events (HOME, END, LEFT arrow, RIGHT arrow...) interacts on the focused thumb (the start thumb or the end thumb) other than the highlighted slider handle.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=125745734

--- closure/goog/ui/sliderbase_test.js
+++ closure/goog/ui/sliderbase_test.js
@@ -17,6 +17,7 @@ goog.setTestOnly('goog.ui.SliderBaseTest');
 
 goog.require('goog.a11y.aria');
 goog.require('goog.a11y.aria.State');
+goog.require('goog.asserts');
 goog.require('goog.dom');
 goog.require('goog.dom.TagName');
 goog.require('goog.dom.classlist');
@@ -75,6 +76,31 @@ OneThumbSlider.prototype.getCssClass = function(orientation) {
 };
 
 
+/**
+ * A basic class to implement the abstract goog.ui.SliderBase for testing.
+ * @constructor
+ * @extends {goog.ui.SliderBase}
+ */
+function OneThumbSliderRtl() {
+  goog.ui.SliderBase.call(this, undefined /* domHelper */, function(value) {
+    return value > 5 ? 'A big value.' : 'A small value.';
+  });
+}
+goog.inherits(OneThumbSliderRtl, goog.ui.SliderBase);
+
+
+/** @override */
+OneThumbSliderRtl.prototype.createThumbs = function() {
+  this.valueThumb = this.extentThumb = goog.dom.getElement('thumbRtl');
+};
+
+
+/** @override */
+OneThumbSliderRtl.prototype.getCssClass = function(orientation) {
+  return goog.getCssName('test-slider', orientation);
+};
+
+
 
 /**
  * A basic class to implement the abstract goog.ui.SliderBase for testing.
@@ -97,6 +123,32 @@ TwoThumbSlider.prototype.createThumbs = function() {
 
 /** @override */
 TwoThumbSlider.prototype.getCssClass = function(orientation) {
+  return goog.getCssName('test-slider', orientation);
+};
+
+
+
+/**
+ * A basic class to implement the abstract goog.ui.SliderBase for testing.
+ * @constructor
+ * @extends {goog.ui.SliderBase}
+ */
+function TwoThumbSliderRtl() {
+  goog.ui.SliderBase.call(this);
+}
+goog.inherits(TwoThumbSliderRtl, goog.ui.SliderBase);
+
+
+/** @override */
+TwoThumbSliderRtl.prototype.createThumbs = function() {
+  this.valueThumb = goog.dom.getElement('valueThumbRtl');
+  this.extentThumb = goog.dom.getElement('extentThumbRtl');
+  this.rangeHighlight = goog.dom.getElement('rangeHighlightRtl');
+};
+
+
+/** @override */
+TwoThumbSliderRtl.prototype.getCssClass = function(orientation) {
   return goog.getCssName('test-slider', orientation);
 };
 
@@ -156,7 +208,7 @@ function setUp() {
       goog.dom.TagName.DIV, {'id': 'oneThumbSliderRtl'},
       goog.dom.createDom(goog.dom.TagName.SPAN, {'id': 'thumbRtl'}));
   sandBoxRtl.appendChild(oneThumbElemRtl);
-  oneThumbSliderRtl = new OneThumbSlider();
+  oneThumbSliderRtl = new OneThumbSliderRtl();
   oneThumbSliderRtl.enableFlipForRtl(true);
   oneThumbSliderRtl.decorate(oneThumbElemRtl);
   goog.events.listen(
@@ -169,7 +221,7 @@ function setUp() {
       goog.dom.createDom(goog.dom.TagName.SPAN, {'id': 'valueThumbRtl'}),
       goog.dom.createDom(goog.dom.TagName.SPAN, {'id': 'extentThumbRtl'}));
   sandBoxRtl.appendChild(twoThumbElemRtl);
-  twoThumbSliderRtl = new TwoThumbSlider();
+  twoThumbSliderRtl = new TwoThumbSliderRtl();
   twoThumbSliderRtl.enableFlipForRtl(true);
   twoThumbSliderRtl.decorate(twoThumbElemRtl);
   twoChangeEventCount = 0;
@@ -238,7 +290,7 @@ function testGetAndSetValueRtl() {
   var thumbElement = goog.dom.getElement('thumbRtl');
   assertEquals(0, goog.style.bidi.getOffsetStart(thumbElement));
   assertEquals('', thumbElement.style.left);
-  assertTrue(thumbElement.style.right >= 0);
+  assertEquals('0px', thumbElement.style.right);
 
   oneThumbSliderRtl.setValue(30);
   assertEquals(30, oneThumbSliderRtl.getValue());
@@ -247,7 +299,7 @@ function testGetAndSetValueRtl() {
       oneChangeEventCount);
 
   assertEquals('', thumbElement.style.left);
-  assertTrue(thumbElement.style.right >= 0);
+  assertEquals('0px', thumbElement.style.right);
 
   oneThumbSliderRtl.setValue(30);
   assertEquals(30, oneThumbSliderRtl.getValue());
@@ -271,9 +323,9 @@ function testGetAndSetValueRtl() {
   assertEquals(0, goog.style.bidi.getOffsetStart(valueThumbElement));
   assertEquals(0, goog.style.bidi.getOffsetStart(extentThumbElement));
   assertEquals('', valueThumbElement.style.left);
-  assertTrue(valueThumbElement.style.right >= 0);
+  assertEquals('0px', valueThumbElement.style.right);
   assertEquals('', extentThumbElement.style.left);
-  assertTrue(extentThumbElement.style.right >= 0);
+  assertEquals('0px', extentThumbElement.style.right);
 
   twoThumbSliderRtl.setExtent(70);
   twoChangeEventCount = 0;
@@ -290,9 +342,9 @@ function testGetAndSetValueRtl() {
       twoChangeEventCount);
 
   assertEquals('', valueThumbElement.style.left);
-  assertTrue(valueThumbElement.style.right >= 0);
+  assertEquals('0px', valueThumbElement.style.right);
   assertEquals('', extentThumbElement.style.left);
-  assertTrue(extentThumbElement.style.right >= 0);
+  assertEquals('0px', extentThumbElement.style.right);
 
   twoThumbSliderRtl.setValue(-60);
   assertEquals(
@@ -331,26 +383,25 @@ function testGetAndSetExtent() {
 function testUpdateValueExtent() {
   twoThumbSlider.setValueAndExtent(30, 50);
 
-  assertNotNull(twoThumbSlider.getElement());
+  assertNotNull(twoThumbSlider.valueThumb);
   assertEquals(
       'Setting value results in updating aria-valuenow', '30',
       goog.a11y.aria.getState(
-          twoThumbSlider.getElement(), goog.a11y.aria.State.VALUENOW));
+          twoThumbSlider.valueThumb, goog.a11y.aria.State.VALUENOW));
   assertEquals(30, twoThumbSlider.getValue());
   assertEquals(50, twoThumbSlider.getExtent());
 }
 
 function testValueText() {
   oneThumbSlider.setValue(10);
+  var valueThumb = goog.asserts.assertElement(oneThumbSlider.valueThumb);
   assertEquals(
       'Setting value results in correct aria-valuetext', 'A big value.',
-      goog.a11y.aria.getState(
-          oneThumbSlider.getElement(), goog.a11y.aria.State.VALUETEXT));
+      goog.a11y.aria.getState(valueThumb, goog.a11y.aria.State.VALUETEXT));
   oneThumbSlider.setValue(2);
   assertEquals(
       'Updating value results in updated aria-valuetext', 'A small value.',
-      goog.a11y.aria.getState(
-          oneThumbSlider.getElement(), goog.a11y.aria.State.VALUETEXT));
+      goog.a11y.aria.getState(valueThumb, goog.a11y.aria.State.VALUETEXT));
 }
 
 function testGetValueText() {
@@ -407,133 +458,200 @@ function assertHighlightedRange(rangeHighlight, startValue, endValue) {
       rangeSize, rangeHighlight.offsetWidth);
 }
 
+
+/**
+ * @param {!Element} element
+ * @param {number} min
+ * @param {number} now
+ * @param {number} max
+ * @param {string} text
+ */
+function assertAriaState(element, min, now, max, text) {
+  assertEquals(
+      min + '',
+      goog.a11y.aria.getState(element, goog.a11y.aria.State.VALUEMIN));
+  assertEquals(
+      max + '',
+      goog.a11y.aria.getState(element, goog.a11y.aria.State.VALUEMAX));
+  assertEquals(
+      now + '',
+      goog.a11y.aria.getState(element, goog.a11y.aria.State.VALUENOW));
+  assertEquals(
+      text, goog.a11y.aria.getState(element, goog.a11y.aria.State.VALUETEXT));
+}
+
 function testKeyHandlingTests() {
-  twoThumbSlider.setValue(0);
-  twoThumbSlider.setExtent(100);
-  assertEquals(0, twoThumbSlider.getValue());
-  assertEquals(100, twoThumbSlider.getExtent());
+  oneThumbSlider.setMinimum(0);
+  oneThumbSlider.setMaximum(100);
+  oneThumbSlider.setValue(20);
+  oneThumbSlider.setStep(5);
+  oneThumbSlider.setBlockIncrement(10);
+  oneThumbSlider.setLabelFn(function(number) { return number + ' kg'; });
+
+  var thumb = goog.asserts.assertElement(oneThumbSlider.valueThumb);
+  assertAriaState(thumb, 0, 20, 100, '20 kg');
+
+  thumb.focus();
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.RIGHT);
+  assertAriaState(thumb, 0, 25, 100, '25 kg');
+
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.LEFT);
+  assertAriaState(thumb, 0, 20, 100, '20 kg');
 
   goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.RIGHT);
-  assertEquals(1, twoThumbSlider.getValue());
-  assertEquals(99, twoThumbSlider.getExtent());
+      thumb, goog.events.KeyCodes.LEFT, {shiftKey: true});
+  assertAriaState(thumb, 0, 10, 100, '10 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.RIGHT);
-  assertEquals(2, twoThumbSlider.getValue());
-  assertEquals(98, twoThumbSlider.getExtent());
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.HOME);
+  assertAriaState(thumb, 0, 0, 100, '0 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.LEFT);
-  assertEquals(1, twoThumbSlider.getValue());
-  assertEquals(98, twoThumbSlider.getExtent());
-
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.LEFT);
-  assertEquals(0, twoThumbSlider.getValue());
-  assertEquals(98, twoThumbSlider.getExtent());
-
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.RIGHT,
-      {shiftKey: true});
-  assertEquals(10, twoThumbSlider.getValue());
-  assertEquals(90, twoThumbSlider.getExtent());
-
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.RIGHT,
-      {shiftKey: true});
-  assertEquals(20, twoThumbSlider.getValue());
-  assertEquals(80, twoThumbSlider.getExtent());
-
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.LEFT, {shiftKey: true});
-  assertEquals(10, twoThumbSlider.getValue());
-  assertEquals(80, twoThumbSlider.getExtent());
-
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.LEFT, {shiftKey: true});
-  assertEquals(0, twoThumbSlider.getValue());
-  assertEquals(80, twoThumbSlider.getExtent());
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.END);
+  assertAriaState(thumb, 0, 100, 100, '100 kg');
 }
 
-function testKeyHandlingLargeStepSize() {
-  twoThumbSlider.setValue(0);
-  twoThumbSlider.setExtent(100);
+
+function testKeyHandlingTestsRtl() {
+  oneThumbSliderRtl.setMinimum(0);
+  oneThumbSliderRtl.setMaximum(100);
+  oneThumbSliderRtl.setValue(20);
+  oneThumbSliderRtl.setStep(5);
+  oneThumbSliderRtl.setBlockIncrement(10);
+  oneThumbSliderRtl.setLabelFn(function(number) { return number + ' kg'; });
+
+  var thumb = goog.asserts.assertElement(oneThumbSliderRtl.valueThumb);
+  assertAriaState(thumb, 0, 20, 100, '20 kg');
+
+  thumb.focus();
+
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.LEFT);
+  assertAriaState(thumb, 0, 25, 100, '25 kg');
+
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.RIGHT);
+  assertAriaState(thumb, 0, 20, 100, '20 kg');
+
+  goog.testing.events.fireKeySequence(
+      thumb, goog.events.KeyCodes.RIGHT, {shiftKey: true});
+  assertAriaState(thumb, 0, 10, 100, '10 kg');
+
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.HOME);
+  assertAriaState(thumb, 0, 0, 100, '0 kg');
+
+  goog.testing.events.fireKeySequence(thumb, goog.events.KeyCodes.END);
+  assertAriaState(thumb, 0, 100, 100, '100 kg');
+}
+
+
+function testKeyHandlingTestsTwoThumb() {
+  twoThumbSlider.setMinimum(0);
+  twoThumbSlider.setMaximum(100);
+  twoThumbSlider.setMinExtent(10);
+  twoThumbSlider.setValueAndExtent(20, 40);
   twoThumbSlider.setStep(5);
-  assertEquals(0, twoThumbSlider.getValue());
-  assertEquals(100, twoThumbSlider.getExtent());
+  twoThumbSlider.setBlockIncrement(10);
+  twoThumbSlider.setLabelFn(function(number) { return number + ' kg'; });
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.RIGHT);
-  assertEquals(5, twoThumbSlider.getValue());
-  assertEquals(95, twoThumbSlider.getExtent());
+  var thumbStart = goog.asserts.assertElement(twoThumbSlider.valueThumb);
+  var thumbEnd = goog.asserts.assertElement(twoThumbSlider.extentThumb);
+  assertAriaState(thumbStart, 0, 20, 50, '20 kg');
+  assertAriaState(thumbEnd, 30, 60, 100, '60 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.RIGHT);
-  assertEquals(10, twoThumbSlider.getValue());
-  assertEquals(90, twoThumbSlider.getExtent());
+  thumbStart.focus();
+  goog.testing.events.fireKeySequence(thumbStart, goog.events.KeyCodes.RIGHT);
+  assertAriaState(thumbStart, 0, 25, 50, '25 kg');
+  assertAriaState(thumbEnd, 35, 60, 100, '60 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.LEFT);
-  assertEquals(5, twoThumbSlider.getValue());
-  assertEquals(90, twoThumbSlider.getExtent());
+  thumbEnd.focus();
+  goog.testing.events.fireKeySequence(thumbEnd, goog.events.KeyCodes.RIGHT);
+  assertAriaState(thumbStart, 0, 25, 55, '25 kg');
+  assertAriaState(thumbEnd, 35, 65, 100, '65 kg');
 
+  thumbStart.focus();
+  goog.testing.events.fireKeySequence(thumbStart, goog.events.KeyCodes.LEFT);
+  assertAriaState(thumbStart, 0, 20, 55, '20 kg');
+  assertAriaState(thumbEnd, 30, 65, 100, '65 kg');
+
+  thumbEnd.focus();
+  goog.testing.events.fireKeySequence(thumbEnd, goog.events.KeyCodes.LEFT);
+  assertAriaState(thumbStart, 0, 20, 50, '20 kg');
+  assertAriaState(thumbEnd, 30, 60, 100, '60 kg');
+
+  thumbStart.focus();
   goog.testing.events.fireKeySequence(
-      twoThumbSlider.getElement(), goog.events.KeyCodes.LEFT);
-  assertEquals(0, twoThumbSlider.getValue());
-  assertEquals(90, twoThumbSlider.getExtent());
+      thumbStart, goog.events.KeyCodes.LEFT, {shiftKey: true});
+  assertAriaState(thumbStart, 0, 10, 50, '10 kg');
+  assertAriaState(thumbEnd, 20, 60, 100, '60 kg');
+
+  goog.testing.events.fireKeySequence(thumbStart, goog.events.KeyCodes.HOME);
+  assertAriaState(thumbStart, 0, 0, 50, '0 kg');
+  assertAriaState(thumbEnd, 10, 60, 100, '60 kg');
+
+  thumbEnd.focus();
+  goog.testing.events.fireKeySequence(
+      thumbEnd, goog.events.KeyCodes.LEFT, {shiftKey: true});
+  assertAriaState(thumbStart, 0, 0, 40, '0 kg');
+  assertAriaState(thumbEnd, 10, 50, 100, '50 kg');
+
+  goog.testing.events.fireKeySequence(thumbEnd, goog.events.KeyCodes.END);
+  assertAriaState(thumbStart, 0, 0, 90, '0 kg');
+  assertAriaState(thumbEnd, 10, 100, 100, '100 kg');
 }
 
-function testKeyHandlingRtl() {
-  twoThumbSliderRtl.setValue(0);
-  twoThumbSliderRtl.setExtent(100);
-  assertEquals(0, twoThumbSliderRtl.getValue());
-  assertEquals(100, twoThumbSliderRtl.getExtent());
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.RIGHT);
-  assertEquals(0, twoThumbSliderRtl.getValue());
-  assertEquals(99, twoThumbSliderRtl.getExtent());
+function testKeyHandlingTestsRtlTwoThumb() {
+  twoThumbSliderRtl.setMinimum(0);
+  twoThumbSliderRtl.setMaximum(100);
+  twoThumbSliderRtl.setMinExtent(10);
+  twoThumbSliderRtl.setValueAndExtent(20, 40);
+  twoThumbSliderRtl.setStep(5);
+  twoThumbSliderRtl.setBlockIncrement(10);
+  twoThumbSliderRtl.setLabelFn(function(number) { return number + ' kg'; });
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.RIGHT);
-  assertEquals(0, twoThumbSliderRtl.getValue());
-  assertEquals(98, twoThumbSliderRtl.getExtent());
+  var thumbStart = goog.asserts.assertElement(twoThumbSliderRtl.valueThumb);
+  var thumbEnd = goog.asserts.assertElement(twoThumbSliderRtl.extentThumb);
+  assertAriaState(thumbStart, 0, 20, 50, '20 kg');
+  assertAriaState(thumbEnd, 30, 60, 100, '60 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.LEFT);
-  assertEquals(1, twoThumbSliderRtl.getValue());
-  assertEquals(98, twoThumbSliderRtl.getExtent());
+  thumbStart.focus();
+  goog.testing.events.fireKeySequence(thumbStart, goog.events.KeyCodes.LEFT);
+  assertAriaState(thumbStart, 0, 25, 50, '25 kg');
+  assertAriaState(thumbEnd, 35, 60, 100, '60 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.LEFT);
-  assertEquals(2, twoThumbSliderRtl.getValue());
-  assertEquals(98, twoThumbSliderRtl.getExtent());
+  thumbEnd.focus();
+  goog.testing.events.fireKeySequence(thumbEnd, goog.events.KeyCodes.LEFT);
+  assertAriaState(thumbStart, 0, 25, 55, '25 kg');
+  assertAriaState(thumbEnd, 35, 65, 100, '65 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.RIGHT,
-      {shiftKey: true});
-  assertEquals(0, twoThumbSliderRtl.getValue());
-  assertEquals(90, twoThumbSliderRtl.getExtent());
+  thumbStart.focus();
+  goog.testing.events.fireKeySequence(thumbStart, goog.events.KeyCodes.RIGHT);
+  assertAriaState(thumbStart, 0, 20, 55, '20 kg');
+  assertAriaState(thumbEnd, 30, 65, 100, '65 kg');
 
-  goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.RIGHT,
-      {shiftKey: true});
-  assertEquals(0, twoThumbSliderRtl.getValue());
-  assertEquals(80, twoThumbSliderRtl.getExtent());
+  thumbEnd.focus();
+  goog.testing.events.fireKeySequence(thumbEnd, goog.events.KeyCodes.RIGHT);
+  assertAriaState(thumbStart, 0, 20, 50, '20 kg');
+  assertAriaState(thumbEnd, 30, 60, 100, '60 kg');
 
+  thumbStart.focus();
   goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.LEFT,
-      {shiftKey: true});
-  assertEquals(10, twoThumbSliderRtl.getValue());
-  assertEquals(80, twoThumbSliderRtl.getExtent());
+      thumbStart, goog.events.KeyCodes.RIGHT, {shiftKey: true});
+  assertAriaState(thumbStart, 0, 10, 50, '10 kg');
+  assertAriaState(thumbEnd, 20, 60, 100, '60 kg');
 
+  goog.testing.events.fireKeySequence(thumbStart, goog.events.KeyCodes.HOME);
+  assertAriaState(thumbStart, 0, 0, 50, '0 kg');
+  assertAriaState(thumbEnd, 10, 60, 100, '60 kg');
+
+  thumbEnd.focus();
   goog.testing.events.fireKeySequence(
-      twoThumbSliderRtl.getElement(), goog.events.KeyCodes.LEFT,
-      {shiftKey: true});
-  assertEquals(20, twoThumbSliderRtl.getValue());
-  assertEquals(80, twoThumbSliderRtl.getExtent());
+      thumbEnd, goog.events.KeyCodes.RIGHT, {shiftKey: true});
+  assertAriaState(thumbStart, 0, 0, 40, '0 kg');
+  assertAriaState(thumbEnd, 10, 50, 100, '50 kg');
+
+  goog.testing.events.fireKeySequence(thumbEnd, goog.events.KeyCodes.END);
+  assertAriaState(thumbStart, 0, 0, 90, '0 kg');
+  assertAriaState(thumbEnd, 10, 100, 100, '100 kg');
 }
+
 
 function testRangeHighlight() {
   var rangeHighlight = goog.dom.getElement('rangeHighlight');
