005bc11d36205f3a1a16a3c730285ecfaea5abe8
danesh <danesh@google.com>
2016-07-07 10:11:38 -0700

    Remove package visibility restriction from goog.html.sanitizer.CssSanitizer.sanitizeInlineStyle and make it return a SafeStyle.
    RELNOTES[NEW]: Make goog.html.sanitizer.CssSanitizer.sanitizeInlineStyle public.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=126817772

--- closure/goog/html/sanitizer/csssanitizer_test.js
+++ closure/goog/html/sanitizer/csssanitizer_test.js
@@ -17,6 +17,7 @@
 goog.setTestOnly();
 
 goog.require('goog.array');
+goog.require('goog.html.SafeStyle');
 goog.require('goog.html.SafeUrl');
 goog.require('goog.html.sanitizer.CssSanitizer');
 goog.require('goog.string');
@@ -89,8 +90,9 @@ function assertCSSTextEquals(expectedCssText, actualCssText) {
  */
 function getSanitizedInlineStyle(sourceCss, opt_urlRewrite) {
   try {
-    return goog.html.sanitizer.CssSanitizer.sanitizeInlineStyle(
-               getStyleFromCssText(sourceCss), opt_urlRewrite) ||
+    return goog.html.SafeStyle.unwrap(
+               goog.html.sanitizer.CssSanitizer.sanitizeInlineStyle(
+                   getStyleFromCssText(sourceCss), opt_urlRewrite)) ||
         '';
   } catch (err) {
     // IE8 doesn't like setting invalid properties. It throws an "Invalid
