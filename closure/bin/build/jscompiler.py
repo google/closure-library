a6e579df9016cf21b785a31946c32706bf0d217a
Cody Raspen <cody+github@raspen.org>
2017-03-16 14:01:16 -0700

    Accommodate parsing of java version strings >= JDK 9
    Clases #796
    
    RELNOTES: n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=150365283

--- closure/bin/build/jscompiler.py
+++ closure/bin/build/jscompiler.py
@@ -20,9 +20,9 @@ import subprocess
 import tempfile
 
 # Pulls just the major and minor version numbers from the first line of
-# 'java -version'. Versions are in the format of [0-9]+\.[0-9]+\..* See:
-# http://www.oracle.com/technetwork/java/javase/versioning-naming-139433.html
-_VERSION_REGEX = re.compile(r'"([0-9]+)\.([0-9]+)')
+# 'java -version'. Versions are in the format of [0-9]+(\.[0-9]+)? See:
+# http://openjdk.java.net/jeps/223
+_VERSION_REGEX = re.compile(r'"([0-9]+)(?:\.([0-9]+))?')
 
 
 class JsCompilerError(Exception):
@@ -46,7 +46,7 @@ def _ParseJavaVersion(version_string):
   """
   match = _VERSION_REGEX.search(version_string)
   if match:
-    version = tuple(int(x, 10) for x in match.groups())
+    version = tuple(int(x or 0) for x in match.groups())
     assert len(version) == 2
     return version
 
@@ -58,9 +58,8 @@ def _JavaSupports32BitMode():
   supported = False
   try:
     devnull = open(os.devnull, 'wb')
-    return subprocess.call(['java', '-d32', '-version'],
-                           stdout=devnull,
-                           stderr=devnull) == 0
+    return subprocess.call(
+        ['java', '-d32', '-version'], stdout=devnull, stderr=devnull) == 0
   except IOError:
     pass
   else:
