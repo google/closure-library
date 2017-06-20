505a761000cec837ba87eaf50f62fc89c6fcafdd
jakubvrana <jakubvrana@google.com>
2017-06-20 08:49:57 -0700

    Use goog.style.installSafeStyleSheet.
    goog.style.installStyles uses a legacy conversion and will be banned. This CL changes the code so that it continues working after the ban without changing the meaning of the code.
    
    Depends on release of cl/157699694.
    
    Tested:
        Some tests failed; test failures are believed to be unrelated to this CL
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=159565817

--- closure/goog/ui/tabpane.js
+++ closure/goog/ui/tabpane.js
@@ -33,6 +33,7 @@ goog.require('goog.events.Event');
 goog.require('goog.events.EventTarget');
 goog.require('goog.events.EventType');
 goog.require('goog.events.KeyCodes');
+goog.require('goog.html.SafeStyleSheet');
 goog.require('goog.style');
 
 
@@ -218,9 +219,10 @@ goog.ui.TabPane.prototype.create_ = function() {
  * @private
  */
 goog.ui.TabPane.prototype.createClear_ = function() {
-  var clearFloatStyle = '.' + goog.getCssName('goog-tabpane-clear') +
-      ' { clear: both; height: 0px; overflow: hidden }';
-  goog.style.installStyles(clearFloatStyle);
+  var clearFloatStyle = goog.html.SafeStyleSheet.createRule(
+      '.' + goog.getCssName('goog-tabpane-clear'),
+      {'clear': 'both', 'height': '0', 'overflow': 'hidden'});
+  goog.style.installSafeStyleSheet(clearFloatStyle);
   return this.dom_.createDom(
       goog.dom.TagName.DIV, goog.getCssName('goog-tabpane-clear'));
 };
