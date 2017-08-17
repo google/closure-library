cf52c1fa9390498337365f11f6379e330cd9f789
bangert <bangert@google.com>
2017-08-17 16:11:35 -0700

    Replace .href assignments with a safe wrapper that filters out all absolute URLs with schemas that are not “http”, “https”, “mailto” or “ftp”. Potentially unsafe URL schemas, e.g. “javascript:”, “about:”  or “data:” will raise an assertion error when running with goog.DEBUG and will be replaced by a harmless URL when running without assertions.
    If you receive this CL, all of your tests pass without raising this assertion, indicating that all code exercised by your tests behaves the same after this CL. If your code is supposed to assign URLs with other schemas such as  “javascript:” or “data:” to href properties without any of your tests running this code path, please respond with a CL comment on the assignment statements.
    
    Unsafe URLs can lead to cross-site-scripting security vulnerabilities. 10% of all high-risk JS security bugs were due to assigning attacker-controlled URLs to href properties without sanitization. Sanitizing URLs makes URL injection not exploitable.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=165643686

--- closure/goog/editor/plugins/linkdialogplugin.js
+++ closure/goog/editor/plugins/linkdialogplugin.js
@@ -22,7 +22,9 @@
 goog.provide('goog.editor.plugins.LinkDialogPlugin');
 
 goog.require('goog.array');
+goog.require('goog.asserts');
 goog.require('goog.dom');
+goog.require('goog.dom.safe');
 goog.require('goog.editor.Command');
 goog.require('goog.editor.plugins.AbstractDialogPlugin');
 goog.require('goog.events.EventHandler');
@@ -331,7 +333,8 @@ goog.editor.plugins.LinkDialogPlugin.prototype.handleOk = function(e) {
   this.touchUpAnchorOnOk_(anchor, e);
   var extraAnchors = this.currentLink_.getExtraAnchors();
   for (var i = 0; i < extraAnchors.length; ++i) {
-    extraAnchors[i].href = anchor.href;
+    goog.dom.safe.setAnchorHref(
+        goog.asserts.assert(extraAnchors[i]), anchor.href);
     this.touchUpAnchorOnOk_(extraAnchors[i], e);
   }
 
