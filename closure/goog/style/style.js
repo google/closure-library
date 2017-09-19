3ea1bed0de97b1026db8205d580e2844bccbe38a
slaks <slaks@google.com>
2017-09-19 12:32:43 -0700

    RELNOTES: Tighten the return type of installSafeStyleSheet().
    And explain why it has such an annoying return type that cannot be simplified.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=169281941

--- closure/goog/style/style.js
+++ closure/goog/style/style.js
@@ -1279,18 +1279,23 @@ goog.style.isElementShown = function(el) {
  * @param {!goog.html.SafeStyleSheet} safeStyleSheet The style sheet to install.
  * @param {?Node=} opt_node Node whose parent document should have the
  *     styles installed.
- * @return {!Element|!StyleSheet} The style element created.
+ * @return {!HTMLStyleElement|!StyleSheet} In IE<11, a StyleSheet object with no
+ *     owning <style> tag (this is how IE creates style sheets).  In every other
+ *     browser, a <style> element with an attached style.  This doesn't return a
+ *     StyleSheet object so that setSafeStyleSheet can replace it (otherwise, if
+ *     you pass a StyleSheet to setSafeStyleSheet, it will make a new StyleSheet
+ *     and leave the original StyleSheet orphaned).
  */
 goog.style.installSafeStyleSheet = function(safeStyleSheet, opt_node) {
   var dh = goog.dom.getDomHelper(opt_node);
-  var styleSheet = null;
 
   // IE < 11 requires createStyleSheet. Note that doc.createStyleSheet will be
   // undefined as of IE 11.
   var doc = dh.getDocument();
   if (goog.userAgent.IE && doc.createStyleSheet) {
-    styleSheet = doc.createStyleSheet();
+    var styleSheet = doc.createStyleSheet();
     goog.style.setSafeStyleSheet(styleSheet, safeStyleSheet);
+    return styleSheet;
   } else {
     var head = dh.getElementsByTagNameAndClass(goog.dom.TagName.HEAD)[0];
 
@@ -1301,15 +1306,15 @@ goog.style.installSafeStyleSheet = function(safeStyleSheet, opt_node) {
       head = dh.createDom(goog.dom.TagName.HEAD);
       body.parentNode.insertBefore(head, body);
     }
-    styleSheet = dh.createDom(goog.dom.TagName.STYLE);
+    var el = dh.createDom(goog.dom.TagName.STYLE);
     // NOTE(user): Setting styles after the style element has been appended
     // to the head results in a nasty Webkit bug in certain scenarios. Please
     // refer to https://bugs.webkit.org/show_bug.cgi?id=26307 for additional
     // details.
-    goog.style.setSafeStyleSheet(styleSheet, safeStyleSheet);
-    dh.appendChild(head, styleSheet);
+    goog.style.setSafeStyleSheet(el, safeStyleSheet);
+    dh.appendChild(head, el);
+    return el;
   }
-  return styleSheet;
 };
 
 
