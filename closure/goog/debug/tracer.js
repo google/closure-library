e1d02f3df8059a7940964a662ee9efa0383025fa
John Plaisted <johnplaisted@google.com>
2017-11-06 07:54:37 -0800

    Add a listener hook to the Closure Tracer class.
    RELNOTES[NEW]: Add callback support to goog.debug.Trace for its following functions: startTrace, stopTrace, and addComment
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=174705491

--- closure/goog/debug/tracer.js
+++ closure/goog/debug/tracer.js
@@ -21,6 +21,7 @@
 goog.provide('goog.debug.Trace');
 
 goog.require('goog.array');
+goog.require('goog.asserts');
 goog.require('goog.debug.Logger');
 goog.require('goog.iter');
 goog.require('goog.log');
@@ -33,6 +34,8 @@ goog.require('goog.structs.SimplePool');
  * Class used for singleton goog.debug.Trace.  Used for timing slow points in
  * the code. Based on the java Tracer class but optimized for javascript.
  * See com.google.common.tracing.Tracer.
+ * It is also possible to bridge from this class to other tracer classes via
+ * adding listeners.
  * @constructor
  * @private
  */
@@ -40,73 +43,63 @@ goog.debug.Trace_ = function() {
 
   /**
    * Events in order.
-   * @type {Array<goog.debug.Trace_.Event_>}
-   * @private
+   * @private {!Array<!goog.debug.Trace_.Event_>}
    */
   this.events_ = [];
 
   /**
    * Outstanding events that have started but haven't yet ended. The keys are
    * numeric ids and the values are goog.debug.Trace_.Event_ objects.
-   * @type {goog.structs.Map}
-   * @private
+   * @private {!goog.structs.Map<number, !goog.debug.Trace_.Event_>}
    */
   this.outstandingEvents_ = new goog.structs.Map();
 
   /**
    * Start time of the event trace
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.startTime_ = 0;
 
   /**
    * Cummulative overhead of calls to startTracer
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.tracerOverheadStart_ = 0;
 
   /**
    * Cummulative overhead of calls to endTracer
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.tracerOverheadEnd_ = 0;
 
   /**
    * Cummulative overhead of calls to addComment
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.tracerOverheadComment_ = 0;
 
   /**
    * Keeps stats on different types of tracers. The keys are strings and the
    * values are goog.debug.Stat
-   * @type {goog.structs.Map}
-   * @private
+   * @private {!goog.structs.Map}
    */
   this.stats_ = new goog.structs.Map();
 
   /**
    * Total number of traces created in the trace.
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.tracerCount_ = 0;
 
   /**
    * Total number of comments created in the trace.
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.commentCount_ = 0;
 
   /**
    * Next id to use for the trace.
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.nextId_ = 1;
 
@@ -143,17 +136,22 @@ goog.debug.Trace_ = function() {
 
   /**
    * Default threshold below which a tracer shouldn't be reported
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.defaultThreshold_ = 3;
+
+  /**
+   * An object containing three callback functions to be called when starting or
+   * stopping a trace, or creating a comment trace.
+   * @private {!goog.debug.Trace_.TracerCallbacks}
+   */
+  this.traceCallbacks_ = {};
 };
 
 
 /**
  * Logger for the tracer
- * @type {goog.log.Logger}
- * @private
+ * @private @const {?goog.log.Logger}
  */
 goog.debug.Trace_.prototype.logger_ = goog.log.getLogger('goog.debug.Trace');
 
@@ -307,6 +305,73 @@ goog.debug.Trace_.Event_.prototype.toString = function() {
 
 
 /**
+ * A class to specify the types of the callback functions used by
+ * {@code addTraceCallbacks}.
+ * @record
+ */
+goog.debug.Trace_.TracerCallbacks = function() {
+  /**
+   * A callback function to be called at {@code startTrace} with two parameters:
+   * a number as the started trace id and a string as the comment on the trace.
+   * @type {function(number, string)|undefined}
+   */
+  this.start;
+  /**
+   * A callback function to be called at {@code stopTrace} with one parameter:
+   * a number as the trace id being stopped.
+   * @type {function(number)|undefined}
+   */
+  this.stop;
+  /**
+   * A callback function to be called at {@code addComment} with two parameters:
+   * a string as the comment on the trace and an optional time stamp number (in
+   * milliseconds since epoch) when the comment should be added as a trace.
+   * @type {function(string, ?number=)|undefined}
+   */
+  this.comment;
+};
+
+/**
+ * A function that combines two function with the same parameters in a sequence.
+ * @param {!Function|undefined} fn1 The first function to be combined.
+ * @param {!Function|undefined} fn2 The second function to be combined.
+ * @return {!Function|undefined} A function that calls the inputs in sequence.
+ * @private
+ */
+goog.debug.Trace_.TracerCallbacks.sequence_ = function(fn1, fn2) {
+  return !fn1 ? fn2 : !fn2 ? fn1 : function() {
+    fn1.apply(undefined, arguments);
+    fn2.apply(undefined, arguments);
+  };
+};
+
+
+/**
+ * Removes all registered callback functions. Mainly used for testing.
+ */
+goog.debug.Trace_.prototype.removeAllListeners = function() {
+  this.traceCallbacks_ = {};
+};
+
+
+/**
+ * Adds up to three callback functions which are called on {@code startTracer},
+ * {@code stopTracer} and {@code addComment} in order to bridge from the Closure
+ * tracer singleton object to any tracer class.
+ * @param {!goog.debug.Trace_.TracerCallbacks} callbacks An object literal
+ *   containing the callback functions.
+ */
+goog.debug.Trace_.prototype.addTraceCallbacks = function(callbacks) {
+  this.traceCallbacks_.start = goog.debug.Trace_.TracerCallbacks.sequence_(
+      this.traceCallbacks_.start, callbacks.start);
+  this.traceCallbacks_.stop = goog.debug.Trace_.TracerCallbacks.sequence_(
+      this.traceCallbacks_.stop, callbacks.stop);
+  this.traceCallbacks_.comment = goog.debug.Trace_.TracerCallbacks.sequence_(
+      this.traceCallbacks_.comment, callbacks.comment);
+};
+
+
+/**
  * Add the ability to explicitly set the start time. This is useful for example
  * for measuring initial load time where you can set a variable as soon as the
  * main page of the app is loaded and then later call this function when the
@@ -433,6 +498,9 @@ goog.debug.Trace_.prototype.startTracer = function(comment, opt_type) {
   var now = goog.debug.Trace_.now();
   event.startTime = event.eventTime = now;
   this.tracerOverheadStart_ += now - tracerStartTime;
+  if (this.traceCallbacks_.start) {
+    this.traceCallbacks_.start(event.id, event.toString());
+  }
   return event.id;
 };
 
@@ -463,6 +531,10 @@ goog.debug.Trace_.prototype.stopTracer = function(id, opt_silenceThreshold) {
   if (startEvent == null) {
     return null;
   }
+  goog.asserts.assertNumber(id);
+  if (this.traceCallbacks_.stop) {
+    this.traceCallbacks_.stop(Number(id));
+  }
 
   this.outstandingEvents_.remove(String(id));
 
@@ -479,7 +551,6 @@ goog.debug.Trace_.prototype.stopTracer = function(id, opt_silenceThreshold) {
         break;
       }
     }
-
   } else {
     stopEvent =
         /** @type {goog.debug.Trace_.Event_} */ (this.eventPool_.getObject());
@@ -563,6 +634,9 @@ goog.debug.Trace_.prototype.addComment = function(
   this.commentCount_++;
 
   if (opt_timeStamp) {
+    if (this.traceCallbacks_.comment) {
+      this.traceCallbacks_.comment(eventComment.toString(), opt_timeStamp);
+    }
     var numEvents = this.events_.length;
     for (var i = 0; i < numEvents; i++) {
       var event = this.events_[i];
@@ -576,7 +650,10 @@ goog.debug.Trace_.prototype.addComment = function(
     if (i == numEvents) {
       this.events_.push(eventComment);
     }
-  } else {
+  } else {  // No time_stamp
+    if (this.traceCallbacks_.comment) {
+      this.traceCallbacks_.comment(eventComment.toString());
+    }
     this.events_.push(eventComment);
   }
 
--- closure/goog/debug/tracer_test.js
+++ closure/goog/debug/tracer_test.js
@@ -15,8 +15,10 @@
 goog.provide('goog.debug.TraceTest');
 goog.setTestOnly('goog.debug.TraceTest');
 
+goog.require('goog.array');
 goog.require('goog.debug.Trace');
 goog.require('goog.testing.jsunit');
+goog.require('goog.testing.recordFunction');
 
 function testTracer() {
   goog.debug.Trace.initCurrentTrace();
@@ -49,4 +51,147 @@ function testPerf() {
   }
   count *= 4;
   var end = goog.now();
+}
+
+/** @type {!Function} */
+const recorder = goog.testing.recordFunction();
+
+/**
+ * Checks if the actual log of a fake listener matches the expectations.
+ * @param {!Array<!Array<*>>} expected The expected log from the fake listener.
+ * @param {!Function} recorder The output of {@code goog.testing.recordFunction}
+ *     for logging all calls to the fake listener.
+ * @return {boolean} True if the recorder's log is expected.
+ */
+function validateRecordedListener(expected, recorder) {
+  assertObjectEquals(
+      expected, goog.array.map(recorder.getCalls(), function(call) {
+        return call.getArguments();
+      }));
+}
+
+function setUp() {
+  goog.debug.Trace.initCurrentTrace();
+  goog.debug.Trace.removeAllListeners();
+  recorder.reset();
+}
+
+function testListenerStopTracerSilence() {
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(recorder, 'start'),
+    stop: goog.partial(recorder, 'stop'),
+    comment: goog.partial(recorder, 'comment'),
+  });
+  const t = goog.debug.Trace.startTracer('first');
+  // 1000ms should be enough for silencing the tracer.
+  goog.debug.Trace.stopTracer(t, 1000);
+  validateRecordedListener([['start', t, 'first'], ['stop', t]], recorder);
+}
+
+function testListenerStartTracerType() {
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(recorder, 'start'),
+    stop: goog.partial(recorder, 'stop'),
+    comment: goog.partial(recorder, 'comment'),
+  });
+  const t = goog.debug.Trace.startTracer('first', 'New Type');
+  goog.debug.Trace.stopTracer(t);
+  validateRecordedListener(
+      [['start', t, '[New Type] first'], ['stop', t]], recorder);
+}
+
+function testListenerCommentTracerType() {
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(recorder, 'start'),
+    stop: goog.partial(recorder, 'stop'),
+    comment: goog.partial(recorder, 'comment'),
+  });
+  goog.debug.Trace.addComment('foo', 'bar');
+  validateRecordedListener([['comment', '[bar] foo']], recorder);
+}
+
+function testListenerCommentTracerTime() {
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(recorder, 'start'),
+    stop: goog.partial(recorder, 'stop'),
+    comment: goog.partial(recorder, 'comment'),
+  });
+  const timestamp = goog.now() - 10;
+  goog.debug.Trace.addComment('first', null, timestamp);
+  validateRecordedListener([['comment', 'first', timestamp]], recorder);
+}
+
+function testListenerCommentTracerAlone() {
+  goog.debug.Trace.addTraceCallbacks({
+    comment: goog.partial(recorder, 'comment'),
+  });
+  goog.debug.Trace.addComment('foo');
+  validateRecordedListener([['comment', 'foo']], recorder);
+}
+
+function testListenerCommentTracerNoLog() {
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(recorder, 'start'),
+    stop: goog.partial(recorder, 'stop'),
+  });
+  goog.debug.Trace.addComment('foo');
+  validateRecordedListener([], recorder);
+}
+
+function testListenerStartStopTracerOnly() {
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(recorder, 'start'),
+    stop: goog.partial(recorder, 'stop'),
+  });
+  const t = goog.debug.Trace.startTracer('bar');
+  goog.debug.Trace.stopTracer(t);
+  validateRecordedListener([['start', t, 'bar'], ['stop', t]], recorder);
+}
+
+function testTwoListeners() {
+  const r1 = goog.testing.recordFunction();
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(r1, 'start'),
+    stop: goog.partial(r1, 'stop'),
+  });
+  const t0 = goog.debug.Trace.startTracer('first');
+  goog.debug.Trace.stopTracer(t0);
+  const expected1 = [['start', t0, 'first'], ['stop', t0]];
+  validateRecordedListener(expected1, r1);
+
+  const r2 = goog.testing.recordFunction();
+  goog.debug.Trace.addTraceCallbacks({
+    start: goog.partial(r2, 'start'),
+    stop: goog.partial(r2, 'stop'),
+    comment: goog.partial(r2, 'comment'),
+  });
+  const t1 = goog.debug.Trace.startTracer('second', 'XType');
+  const t2 = goog.debug.Trace.startTracer('third');
+  goog.debug.Trace.addComment('NoTime');
+  const currentTime = goog.now();
+  goog.debug.Trace.addComment('WithTime', null, currentTime);
+  goog.debug.Trace.addComment('NoTime', 'YType');
+  goog.debug.Trace.stopTracer(t2);
+  goog.debug.Trace.stopTracer(t1);
+
+  const expected2 = [
+    ['start', t1, '[XType] second'],
+    ['start', t2, 'third'],
+    ['comment', 'NoTime'],
+    ['comment', 'WithTime', currentTime],
+    ['comment', '[YType] NoTime'],
+    ['stop', t2],
+    ['stop', t1],
+  ];
+  validateRecordedListener(expected2, r2);
+
+  const expectedNoComment = [
+    ['start', t0, 'first'],
+    ['stop', t0],
+    ['start', t1, '[XType] second'],
+    ['start', t2, 'third'],
+    ['stop', t2],
+    ['stop', t1],
+  ];
+  validateRecordedListener(expectedNoComment, r1);
 }
