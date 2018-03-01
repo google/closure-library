d1c480a40bc4559a93502620982d83634463223d
John Plaisted <johnplaisted@google.com>
2018-03-01 13:24:18 -0800

    Fix single character typo in comment of goog.events.Listenable.
    RELNOTES: n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=187524331

--- closure/goog/events/listenable.js
+++ closure/goog/events/listenable.js
@@ -73,7 +73,7 @@ goog.events.Listenable.IMPLEMENTED_BY_PROP =
 
 /**
  * Marks a given class (constructor) as an implementation of
- * Listenable, do that we can query that fact at runtime. The class
+ * Listenable, so that we can query that fact at runtime. The class
  * must have already implemented the interface.
  * @param {!function(new:goog.events.Listenable,...)} cls The class constructor.
  *     The corresponding class must have already implemented the interface.
