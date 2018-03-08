8469e1e0ee5e5b7d8a78c5b7b6f78c7dbe04ebf0
John Plaisted <johnplaisted@google.com>
2018-03-08 14:35:04 -0800

    Gives the RespondingChannel the ability to wait for callbacks to complete asynchronously before sending a response.
    Prior to this change, the RespondingChannel would respond immediately to each message sent to its service handlers. This CL allows it to wait for asynchronous service handlers to complete before sending a response.
    
    RELNOTES: RespondingChannel can now wait for asynchronous services before sending responses to cross-channel messages.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=188391210

--- closure/goog/messaging/respondingchannel_test.js
+++ closure/goog/messaging/respondingchannel_test.js
@@ -15,10 +15,13 @@
 goog.provide('goog.messaging.RespondingChannelTest');
 goog.setTestOnly('goog.messaging.RespondingChannelTest');
 
+goog.require('goog.Promise');
 goog.require('goog.messaging.RespondingChannel');
 goog.require('goog.testing.MockControl');
+goog.require('goog.testing.PropertyReplacer');
 goog.require('goog.testing.jsunit');
 goog.require('goog.testing.messaging.MockMessageChannel');
+goog.require('goog.testing.recordFunction');
 
 var CH1_REQUEST = {'request': 'quux1'};
 var CH2_REQUEST = {'request': 'quux2'};
@@ -31,6 +34,7 @@ var ch1;
 var ch2;
 var respondingCh1;
 var respondingCh2;
+var stubs;
 
 function setUp() {
   mockControl = new goog.testing.MockControl();
@@ -40,12 +44,15 @@ function setUp() {
 
   respondingCh1 = new goog.messaging.RespondingChannel(ch1);
   respondingCh2 = new goog.messaging.RespondingChannel(ch2);
+
+  stubs = new goog.testing.PropertyReplacer();
 }
 
 function tearDown() {
   respondingCh1.dispose();
   respondingCh2.dispose();
   mockControl.$verifyAll();
+  stubs.reset();
 }
 
 function testSendWithSignature() {
@@ -60,16 +67,22 @@ function testSendWithSignature() {
   var message4Ch2Request = {'data': CH2_REQUEST, 'signature': 1};
   var message4Ch1Response = {'data': CH1_RESPONSE, 'signature': 1};
 
-  // 1 to 2 and back.
+  // The RespondingChannel calls send() synchronously from its send() method.
+  // Request sent from channel 1 to channel 2.
   ch1.send('public:' + SERVICE_NAME, message1Ch1Request);
-  ch2.send('private:mics', message1Ch2Response);
   ch1.send('public:' + SERVICE_NAME, message2Ch1Request);
+
+  // Request sent from channel 2 to channel 1.
+  ch2.send('public:' + SERVICE_NAME, message3Ch2Request);
+  ch2.send('public:' + SERVICE_NAME, message4Ch2Request);
+
+  // It calls send() asynchronously when it receives a message.
+  // Request sent from channel 1 to channel 2.
+  ch2.send('private:mics', message1Ch2Response);
   ch2.send('private:mics', message2Ch2Response);
 
-  // 2 to 1 and back.
-  ch2.send('public:' + SERVICE_NAME, message3Ch2Request);
+  // Request sent from channel 2 to channel 1.
   ch1.send('private:mics', message3Ch1Response);
-  ch2.send('public:' + SERVICE_NAME, message4Ch2Request);
   ch1.send('private:mics', message4Ch1Response);
 
   mockControl.$replayAll();
@@ -120,7 +133,31 @@ function testSendWithSignature() {
   ch1.receive('public:' + SERVICE_NAME, message4Ch2Request);
   ch2.receive('private:mics', message4Ch1Response);
 
-  assertTrue(
-      hasInvokedCh1 && hasInvokedCh2 && hasReturnedFromCh1 &&
-      hasReturnedFromCh2);
+  // Wait for asynchronous calls to occur.
+  return goog.Promise.resolve().then(function() {
+    assertTrue(
+        hasInvokedCh1 && hasInvokedCh2 && hasReturnedFromCh1 &&
+        hasReturnedFromCh2);
+  });
+}
+
+function testWaitsForAsyncCallbackBeforeSendingResponse() {
+  stubs.set(ch1, 'send', goog.testing.recordFunction());
+  ch1.send('private:mics', {'data': CH1_RESPONSE, 'signature': 0});
+  mockControl.$replayAll();
+
+  var whenResponseReady = goog.Promise.withResolver();
+  var serviceHandler = function(message) {
+    return whenResponseReady.promise;
+  };
+
+  respondingCh1.registerService(SERVICE_NAME, serviceHandler);
+  ch1.receive('public:' + SERVICE_NAME, {'data': CH1_REQUEST, 'signature': 0});
+  // The call to send() before $replayAll() counts as one call.
+  ch1.send.assertCallCount(1);
+
+  whenResponseReady.resolve(CH1_RESPONSE);
+  return whenResponseReady.promise.then(function() {
+    ch1.send.assertCallCount(2);
+  });
 }
