ecd46608b2523cb34fc7feffd4190fd8977b6091
nnaze <nnaze@google.com>
2019-01-30 19:49:55 -0800

    Add common logic for producing additional input types.
    Start using these input types in the unit tests for union().
    
    Change per comment in code review (go/heuvk)
    
    RELNOTES: n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=231715805

--- closure/goog/collections/sets_test.js
+++ closure/goog/collections/sets_test.js
@@ -24,10 +24,35 @@ const testSuite = goog.require('goog.testing.testSuite');
 
 goog.require('goog.testing.jsunit');
 
+/** Yield of the given arguments in order. */
+function* yieldArguments(...args) {
+  for (const arg of args) {
+    yield arg;
+  }
+}
+
+/** Produce an empty generator. */
+function* emptyGenerator() {}
+
 testSuite({
   testIntersection() {
+    // arrays
     assertSameElements([2], sets.intersection(new Set([1, 2]), [2, 3]));
     assertSameElements([], sets.intersection(new Set([]), []));
     assertSameElements([1], sets.intersection(new Set([1, 1, 1]), [1, 1]));
+
+    // generators
+    assertSameElements(
+        [2], sets.intersection(new Set([1, 2]), yieldArguments(2, 3)));
+    assertSameElements([], sets.intersection(new Set([]), emptyGenerator()));
+    assertSameElements(
+        [1], sets.intersection(new Set([1, 1, 1]), yieldArguments(1, 1)));
+
+    // sets
+    assertSameElements(
+        [2], sets.intersection(new Set([1, 2]), new Set([2, 3])));
+    assertSameElements([], sets.intersection(new Set([]), new Set()));
+    assertSameElements(
+        [1], sets.intersection(new Set([1, 1, 1]), new Set([1, 1])));
   }
 });
