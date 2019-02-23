a07de0342f34d13049074848def1b3be1a2872fe
Stephen Hicks <stephenhicks@gmail.com>
2019-02-23 15:29:07 -0800

    Automated g4 rollback of changelist 235186114.
    *** Reason for rollback ***
    
    Breaks some development workflows.
    
    We're looking into the root cause, but it's likely to take non-trivial investigation.
    
    *** Original change description ***
    
    Use async.run to schedule the next func call instead of setTimeout(0) to avoid throttling in background tabs.
    
    RELNOTES: improved webchannel responsiveness in background tabs.
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=235365603

--- closure/goog/labs/net/webchannel/webchannelbase.js
+++ closure/goog/labs/net/webchannel/webchannelbase.js
@@ -214,9 +214,7 @@ goog.labs.net.webChannel.WebChannelBase = function(
 
   /**
    * Timer identifier for asynchronously making a forward channel request.
-   * This is set to true if the func is scheduled with async.run, which
-   * is equivalent to setTimeout(0).
-   * @private {?number|?boolean}
+   * @private {?number}
    */
   this.forwardChannelTimerId_ = null;
 
@@ -742,21 +740,9 @@ WebChannelBase.prototype.cancelRequests_ = function() {
   this.forwardChannelRequestPool_.cancel();
 
   if (this.forwardChannelTimerId_) {
-    this.clearForwardChannelTimer_();
-  }
-};
-
-
-/**
- * Clears the forward channel timer.
- * @private
- */
-WebChannelBase.prototype.clearForwardChannelTimer_ = function() {
-  if (goog.isNumber(this.forwardChannelTimerId_)) {
     goog.global.clearTimeout(this.forwardChannelTimerId_);
+    this.forwardChannelTimerId_ = null;
   }
-
-  this.forwardChannelTimerId_ = null;
 };
 
 
@@ -1013,7 +999,8 @@ WebChannelBase.prototype.setFailFast = function(failFast) {
     if (!this.forwardChannelRequestPool_.forceComplete(
             goog.bind(this.onRequestComplete, this))) {
       // i.e., this.forwardChannelTimerId_
-      this.clearForwardChannelTimer_();
+      goog.global.clearTimeout(this.forwardChannelTimerId_);
+      this.forwardChannelTimerId_ = null;
       // The error code from the last failed request is gone, so just use a
       // generic one.
       this.signalError_(WebChannelBase.Error.REQUEST_FAILED);
@@ -1132,11 +1119,8 @@ WebChannelBase.prototype.ensureForwardChannel_ = function() {
     return;
   }
 
-  // Use async.run instead of setTimeout(0) to avoid the 1s message delay
-  // from chrome/firefox background tabs
-  this.forwardChannelTimerId_ = true;
-  goog.async.run(this.onStartForwardChannelTimer_, this);
-
+  this.forwardChannelTimerId_ = requestStats.setTimeout(
+      goog.bind(this.onStartForwardChannelTimer_, this), 0);
   this.forwardChannelRetryCount_ = 0;
 };
 
@@ -1190,11 +1174,8 @@ WebChannelBase.prototype.maybeRetryForwardChannel_ = function(request) {
  */
 WebChannelBase.prototype.onStartForwardChannelTimer_ = function(
     opt_retryRequest) {
-  // null is possible if scheduled with async.run
-  if (this.forwardChannelTimerId_) {
-    this.forwardChannelTimerId_ = null;
-    this.startForwardChannel_(opt_retryRequest);
-  }
+  this.forwardChannelTimerId_ = null;
+  this.startForwardChannel_(opt_retryRequest);
 };
 
 
