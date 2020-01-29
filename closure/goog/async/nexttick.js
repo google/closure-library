a4f859510488e9e0f731286b6ca8ba36050fc7ea
Closure Team <closure-library-bot@google.com>
2020-01-29 11:42:28 -0800

    Remove special case for ie6-ie10 in nexttick. Just fall back to setTimeout for those browsers as we do in ie11. RELNOTES: Remove special case for ie6-ie10 in nexttick.
    PiperOrigin-RevId: 292186345

--- closure/goog/async/nexttick.js
+++ closure/goog/async/nexttick.js
@@ -1,16 +1,8 @@
-// Copyright 2013 The Closure Library Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * @license
+ * Copyright The Closure Library Authors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 /**
  * @fileoverview Provides a function to schedule running a function as soon
@@ -223,26 +215,9 @@ goog.async.nextTick.getSetImmediateEmulator_ = function() {
       channel['port2'].postMessage(0);
     };
   }
-  // Implementation for IE6 to IE10: Script elements fire an asynchronous
-  // onreadystatechange event when inserted into the DOM.
-  if (typeof document !== 'undefined' &&
-      'onreadystatechange' in goog.dom.createElement(goog.dom.TagName.SCRIPT)) {
-    return function(cb) {
-      var script = goog.dom.createElement(goog.dom.TagName.SCRIPT);
-      script.onreadystatechange = function() {
-        // Clean up and call the callback.
-        script.onreadystatechange = null;
-        script.parentNode.removeChild(script);
-        script = null;
-        cb();
-        cb = null;
-      };
-      document.documentElement.appendChild(/** @type {!Node} */ (script));
-    };
-  }
   // Fall back to setTimeout with 0. In browsers this creates a delay of 5ms
   // or more.
-  // NOTE(user): This fallback is used for IE11.
+  // NOTE(user): This fallback is used for IE.
   return function(cb) {
     goog.global.setTimeout(/** @type {function()} */ (cb), 0);
   };
