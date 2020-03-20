7096b9b5a92fc51dd961fbe7073e6371587b26a9
Stephen Hicks <sdh@google.com>
2020-03-20 09:07:31 -0700

    Convert more license blocks to SPDX. RELNOTES: n/a
    PiperOrigin-RevId: 302038908

--- closure/goog/net/channelrequest.js
+++ closure/goog/net/channelrequest.js
@@ -1,16 +1,8 @@
-// Copyright 2006 The Closure Library Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * @license
+ * Copyright The Closure Library Authors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 /**
  * @fileoverview Definition of the ChannelRequest class. The ChannelRequest
@@ -28,6 +20,7 @@ goog.provide('goog.net.ChannelRequest.Error');
 
 goog.forwardDeclare('goog.net.BrowserChannel');
 goog.forwardDeclare('goog.net.BrowserTestChannel');
+goog.forwardDeclare('goog.net.ChannelDebug');
 goog.require('goog.Timer');
 goog.require('goog.async.Throttle');
 goog.require('goog.dom.TagName');
@@ -43,7 +36,6 @@ goog.require('goog.string');
 goog.require('goog.string.Const');
 goog.require('goog.userAgent');
 goog.requireType('goog.Uri');
-goog.requireType('goog.net.ChannelDebug');
 goog.requireType('goog.net.XhrIo');
 
 // TODO(nnaze): This file depends on goog.net.BrowserChannel and vice versa (a
