5cfbdbe64ffcfff62ea146c6daada694d42b46c0
John Lenz <johnlenz@google.com>
2020-09-29 22:16:21 +0000

    Modernized Closure Library classes RELNOTES[INC]: Modernized Closure Library classes
    PiperOrigin-RevId: 334472761

--- closure/goog/labs/net/webchannel/wire.js
+++ closure/goog/labs/net/webchannel/wire.js
@@ -17,10 +17,11 @@ goog.requireType('goog.structs.Map');
 
 /**
  * The interface class.
- *
  * @interface
  */
-goog.labs.net.webChannel.Wire = function() {};
+goog.labs.net.webChannel.Wire = class {
+  constructor() {}
+};
 
 
 /**
@@ -42,46 +43,47 @@ goog.labs.net.webChannel.Wire.RAW_DATA_KEY = '__data__';
 
 /**
  * Simple container class for a (mapId, map) pair.
- * @param {number} mapId The id for this map.
- * @param {!Object|!goog.structs.Map} map The map itself.
- * @param {!Object=} opt_context The context associated with the map.
- * @constructor
- * @struct
  */
-goog.labs.net.webChannel.Wire.QueuedMap = function(mapId, map, opt_context) {
-  'use strict';
+goog.labs.net.webChannel.Wire.QueuedMap = class {
   /**
-   * The id for this map.
-   * @type {number}
+   * @param {number} mapId The id for this map.
+   * @param {!Object|!goog.structs.Map} map The map itself.
+   * @param {!Object=} opt_context The context associated with the map.
    */
-  this.mapId = mapId;
+  constructor(mapId, map, opt_context) {
+    'use strict';
+    /**
+     * The id for this map.
+     * @type {number}
+     */
+    this.mapId = mapId;
 
-  /**
-   * The map itself.
-   * @type {!Object|!goog.structs.Map}
-   */
-  this.map = map;
+    /**
+     * The map itself.
+     * @type {!Object|!goog.structs.Map}
+     */
+    this.map = map;
 
-  /**
-   * The context for the map.
-   * @type {Object}
-   */
-  this.context = opt_context || null;
-};
-
-
-/**
- * @return {number|undefined} the size of the raw JSON message or
- * undefined if the message is not encoded as a raw JSON message
- */
-goog.labs.net.webChannel.Wire.QueuedMap.prototype.getRawDataSize = function() {
-  'use strict';
-  if (goog.labs.net.webChannel.Wire.RAW_DATA_KEY in this.map) {
-    const data = this.map[goog.labs.net.webChannel.Wire.RAW_DATA_KEY];
-    if (typeof data === 'string') {
-      return data.length;
-    }
+    /**
+     * The context for the map.
+     * @type {Object}
+     */
+    this.context = opt_context || null;
   }
 
-  return undefined;
+  /**
+   * @return {number|undefined} the size of the raw JSON message or
+   * undefined if the message is not encoded as a raw JSON message
+   */
+  getRawDataSize() {
+    'use strict';
+    if (goog.labs.net.webChannel.Wire.RAW_DATA_KEY in this.map) {
+      const data = this.map[goog.labs.net.webChannel.Wire.RAW_DATA_KEY];
+      if (typeof data === 'string') {
+        return data.length;
+      }
+    }
+
+    return undefined;
+  }
 };
