03589e4f99624af0d4a6fa84ccf29657d10e0d00
Closure Team <closure-library-bot@google.com>
2021-01-20 15:32:43 +0000

    RELNOTES[INC]: Removed setKeyboardShortcutKey() from the editor plugin RemoveFormatting. This plugin now defaults to Cmd + \ on Mac, and both Ctrl + Space and Ctrl + \ on other platforms.
    PiperOrigin-RevId: 352789804

--- closure/goog/editor/plugins/removeformatting.js
+++ closure/goog/editor/plugins/removeformatting.js
@@ -43,14 +43,6 @@ goog.editor.plugins.RemoveFormatting = function() {
    * @private
    */
   this.optRemoveFormattingFunc_ = null;
-
-  /**
-   * The key that this plugin triggers on when pressed with the platform
-   * modifier key. Can be set by calling {@link #setKeyboardShortcutKey}.
-   * @type {string}
-   * @private
-   */
-  this.keyboardShortcutKey_ = ' ';
 };
 goog.inherits(goog.editor.plugins.RemoveFormatting, goog.editor.Plugin);
 
@@ -151,23 +143,14 @@ goog.editor.plugins.RemoveFormatting.prototype.handleKeyboardShortcut =
     return false;
   }
 
-  if (key == this.keyboardShortcutKey_) {
+  // Cmd + Space does not work on Mac. Ctrl/Cmd + \ is platform agnostic.
+  if ((!goog.userAgent.MAC && key === ' ') || (key === '\\')) {
     this.getFieldObject().execCommand(
         goog.editor.plugins.RemoveFormatting.REMOVE_FORMATTING_COMMAND);
     return true;
   }
 
   return false;
-};
-
-
-/**
- * @param {string} key
- */
-goog.editor.plugins.RemoveFormatting.prototype.setKeyboardShortcutKey =
-    function(key) {
-  'use strict';
-  this.keyboardShortcutKey_ = key;
 };
 
 
