9f91cacc86fa204536a523b61033106d50f1e63d
Stephen Hicks <sdh@google.com>
2021-01-26 15:54:52 -0800

    Remove useragent checking for various goog.events.BrowserFeature properties:
    * HAS_W3C_BUTTON
    * HAS_W3C_EVENT_SUPPORT
    * HAS_NAVIGATOR_ONLINE_PROPERTY
    * HAS_HTML5_NETWORK_EVENT_SUPPORT
    
    These are now always true.  Also sets some workaround features to permanently false:
    
    * SET_KEY_CODE_TO_PREVENT_DEFAULT
    * HTML5_NETWORK_EVENTS_FIRE_ON_BODY
    
    Assuming this sticks, we will delete all the special handling for older browsers in a future change.
    
    RELNOTES[INC]: Removed some goog.events support for IE8- era browsers.
    
    PiperOrigin-RevId: 353969984

--- closure/goog/events/browserfeature.js
+++ closure/goog/events/browserfeature.js
@@ -11,8 +11,6 @@
 goog.module('goog.events.BrowserFeature');
 goog.module.declareLegacyNamespace();
 
-const googUserAgent = goog.require('goog.userAgent');
-
 
 /**
  * Tricks Closure Compiler into believing that a function is pure.  The compiler
@@ -36,43 +34,34 @@ exports = {
    * Whether the button attribute of the event is W3C compliant.  False in
    * Internet Explorer prior to version 9; document-version dependent.
    */
-  HAS_W3C_BUTTON: !googUserAgent.IE || googUserAgent.isDocumentModeOrHigher(9),
+  HAS_W3C_BUTTON: true,
 
   /**
    * Whether the browser supports full W3C event model.
    */
-  HAS_W3C_EVENT_SUPPORT:
-      !googUserAgent.IE || googUserAgent.isDocumentModeOrHigher(9),
+  HAS_W3C_EVENT_SUPPORT: true,
 
   /**
    * To prevent default in IE7-8 for certain keydown events we need set the
    * keyCode to -1.
    */
-  SET_KEY_CODE_TO_PREVENT_DEFAULT:
-      googUserAgent.IE && !googUserAgent.isVersionOrHigher('9'),
+  SET_KEY_CODE_TO_PREVENT_DEFAULT: false,
 
   /**
    * Whether the `navigator.onLine` property is supported.
    */
-  HAS_NAVIGATOR_ONLINE_PROPERTY:
-      !googUserAgent.WEBKIT || googUserAgent.isVersionOrHigher('528'),
+  HAS_NAVIGATOR_ONLINE_PROPERTY: true,
 
   /**
    * Whether HTML5 network online/offline events are supported.
    */
-  HAS_HTML5_NETWORK_EVENT_SUPPORT:
-      googUserAgent.GECKO && googUserAgent.isVersionOrHigher('1.9b') ||
-      googUserAgent.IE && googUserAgent.isVersionOrHigher('8') ||
-      googUserAgent.OPERA && googUserAgent.isVersionOrHigher('9.5') ||
-      googUserAgent.WEBKIT && googUserAgent.isVersionOrHigher('528'),
+  HAS_HTML5_NETWORK_EVENT_SUPPORT: true,
 
   /**
    * Whether HTML5 network events fire on document.body, or otherwise the
    * window.
    */
-  HTML5_NETWORK_EVENTS_FIRE_ON_BODY:
-      googUserAgent.GECKO && !googUserAgent.isVersionOrHigher('8') ||
-      googUserAgent.IE && !googUserAgent.isVersionOrHigher('9'),
+  HTML5_NETWORK_EVENTS_FIRE_ON_BODY: false,
 
   /**
    * Whether touch is enabled in the browser.
