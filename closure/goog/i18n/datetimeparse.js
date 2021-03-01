54b59276d091d411556ce845dda9d1a4947f1e4d
Closure Team <closure-library-bot@google.com>
2021-03-01 14:32:28 -0800

    Improve documentation and test coverage of goog.i18n.DateTimeParse. RELNOTES: n/a
    PiperOrigin-RevId: 360279027

--- closure/goog/i18n/datetimeparse.js
+++ closure/goog/i18n/datetimeparse.js
@@ -263,8 +263,9 @@ goog.i18n.DateTimeParse.prototype.applyStandardPattern_ = function(formatType) {
 
 
 /**
- * Parse the given string and fill info into date object. This version does
- * not validate the input.
+ * Parse the given string and fill parsed values into date object. The existing
+ * values of any temporal fields of `date` not parsed from `text` are unchanged.
+ * This version does not validate that the result is a valid date/time.
  * @param {string} text The string being parsed.
  * @param {goog.date.DateLike} date The Date object to hold the parsed date.
  * @return {number} How many characters parser advanced.
@@ -277,7 +278,7 @@ goog.i18n.DateTimeParse.prototype.parse = function(text, date) {
 
 /**
  * Parse the given string and fill info into date object. This version will
- * validate the input and make sure it is a valid date/time.
+ * validate that the result is a valid date/time.
  * @param {string} text The string being parsed.
  * @param {goog.date.DateLike} date The Date object to hold the parsed date.
  * @return {number} How many characters parser advanced.
@@ -1054,7 +1055,7 @@ goog.i18n.DateTimeParse.MyDate_.prototype.setTwoDigitYear_ = function(year) {
  * set, use the passed in date object's value.
  *
  * @param {goog.date.DateLike} date Date object to be filled.
- * @param {boolean} validation If true, input string will be checked to make
+ * @param {boolean} validation If true, input date will be checked to make
  *     sure it is valid.
  *
  * @return {boolean} false if fields specify a invalid date.
