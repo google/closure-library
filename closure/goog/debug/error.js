8797fa82e0f1c061e64f04de3c203ab13ae43e1c
Closure Team <closure-library-bot@google.com>
2021-04-02 22:26:02 +0000

    Allow specifying a cause when creating a goog.debug.Error to be used with cause chaining in goog.debug.serializeErrorStack_. RELNOTES[NEW]: DebugError constructor takes an optional error as a cause for error chaining.
    PiperOrigin-RevId: 366524121

--- closure/goog/debug/error.js
+++ closure/goog/debug/error.js
@@ -19,11 +19,19 @@ goog.module.declareLegacyNamespace();
 
 /**
  * Base class for custom error objects.
- * @param {*=} opt_msg The message associated with the error.
+ * @param {*=} msg The message associated with the error.
+ * @param {{
+ *    message: (?|undefined),
+ *    name: (?|undefined),
+ *    lineNumber: (?|undefined),
+ *    fileName: (?|undefined),
+ *    stack: (?|undefined),
+ *    cause: (?|undefined),
+ * }=} cause The original error object to chain with.
  * @constructor
  * @extends {Error}
  */
-function DebugError(opt_msg) {
+function DebugError(msg = undefined, cause = undefined) {
   // Attempt to ensure there is a stack trace.
   if (Error.captureStackTrace) {
     Error.captureStackTrace(this, DebugError);
@@ -35,9 +43,13 @@ function DebugError(opt_msg) {
     }
   }
 
-  if (opt_msg) {
+  if (msg) {
     /** @override */
-    this.message = String(opt_msg);
+    this.message = String(msg);
+  }
+
+  if (cause) {
+    this.cause = cause;
   }
 
   /**
