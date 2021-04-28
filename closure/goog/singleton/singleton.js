de7728494203b5578305a2a0978a66e7e37a965e
Closure Team <closure-library-bot@google.com>
2021-04-28 12:48:02 -0700

    Introduce goog.singleton.getInstance() which can replace goog.addSingletonGetter(). RELNOTES[NEW]: Introduce goog.singleton.getInstance() which can replace goog.addSingletonGetter().
    PiperOrigin-RevId: 370964786
    Change-Id: I81aea063b9b584791d0a0ca409b502161e9164ee

---
+++ closure/goog/singleton/singleton.js
@@ -0,0 +1,63 @@
+/**
+ * @license
+ * Copyright The Closure Library Authors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Provides an implementation for getInstance() methods.
+ */
+
+goog.module('goog.singleton');
+goog.module.declareLegacyNamespace();
+
+const {assert} = goog.require('goog.asserts');
+
+/** @type {!Array<function(new: ?): ?>} */
+const instantiatedSingletons = [];
+
+/**
+ * @template T
+ * @record
+ */
+class Singleton {
+  constructor() {
+    /** @type {!T} */
+    this.instance_;
+  }
+}
+
+/**
+ * Used as the implementation body for a static getInstance method.
+ *
+ * ```
+ * class Foo {
+ *   static getInstance() {
+ *     return getInstance(Foo);
+ *   }
+ * }
+ * ```
+ * @param {function(new: T): T} ctor
+ * @return {T}
+ * @template T
+ * @deprecated Singleton patterns are discouraged. Use dependency injection
+ *     instead.
+ */
+exports.getInstance = (ctor) => {
+  assert(
+      !Object.isSealed(ctor),
+      'Cannot use getInstance() with a sealed constructor.');
+  const ctorWithInstance = /** @type {!Singleton} */ (ctor);
+  if (ctorWithInstance.instance_) {
+    return ctorWithInstance.instance_;
+  }
+  if (goog.DEBUG) {
+    // Used to reset singletons in test code.
+    instantiatedSingletons.push(ctor);
+  }
+  const instance = new ctor();
+  ctorWithInstance.instance_ = instance;
+  return instance;
+};
+
+exports.instantiatedSingletons = instantiatedSingletons;
