4b154bf3bd952122ca8e01a975b7f65af2b97ae2
Closure Team <closure-library-bot@google.com>
2021-06-04 10:37:20 -0700

    Set test MouseEvent page coordinates to the same as client coordinates. RELNOTES: n/a
    PiperOrigin-RevId: 377541544
    Change-Id: Ia88fdc63c3bb5efc7bf55fe5cf283a51850241e7

--- closure/goog/testing/events/events.js
+++ closure/goog/testing/events/events.js
@@ -145,9 +145,13 @@ goog.testing.events.setEventClientXY_ = function(event, opt_coords) {
   event.clientX = opt_coords ? opt_coords.x : 0;
   event.clientY = opt_coords ? opt_coords.y : 0;
 
-  // Pretend the browser window is at (0, 0).
+  // Pretend the browser window is at (0, 0) of the screen.
   event.screenX = event.clientX;
   event.screenY = event.clientY;
+
+  // Assume that there was no page scroll.
+  event.pageX = event.clientX;
+  event.pageY = event.clientY;
 };
 
 
