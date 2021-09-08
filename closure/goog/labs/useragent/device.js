cb82c4773473bbc29e2a0c0702aa15f266452021
Closure Team <closure-library-bot@google.com>
2021-09-08 12:22:53 -0700

    Get device isMobile information from navigator.userAgentData whenever is available. RELNOTES: Use Client hints API to get device isMobile info.
    PiperOrigin-RevId: 395531892
    Change-Id: I8715d969ca50771cef41aad6d88e5910392300ee

--- closure/goog/labs/useragent/device.js
+++ closure/goog/labs/useragent/device.js
@@ -23,8 +23,10 @@ goog.require('goog.labs.userAgent.util');
  */
 goog.labs.userAgent.device.isMobile = function() {
   'use strict';
-  // TODO(user): Use userAgentData.mobile to detect after fixing tablet
-  // related tests.
+  if (goog.labs.userAgent.ASSUME_CLIENT_HINTS_SUPPORT ||
+      goog.labs.userAgent.util.getUserAgentData()) {
+    return goog.labs.userAgent.util.getUserAgentData().mobile;
+  }
   return !goog.labs.userAgent.device.isTablet() &&
       (goog.labs.userAgent.util.matchUserAgent('iPod') ||
        goog.labs.userAgent.util.matchUserAgent('iPhone') ||
@@ -41,6 +43,13 @@ goog.labs.userAgent.device.isMobile = function() {
  */
 goog.labs.userAgent.device.isTablet = function() {
   'use strict';
+  if (goog.labs.userAgent.ASSUME_CLIENT_HINTS_SUPPORT ||
+      goog.labs.userAgent.util.getUserAgentData()) {
+    return !goog.labs.userAgent.util.getUserAgentData().mobile &&
+        (goog.labs.userAgent.util.matchUserAgent('iPad') ||
+         goog.labs.userAgent.util.matchUserAgent('Android') ||
+         goog.labs.userAgent.util.matchUserAgent('Silk'));
+  }
   return goog.labs.userAgent.util.matchUserAgent('iPad') ||
       (goog.labs.userAgent.util.matchUserAgent('Android') &&
        !goog.labs.userAgent.util.matchUserAgent('Mobile')) ||
--- closure/goog/labs/useragent/device_test.js
+++ closure/goog/labs/useragent/device_test.js
@@ -9,27 +9,46 @@
 goog.module('goog.labs.userAgent.deviceTest');
 goog.setTestOnly();
 
+const PropertyReplacer = goog.require('goog.testing.PropertyReplacer');
 const device = goog.require('goog.labs.userAgent.device');
+const functions = goog.require('goog.functions');
 const testAgents = goog.require('goog.labs.userAgent.testAgents');
 const testSuite = goog.require('goog.testing.testSuite');
 const util = goog.require('goog.labs.userAgent.util');
 
-function assertIsMobile(uaString) {
+const stubs = new PropertyReplacer();
+
+/**
+ * @param {?string} uaString
+ * @param {?NavigatorUAData=} uaData
+ */
+function assertIsMobile(uaString, uaData) {
   util.setUserAgent(uaString);
+  stubs.set(util, 'getUserAgentData', functions.constant(uaData || null));
   assertTrue(device.isMobile());
   assertFalse(device.isTablet());
   assertFalse(device.isDesktop());
 }
 
-function assertIsTablet(uaString) {
+/**
+ * @param {?string} uaString
+ * @param {?NavigatorUAData=} uaData
+ */
+function assertIsTablet(uaString, uaData) {
   util.setUserAgent(uaString);
+  stubs.set(util, 'getUserAgentData', functions.constant(uaData || null));
   assertTrue(device.isTablet());
   assertFalse(device.isMobile());
   assertFalse(device.isDesktop());
 }
 
-function assertIsDesktop(uaString) {
+/**
+ * @param {?string} uaString
+ * @param {?NavigatorUAData=} uaData
+ */
+function assertIsDesktop(uaString, uaData) {
   util.setUserAgent(uaString);
+  stubs.set(util, 'getUserAgentData', functions.constant(uaData || null));
   assertTrue(device.isDesktop());
   assertFalse(device.isMobile());
   assertFalse(device.isTablet());
@@ -44,12 +63,16 @@ testSuite({
     assertIsMobile(testAgents.CHROME_ANDROID);
     assertIsMobile(testAgents.SAFARI_IPHONE_6);
     assertIsMobile(testAgents.IE_10_MOBILE);
+    assertIsMobile(null, testAgents.CHROME_USERAGENT_DATA_MOBILE);
   },
 
   testTablet() {
     assertIsTablet(testAgents.CHROME_ANDROID_TABLET);
     assertIsTablet(testAgents.KINDLE_FIRE);
     assertIsTablet(testAgents.IPAD_6);
+    assertIsTablet(
+        testAgents.CHROME_ANDROID_TABLET, testAgents.CHROME_USERAGENT_DATA);
+    assertIsTablet(testAgents.KINDLE_FIRE, testAgents.CHROME_USERAGENT_DATA);
   },
 
   testDesktop() {
@@ -59,5 +82,6 @@ testSuite({
     assertIsDesktop(testAgents.IE_9);
     assertIsDesktop(testAgents.IE_10);
     assertIsDesktop(testAgents.IE_11);
+    assertIsDesktop(testAgents.CHROME_25, testAgents.CHROME_USERAGENT_DATA);
   },
 });
