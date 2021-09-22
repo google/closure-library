f8286a57ec53f43100561d2a39b768fc0957738d
Closure Team <closure-library-bot@google.com>
2021-09-21 18:29:52 -0700

    Automated rollback of commit 46ffcc37da049a4e36fe8d3a1125e39114331a27.
    PiperOrigin-RevId: 398130740
    Change-Id: I1d0bad4ec48b28d3e18ce0f065a30c540d0f6b57

--- closure/goog/labs/useragent/useragent.js
+++ closure/goog/labs/useragent/useragent.js
@@ -11,24 +11,9 @@
 goog.module('goog.labs.userAgent');
 
 /**
- * @define {string} Optional runtime override for the USE_CLIENT_HINTS flag.
- * If this is set (for example, to 'foo.bar') then any value of USE_CLIENT_HINTS
- * will be overridden by `globalThis.foo.bar` if it is non-null.
- * This flag will be removed in December 2021.
- */
-const USE_CLIENT_HINTS_OVERRIDE =
-    goog.define('goog.labs.userAgent.USE_CLIENT_HINTS_OVERRIDE', '');
-
-/**
  * @define {boolean} If true, use navigator.userAgentData
  * TODO(user) Flip flag in 2021/12.
  */
 const USE_CLIENT_HINTS =
     goog.define('goog.labs.userAgent.USE_CLIENT_HINTS', false);
-
-/** @const {boolean} */
-exports.USE_CLIENT_HINTS =
-    (USE_CLIENT_HINTS_OVERRIDE ?
-         goog.getObjectByName(USE_CLIENT_HINTS_OVERRIDE) :
-         undefined) ??
-    USE_CLIENT_HINTS;
+exports.USE_CLIENT_HINTS = USE_CLIENT_HINTS;
