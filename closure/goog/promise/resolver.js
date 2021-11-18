a1f61c86f8d95e041e86ab6865539bc0e01d39f2
Closure Team <closure-library-bot@google.com>
2021-11-18 12:18:40 -0800

    Automated rollback of commit 63d65e226666436adff7e5ece048656a2f90ebfa.
    PiperOrigin-RevId: 410869609
    Change-Id: Ie445433e329df7ff5d17a753f64e858dc8b72e3f

--- closure/goog/promise/resolver.js
+++ closure/goog/promise/resolver.js
@@ -3,37 +3,40 @@
  * Copyright The Closure Library Authors.
  * SPDX-License-Identifier: Apache-2.0
  */
-goog.module('goog.promise.Resolver');
-goog.module.declareLegacyNamespace();
 
-const GoogPromise = goog.requireType('goog.Promise');
-const Thenable = goog.requireType('goog.Thenable');
+goog.provide('goog.promise.Resolver');
+
+goog.requireType('goog.Promise');
+
+
 
 /**
  * Resolver interface for promises. The resolver is a convenience interface that
  * bundles the promise and its associated resolve and reject functions together,
  * for cases where the resolver needs to be persisted internally.
- * @template TYPE
+ *
  * @interface
+ * @template TYPE
  */
-class Resolver {
-  constructor() {
-    /**
-     * The promise that created this resolver.
-     * @type {!GoogPromise<TYPE>}
-     */
-    this.promise;
-    /**
-     * Resolves this resolver with the specified value.
-     * @type {function((TYPE|GoogPromise<TYPE>|Thenable)=)}
-     */
-    this.resolve;
-    /**
-     * Rejects this resolver with the specified reason.
-     * @type {function(*=): void}
-     */
-    this.reject;
-  }
-}
+goog.promise.Resolver = function() {};
 
-exports = Resolver;
+
+/**
+ * The promise that created this resolver.
+ * @type {!goog.Promise<TYPE>}
+ */
+goog.promise.Resolver.prototype.promise;
+
+
+/**
+ * Resolves this resolver with the specified value.
+ * @type {function((TYPE|goog.Promise<TYPE>|Thenable)=)}
+ */
+goog.promise.Resolver.prototype.resolve;
+
+
+/**
+ * Rejects this resolver with the specified reason.
+ * @type {function(*=): void}
+ */
+goog.promise.Resolver.prototype.reject;
