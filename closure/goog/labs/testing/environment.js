fa3bd8e42166d4ba3019b78a7f66fbe762f2b6ab
Closure Team <closure-library-bot@google.com>
2022-02-16 10:46:40 -0800

    Expose a new option for withMockClock() on Environments that installs an async MockClock instead of a normal MockClock.
    This is useful for teams trying to transition off of a normal MockClock to an async MockClock, which treats Promise and goog.Promise equivalently and uses microtasks.
    RELNOTES: n/a
    PiperOrigin-RevId: 429087432
    Change-Id: Ia258e9a3ccebb11fb3c522ee9b389139f18d53b1

--- closure/goog/labs/testing/environment.js
+++ closure/goog/labs/testing/environment.js
@@ -91,14 +91,13 @@ class Environment {
     // Make sure promises and other stuff that may still be scheduled,
     // get a
     // chance to run (and throw errors).
-    if (this.mockClock) {
-      for (let i = 0; i < 100; i++) {
-        this.mockClock.tick(1000);
+    if (this.hasMockClock()) {
+      if (this.mockClock.isSynchronous()) {
+        for (let i = 0; i < 100; i++) {
+          this.mockClock.tick(1000);
+        }
       }
-      // If we created the mockClock, we'll also reset it.
-      if (this.hasMockClock()) {
-        this.mockClock.reset();
-      }
+      this.mockClock.reset();
     }
     // Reset all changes made by the PropertyReplacer.
     this.replacer.reset();
@@ -149,13 +148,17 @@ class Environment {
    * installed (override i.e. setTimeout) by default. It can be accessed
    * using `env.mockClock`. If your test has more than one testing
    * environment, don't call this on more than one of them.
-   * @param {{install: (boolean|undefined)}=} options Options about the
-   *     mockClock.
+   * @param {{install: (boolean|undefined), async: (boolean|undefined)}=}
+   *     options Options about the mockClock.
    * @return {!Environment} For chaining.
    */
-  withMockClock({install = true} = {}) {
-    if (!this.hasMockClock()) {
-      this.mockClock = new MockClock(install);
+  withMockClock({install = true, async = false} = {}) {
+    if (!this.hasMockClock() || this.mockClock.isSynchronous() === async) {
+      this.mockClock =
+          async ? MockClock.createAsyncMockClock() : new MockClock();
+      if (install) {
+        this.mockClock.install();
+      }
     }
     return this;
   }
--- closure/goog/labs/testing/environment_test.js
+++ closure/goog/labs/testing/environment_test.js
@@ -411,6 +411,39 @@ testingTestSuite(testSuite = {
     testing = false;
   },
 
+  async testAsyncMockClock() {
+    testing = true;
+
+    const env = new Environment().withMockClock({async: true});
+
+    testCase.setUp = () => {
+      env.mockClock.install();
+    };
+    testCase.addNewTest('testThatThrowsEventuallyAsync', () => {
+      setTimeout(() => {
+        throw new Error('LateErrorMessage');
+      }, 200);
+    });
+    testCase.tearDown = async () => {
+      await env.mockClock.tickAsync(Infinity);
+    };
+
+    await runTestsReturningPromise(testCase);
+    assertTestFailure(
+        testCase, 'testThatThrowsEventuallyAsync', 'LateErrorMessage');
+
+    testing = false;
+  },
+
+  async testMockClockOverwrites() {
+    const env = new Environment().withMockClock();
+    assertTrue(env.mockClock.isSynchronous());
+    env.withMockClock({async: true});
+    assertFalse(env.mockClock.isSynchronous());
+    env.withMockClock();
+    assertTrue(env.mockClock.isSynchronous());
+  },
+
   async testMockControl() {
     testing = true;
 
