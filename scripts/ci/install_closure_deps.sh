cda4123da863a780b71c2d41d2f64b40200fc12b
James Wright <jameswr@google.com>
2022-03-09 15:57:49 -0800

    Correctly fetch the latest nightly builds of closure compiler and linter from Sonatype. RELNOTES: n/a
    PiperOrigin-RevId: 433598328
    Change-Id: I9f3d3915c1af8c45282bcbbdedc580203a5bf3b3

--- scripts/ci/install_closure_deps.sh
+++ scripts/ci/install_closure_deps.sh
@@ -22,16 +22,27 @@ set -ex
 cd ..
 
 # Fetches Closure Compiler components from oss.sonatype.org
+# Sonatype doesn't appear to have a REST API, so instead, we manually scrape the
+# relevant pages for a particular repository.
+# For example, we are scraping
+# https://oss.sonatype.org/content/repositories/snapshots/com/google/javascript/closure-compiler/1.0-SNAPSHOT/
+# for the compiler.
 fetch () {
   local name="$1"
   local snapshots="https://oss.sonatype.org/content/repositories/snapshots"
   local repo="$snapshots/com/google/javascript"
   local dir="$repo/$name/1.0-SNAPSHOT"
   local jar="$name-1.0-SNAPSHOT.jar"
+  # use Curl to download the HTML of the page above and scrape it for all jar
+  # links. Then, sort this list (latest ends up last), and use it.
+  # Previously, this script might have relied on the page contents being sorted
+  # latest first.
+  # TODO(user): Change this to read the maven-metadata.xml file instead
   local url=$(
     curl -o - "$dir/" |
       sed -n 's+.*<a href="\('"$dir/$name"'-[-.0-9]*\.jar\)".*+\1+p' |
-      head -n 1)
+      sort |
+      tail -n 1)
   [ -n "$url" ]
   wget -O "$jar" "$url"
 }
