461dc5fe23cb23d8f97eb43f2572c0879dea910b
Closure Team <closure-library-bot@google.com>
2022-05-10 11:13:15 -0700

    RELNOTES[INC]: Remove deprecated goog.net.xpc.CfgFields.DIRECT_TRANSPORT_SYNC_MODE as the Direct Transport mechanism has been removed from Closure.
    PiperOrigin-RevId: 447780568
    Change-Id: Iaa5f6d43d23ac9adf21ff5a8af9deec04e3c94ba

--- closure/goog/net/xpc/xpc.js
+++ closure/goog/net/xpc/xpc.js
@@ -164,13 +164,6 @@ goog.net.xpc.CfgFields = {
    * All other initial startup and reconnection scenarios are supported.
    */
   NATIVE_TRANSPORT_PROTOCOL_VERSION: 'nativeProtocolVersion',
-  /**
-   * Whether the direct transport runs in synchronous mode. The default is to
-   * emulate the other transports and run asyncronously but there are some
-   * circumstances where syncronous calls are required. If this property is
-   * set to true, the transport will send the messages synchronously.
-   */
-  DIRECT_TRANSPORT_SYNC_MODE: 'directSyncMode',
 };
 
 
